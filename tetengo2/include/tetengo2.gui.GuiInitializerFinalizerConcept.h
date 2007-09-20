fec60b07d062016cae200bb63740b3b6f0e120c2
kaoru <kaorut@mc.pikara.ne.jp>
2007-09-20 00:29:48 +0000

    Corrected the DoxyS document.
    git-svn-id: file:///home/kaoru/svnrepos/bobura/trunk@101 5a212d1d-074f-4316-8284-043cf949738a

--- tetengo2/include/tetengo2.gui.GuiInitializerFinalizerConcept.h
+++ tetengo2/include/tetengo2.gui.GuiInitializerFinalizerConcept.h
@@ -17,9 +17,11 @@
 namespace tetengo2 { namespace gui
 {
     /*!
-        \brief The concept check class template for canvases.
+        \brief The concept check class template for GUI initialization and
+               finalization managers.
 
-        \param GuiInitializerFinalizer A canvas type.
+        \param GuiInitializerFinalizer A GUI initialization and finalization
+                                       manager type.
     */
     template <typename GuiInitializerFinalizer>
     class GuiInitializerFinalizerConcept
