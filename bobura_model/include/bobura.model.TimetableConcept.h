0f0c5cf9865b40ca3b56e4520243ddc30e5ed843
kaoru <kaorut@mc.pikara.ne.jp>
2007-12-30 13:51:47 +0000

    Created bobura::model::TimetableConcept.
    git-svn-id: file:///home/kaoru/svnrepos/bobura/trunk@129 5a212d1d-074f-4316-8284-043cf949738a

---
+++ bobura_model/include/bobura.model.TimetableConcept.h
@@ -0,0 +1,67 @@
+/*! \file
+    \brief The definition of bobura::model::TimetableConcept.
+
+    Copyright (C) 2007 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_MODEL_TIMETABLECONCEPT_H)
+#define BOBURA_MODEL_TIMETABLECONCEPT_H
+
+#include <algorithm>
+
+#include <boost/concept_check.hpp>
+
+
+namespace bobura { namespace model
+{
+    /*!
+        \brief The concept check class template for timetables.
+
+        \param Timetable A timetable type.
+    */
+    template <typename Timetable>
+    class TimetableConcept
+    {
+    public:
+        // functions
+
+        /*!
+            \brief Checks the constraints on a canvas.
+        */
+        void constraints()
+        {
+            typedef typename Timetable::stations_type stations_type;
+
+            m_timetable.swap(m_timetable);
+            std::swap(m_timetable, m_timetable);
+
+            m_timetable = m_timetable;
+
+            const_constraints(m_timetable);
+        }
+
+        /*!
+            \brief Checks the const constraints on a canvas.
+        */
+        void const_constraints(const Timetable& timetable)
+        {
+            typename Timetable::stations_type* const p_stations =
+                timetable.p_stations();
+            boost::ignore_unused_variable_warning(p_stations);
+        }
+
+
+    private:
+        // variables
+
+        Timetable m_timetable;
+
+
+    };
+
+
+}}
+
+#endif
