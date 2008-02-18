0228b2293de8102929aef683012d79d0daaee807
kaoru <kaorut@mc.pikara.ne.jp>
2008-02-18 08:08:59 +0000

    Created launch_vsee.bat. Modified bobura.rc. Modified the project.file Modified set_svn_props.sh. Modified the svn properties.
    git-svn-id: file:///home/kaoru/svnrepos/bobura/trunk@177 5a212d1d-074f-4316-8284-043cf949738a

--- set_svn_props.sh
+++ set_svn_props.sh
@@ -1,17 +1,37 @@
 #!/bin/sh
 
 # Subversion property setter.
+# $Id$
 # Copyright (C) 2007-2008 kaoru
 
 LANG=C
 
 for f in ` \
-    find bobura bobura_model tetengo2 precompiled docsrc \
-    -type f -name '*.h' -or -name '*.cpp' -or -name '*.page' \
+    find . bobura bobura_model tetengo2 precompiled docsrc \
+        -type f \
+        -name '*.h' -or \
+        -name '*.cpp' -or \
+        -name '*.page' -or \
+        -name 'Doxyfile' -or \
+        -name 'Doxyfile.test' \
 `; \
 do
-    echo 'Setting to '$f
-    chmod 666 $f;
+    echo 'Setting to '$f; \
+    chmod 644 $f; \
     svn propdel -q svn:executable $f; \
+    svn propset -q svn:keywords 'Author Date Rev Id' $f; \
+done
+
+for f in ` \
+    find . bobura bobura_model tetengo2 \
+        -type f \
+        -name 'bobura.sln' -or \
+        -name 'lauch_vsee.bat' -or \
+        -name 'set_svn_props.sh'
+`; \
+do
+    echo 'Setting to '$f; \
+    chmod 755 $f; \
+    svn propset -q svn:executable '*' $f; \
     svn propset -q svn:keywords 'Author Date Rev Id' $f; \
 done
