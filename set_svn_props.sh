db43c3f0ba87bde64c7916ee85f918189169886f
kaoru <kaorut@mc.pikara.ne.jp>
2008-02-18 23:21:42 +0000

    Modified set_svn_props.sh. Modified svn properties.
    git-svn-id: file:///home/kaoru/svnrepos/bobura/trunk@180 5a212d1d-074f-4316-8284-043cf949738a

--- set_svn_props.sh
+++ set_svn_props.sh
@@ -6,32 +6,52 @@
 
 LANG=C
 
-for f in ` \
-    find . bobura bobura_model tetengo2 precompiled docsrc \
-        -type f \
-        -name '*.h' -or \
-        -name '*.cpp' -or \
-        -name '*.page' -or \
-        -name 'Doxyfile' -or \
-        -name 'Doxyfile.test' -or \
-        -name 'bobura_config.h-original' \
-`; \
-do
-    echo 'Setting to '$f; \
-    chmod 644 $f; \
-    svn propdel -q svn:executable $f; \
-    svn propset -q svn:keywords 'Author Date Rev Id' $f; \
-done
+set_prop_for_ordinal()
+{
+    echo 'Setting for '$1; \
+    chmod 644 $1; \
+    svn propdel -q svn:executable $1; \
+    svn propset -q svn:keywords 'Author Date Rev Id' $1; \
+}
+
+set_prop_for_executable()
+{
+    echo 'Setting for '$1; \
+    chmod 755 $1; \
+    svn propset -q svn:executable '*' $1; \
+    svn propset -q svn:keywords 'Author Date Rev Id' $1; \
+}
 
 for f in ` \
-    find . bobura bobura_model tetengo2 \
+    find \
+        bobura/src bobura/test \
+        bobura_model/include bobura_model/test \
+        tetengo2/include tetengo2/test \
+        precompiled \
+        -maxdepth 1 \
         -type f \
-        -name 'bobura.sln' -or \
-        -name 'set_svn_props.sh'
+        -name '*.h' -or \
+        -name '*.cpp' \
 `; \
 do
-    echo 'Setting to '$f; \
-    chmod 755 $f; \
-    svn propset -q svn:executable '*' $f; \
-    svn propset -q svn:keywords 'Author Date Rev Id' $f; \
+    set_prop_for_ordinal $f;
 done
+for f in `find docsrc -maxdepth 1 -type f -name '*.page'`; \
+do
+    set_prop_for_ordinal $f;
+done
+set_prop_for_ordinal bobura_config.h-original
+set_prop_for_ordinal Doxyfile
+set_prop_for_ordinal Doxyfile.test
+
+set_prop_for_executable bobura.sln
+for f in ` \
+    find . bobura bobura_model tetengo2 \
+        -maxdepth 1 \
+        -type f \
+        -name '*.vcproj' \
+`; \
+do
+    set_prop_for_executable $f;
+done
+set_prop_for_executable set_svn_props.sh
