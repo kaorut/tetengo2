705dc6cd7c787e7ad4d03c76f3df9dc2afe070d4
kaoru <kaorut@mc.pikara.ne.jp>
2008-02-20 08:38:46 +0000

    Implemented bobura::model::timetable_info::StationAndMetersConcept.
    git-svn-id: file:///home/kaoru/svnrepos/bobura/trunk@193 5a212d1d-074f-4316-8284-043cf949738a

--- bobura_model/include/bobura.model.timetable_info.StationAndMetersConcept.h
+++ bobura_model/include/bobura.model.timetable_info.StationAndMetersConcept.h
@@ -33,6 +33,15 @@ namespace bobura { namespace model { namespace timetable_info
             typedef typename StationAndMeters::station_type station_type;
             typedef typename StationAndMeters::size_type size_type;
 
+            m_station_and_meters.swap(m_station_and_meters);
+            std::swap(m_station_and_meters, m_station_and_meters);
+
+            StationAndMeters& station_and_meters =
+                m_station_and_meters = m_station_and_meters;
+
+            station_type& station = m_station_and_meters.station();
+            boost::ignore_unused_variable_warning(station);
+
             const_constraints(m_station_and_meters);
         }
 
@@ -41,9 +50,13 @@ namespace bobura { namespace model { namespace timetable_info
         */
         void const_constraints(const StationAndMeters& station_and_meters)
         {
-            //const typename StationAndMeters::string_type name =
-            //    station_and_meters.name();
-            //boost::ignore_unused_variable_warning(name);
+            const typename StationAndMeters::station_type& station =
+                station_and_meters.station();
+            boost::ignore_unused_variable_warning(station);
+
+            const typename StationAndMeters::size_type& meters =
+                station_and_meters.meters();
+            boost::ignore_unused_variable_warning(meters);
         }
 
 
