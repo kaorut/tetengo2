fe25d14c0cc368a1766b4486940c65e07afc650b
kaoru <kaorut@mc.pikara.ne.jp>
2009-02-18 16:20:40 +0000

    Created copy_boost_dlls.js.
    git-svn-id: file:///home/kaoru/svnrepos/bobura/trunk@390 5a212d1d-074f-4316-8284-043cf949738a

---
+++ tools/copy_boost_dlls.js
@@ -0,0 +1,128 @@
+/*
+	Boost DLL Copier
+	
+	Copyright (C) 2007-2009 kaoru
+	
+	$Id$
+*/
+
+function trim(string)
+{
+	var trimmed = string;
+	
+	var character_offset = trimmed.search(/[^ \t]/);
+	if (character_offset == -1) return "";
+		trimmed = trimmed.substring(character_offset);
+	var whitespace_offset = trimmed.search(/[ \t]+$/);
+	if (whitespace_offset >= 0)
+		trimmed = trimmed.substring(0, whitespace_offset);
+	
+	return trimmed;
+}
+
+function parseLine(line, pattern)
+{
+	var trimmed = trim(line);
+	
+	var found_define_lib = trimmed.match(pattern);
+	if (found_define_lib == null) return null;
+	
+	var key_value = trimmed.substring(found_define_lib.lastIndex);
+	var found_whitespace = key_value.match(/\s+/);
+	if (found_whitespace == null)
+	{
+		WScript.Echo("Syntax Error: " + trimmed);
+		return null;
+	}
+	
+	var key = key_value.substring(0, found_whitespace.index);
+	var value = trim(key_value.substring(found_whitespace.lastIndex));
+	value = value.replace(/\\\\/g, "\\");
+	value = value.replace(/\"/g, "");
+	return new Array(key, value);
+}
+
+function getSourceAndVersion()
+{
+	var source = new Array;
+	var version = null;
+	
+	var filesystem = WScript.CreateObject("Scripting.FileSystemObject");
+	var stream = filesystem.OpenTextFile("..\\bobura_config.h", 1, false, 0);
+	while (!stream.AtEndOfStream)
+	{
+		var line = stream.ReadLine();
+		
+		var parsed1 = parseLine(line, /^#\s*define\s+BOOST_LIB_/);
+		if (parsed1 != null)
+		{
+			source[parsed1[0]] = parsed1[1];
+			continue;
+		}
+		
+		var parsed2 = parseLine(line, /^#\s*define\s+BOOST_VERSION/);
+		if (parsed2 != null)
+		{
+			version = parsed2[1];
+			continue;
+		}
+	}
+	stream.Close();
+	
+	return new Array(source, version);
+}
+
+function getDestination(solutionDirectory, releaseOrDebug)
+{
+	var filesystem = WScript.CreateObject("Scripting.FileSystemObject");
+	
+	return filesystem.BuildPath(solutionDirectory, "bin\\" + releaseOrDebug);
+}
+
+function getDllNames(version, debug)
+{
+	var dllNames = new Array;
+	
+	dllNames.push("boost_signals-*-mt-" + (debug ? "gd-" : "") + version + ".dll");
+	
+	return dllNames;
+}
+
+function copyFile(sourceDirectory, destinationDirectory, fileName)
+{
+	var filesystem = WScript.CreateObject("Scripting.FileSystemObject");
+	
+	var sourceFile = filesystem.BuildPath(sourceDirectory, fileName);
+	
+	WScript.Echo(
+		"Copying \"" + sourceFile + "\" to \"" + destinationDirectory +
+		"\" ..."
+	);
+	filesystem.CopyFile(sourceFile, destinationDirectory + "\\");
+}
+
+
+// main
+
+if (WScript.Arguments.length < 3)
+{
+	WScript.Echo(
+		"Usage: cscript copy_boost_dlls.js SOLUTION_DIRECTORY RELEASE_OR_DEBUG PLATFORM"
+	);
+	WScript.Quit(1);
+}
+var solutionDirectory = WScript.Arguments(0);
+var releaseOrDebug = WScript.Arguments(1);
+var platform = WScript.Arguments(2);
+
+var sourceAndVersion = getSourceAndVersion();
+var destination = getDestination(solutionDirectory, releaseOrDebug);
+
+var sourceDirectory = sourceAndVersion[0][platform];
+var destinationDirectory = destination + "." + platform;
+
+var dllNames =
+	getDllNames(sourceAndVersion[1], releaseOrDebug.toLowerCase() == "debug");
+for (var i = 0; i < dllNames.length; ++i)
+	copyFile(sourceDirectory, destinationDirectory, dllNames[i]);
+
