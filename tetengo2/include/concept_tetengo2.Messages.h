87c5cf6476032e8eaf4f0bf71d793e0ef28d2c3a
kaoru <kaorut@mc.pikara.ne.jp>
2009-05-11 13:21:33 +0000

    Created concept_tetengo2::Messages.
    git-svn-id: file:///home/kaoru/svnrepos/bobura/trunk@488 5a212d1d-074f-4316-8284-043cf949738a

---
+++ tetengo2/include/concept_tetengo2.Messages.h
@@ -0,0 +1,74 @@
+/*! \file
+    \brief The definition of concept_tetengo2::Messages.
+
+    Copyright (C) 2007-2009 kaoru
+
+    $Id$
+*/
+
+#if !defined(CONCEPTTETENGO2_MESSAGES_H)
+#define CONCEPTTETENGO2_MESSAGES_H
+
+#include <locale>
+#include <string>
+
+#include <boost/concept_check.hpp>
+
+
+namespace concept_tetengo2
+{
+    /*!
+        \brief The concept check class template for a messages facet.
+
+        \tparam Type A type.
+    */
+    template <typename Type>
+    class Messages
+    {
+#if !defined(DOCUMENTATION)
+    public:
+        // typedef checks
+
+        typedef typename Type::catalog catalog;
+
+        typedef typename Type::char_type char_type;
+
+        typedef typename Type::string_type string_type;
+
+        typedef typename Type::byte_type byte_type;
+
+
+        // usage checks
+
+        BOOST_CONCEPT_USAGE(Messages)
+        {
+            const_constraints(m_object);
+        }
+
+        void const_constraints(const Type& object)
+        {
+            const catalog catalog_value =
+                object.open(std::string(), std::locale());
+            boost::ignore_unused_variable_warning(catalog_value);
+
+            const string_type text =
+                object.get(catalog(), 0, 0, string_type());
+            boost::ignore_unused_variable_warning(text);
+
+            object.close(catalog());
+        }
+
+
+    private:
+        // variables
+
+        Type m_object;
+
+
+#endif
+    };
+
+
+}
+
+#endif
