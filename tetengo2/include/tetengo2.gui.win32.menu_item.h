4cae7b9ecb86592b6c9ff49eaad04e73411a7186
kaoru <kaorut@mc.pikara.ne.jp>
2010-01-14 19:01:18 +0900

    Implemented tetengo2::gui::win32::menu_item.

--- tetengo2/include/tetengo2.gui.win32.menu_item.h
+++ tetengo2/include/tetengo2.gui.win32.menu_item.h
@@ -19,6 +19,10 @@
 #include <boost/signal.hpp>
 #include <boost/ptr_container/ptr_vector.hpp>
 
+#define NOMINMAX
+#define OEMRESOURCE
+#include <windows.h>
+
 #include "concept_tetengo2.String.h"
 #include "concept_tetengo2.gui.Handle.h"
 #include "concept_tetengo2.gui.MenuObserver.h"
@@ -102,10 +106,12 @@ namespace tetengo2 { namespace gui { namespace win32
         typedef MenuObserver menu_observer_type;
 
         //! The menu item iterator type.
-        typedef menu_item* iterator;
+        typedef typename boost::ptr_vector<menu_item>::iterator iterator;
 
         //! The const menu item iterator type.
-        typedef const menu_item* const_iterator;
+        typedef
+            typename boost::ptr_vector<menu_item>::const_iterator
+            const_iterator;
 
 
         // constructors and destructor
@@ -228,7 +234,7 @@ namespace tetengo2 { namespace gui { namespace win32
         const_iterator begin()
         const
         {
-            return NULL;
+            return m_children.begin();
         }
 
         /*!
@@ -238,7 +244,7 @@ namespace tetengo2 { namespace gui { namespace win32
         */
         iterator begin()
         {
-            return NULL;
+            return m_children.begin();
         }
 
         /*!
@@ -249,7 +255,7 @@ namespace tetengo2 { namespace gui { namespace win32
         const_iterator end()
         const
         {
-            return NULL;
+            return m_children.end();
         }
 
         /*!
@@ -259,7 +265,7 @@ namespace tetengo2 { namespace gui { namespace win32
         */
         iterator end()
         {
-            return NULL;
+            return m_children.end();
         }
 
         /*!
@@ -334,7 +340,16 @@ namespace tetengo2 { namespace gui { namespace win32
             std::auto_ptr<menu_item> p_menu_item
         )
         {
+            if (p_menu_item.get() == NULL)
+            {
+                throw std::invalid_argument(
+                    "The auto pointer to a menu item is NULL."
+                );
+            }
 
+            insert_native_menu_item(offset, *p_menu_item);
+
+            m_children.insert(offset, p_menu_item);
         }
 
         /*!
@@ -364,7 +379,8 @@ namespace tetengo2 { namespace gui { namespace win32
         m_handle(handle),
         m_text(text),
         m_menu_observers(),
-        m_menu_selected_handler()
+        m_menu_selected_handler(),
+        m_children()
         {}
 
 
@@ -390,6 +406,145 @@ namespace tetengo2 { namespace gui { namespace win32
         boost::ptr_vector<menu_observer_type> m_menu_observers;
 
         boost::signal<void ()> m_menu_selected_handler;
+
+        boost::ptr_vector<menu_item> m_children;
+
+
+        // functions
+
+        void insert_native_menu_item(
+            const_iterator offset,
+            menu_item&     menu_item
+        )
+        const
+        {
+            std::vector< ::WCHAR> duplicated_text =
+                duplicate_text(menu_item.text());
+
+            ::MENUITEMINFOW menu_item_info;
+            std::memset(&menu_item_info, 0, sizeof(::MENUITEMINFO));
+            menu_item_info.cbSize = sizeof(::MENUITEMINFO);
+
+            if      (menu_item.is_command())
+            {
+                set_menu_item_info_for_command(
+                    menu_item, menu_item_info, duplicated_text
+                );
+            }
+            else if (menu_item.is_popup())
+            {
+                set_menu_item_info_for_popup(
+                    menu_item, menu_item_info, duplicated_text
+                );
+            }
+            else if (menu_item.is_separator())
+            {
+                set_menu_item_info_for_separator(menu_item, menu_item_info);
+            }
+            else
+            {
+                set_menu_item_info_for_unknown(
+                    menu_item, menu_item_info, duplicated_text
+                );
+            }
+
+            const ::BOOL result = ::InsertMenuItem(
+                m_handle,
+                static_cast< ::UINT>(
+                    std::distance(m_children.begin(), offset)
+                ),
+                TRUE,
+                &menu_item_info
+            );
+            if (result == 0)
+            {
+                throw std::runtime_error(
+                    "Can't insert a native menu command."
+                );
+            }
+        }
+
+        const std::vector< ::WCHAR> duplicate_text(const string_type& text)
+        const
+        {
+            const std::wstring native_string = encode_to_native_type()(text);
+
+            std::vector< ::WCHAR> duplicated;
+            duplicated.reserve(native_string.length() + 1);
+            std::copy(
+                native_string.begin(),
+                native_string.end(),
+                std::back_inserter(duplicated)
+            );
+            duplicated.push_back(L'\0');
+
+            return duplicated;
+        }
+
+        void set_menu_item_info_for_command(
+            menu_item&             menu_item,
+            ::MENUITEMINFOW&       menu_item_info,
+            std::vector< ::WCHAR>& text
+        )
+        const
+        {
+            menu_item_info.fMask = MIIM_STRING | MIIM_ID;
+            menu_item_info.dwTypeData = &text[0];
+            menu_item_info.wID = menu_item.id();
+        }
+
+        void set_menu_item_info_for_popup(
+            menu_item&             menu_item,
+            ::MENUITEMINFOW&       menu_item_info,
+            std::vector< ::WCHAR>& text
+        )
+        const
+        {
+            menu_item_info.fMask = MIIM_STRING | MIIM_SUBMENU;
+            menu_item_info.dwTypeData = &text[0];
+            menu_item_info.hSubMenu = menu_item.handle();
+        }
+
+        void set_menu_item_info_for_separator(
+            menu_item&       menu_item,
+            ::MENUITEMINFOW& menu_item_info
+        )
+        const
+        {
+            menu_item_info.fMask = MIIM_FTYPE;
+            menu_item_info.fType = MFT_SEPARATOR;
+        }
+
+        void set_menu_item_info_for_unknown(
+            menu_item&             menu_item,
+            ::MENUITEMINFOW&       menu_item_info,
+            std::vector< ::WCHAR>& text
+        )
+        const
+        {
+            menu_item_info.fMask = MIIM_STRING | MIIM_STATE;
+            menu_item_info.dwTypeData = &text[0];
+            menu_item_info.fState = MFS_DISABLED;
+        }
+
+        void erase_native_menus(const_iterator first, const_iterator last)
+        const
+        {
+            for (const_iterator i = first; i != last; ++i)
+                erase_native_menu(i);
+        }
+
+        void erase_native_menu(const_iterator offset)
+        const
+        {
+            ::RemoveMenu(
+                m_handle,
+                static_cast< ::UINT>(
+                    std::distance(m_children.begin(), offset)
+                ),
+                MF_BYPOSITION
+            );
+        }
 
 
     };
