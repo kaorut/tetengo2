50d5b4a7370600ea98cbef06a5fd1b38b1dceae7
kaoru <kaorut@mc.pikara.ne.jp>
2010-02-15 19:04:10 +0900

    Implemented tetengo2::encoder.

--- tetengo2/include/tetengo2.encoder.h
+++ tetengo2/include/tetengo2.encoder.h
@@ -9,6 +9,8 @@
 #if !defined(TETENGO2_ENCODER_H)
 #define TETENGO2_ENCODER_H
 
+#include "tetengo2.assignable.h"
+
 
 namespace tetengo2
 {
@@ -19,8 +21,13 @@ namespace tetengo2
         \tparam ExternalEncoding An external encoding type.
     */
     template <typename InternalEncoding, typename ExternalEncoding>
-    class encoder
+    class encoder :
+        public assignable<encoder<InternalEncoding, ExternalEncoding> >
     {
+    private:
+        // concept checks
+
+
     public:
         // types
 
@@ -43,10 +50,32 @@ namespace tetengo2
 
         /*!
             \brief Creates an encoder.
+
+            \param internal_encoding An internal encoding.
+            \param external_encoding An external encoding.
         */
-        encoder()
+        explicit encoder(
+            const internal_encoding_type& internal_encoding =
+                internal_encoding_type(),
+            const external_encoding_type& external_encoding =
+                external_encoding_type()
+        )
+        :
+        m_internal_encoding(internal_encoding),
+        m_external_encoding(external_encoding)
         {}
 
+        /*!
+            \brief Copies an encoder.
+
+            \param another Another encoder;
+        */
+        encoder(const encoder& another)
+        :
+        m_internal_encoding(another.m_internal_encoding),
+        m_external_encoding(another.m_external_encoding)
+        {}
+        
         /*!
             \brief Destroys the encoder.
         */
@@ -58,6 +87,30 @@ namespace tetengo2
         // functions
 
         /*!
+            \brief Swaps the members with another encoder.
+
+            \param another Another encoder.
+        */
+        void swap(encoder& another)
+        throw ()
+        {
+            m_internal_encoding.swap(another.m_internal_encoding);
+            m_external_encoding.swap(another.m_external_encoding);
+        }
+
+        /*!
+            \brief Assigns another encoder.
+
+            \param another Another encoder.
+
+            \return this object.
+        */
+        encoder& operator=(const encoder& another)
+        {
+            return assign(another);
+        }
+
+        /*!
             \brief Encodes a string.
 
             \param string A string
@@ -67,7 +120,9 @@ namespace tetengo2
         external_string_type encode(const internal_string_type& string)
         const
         {
-            
+            return m_external_encoding.from_pivot(
+                m_internal_encoding.to_pivot(string)
+            );
         }
 
         /*!
@@ -80,12 +135,18 @@ namespace tetengo2
         internal_string_type decode(const external_string_type& string)
         const
         {
-
+            return m_internal_encoding.from_pivot(
+                m_external_encoding.to_pivot(string)
+            );
         }
 
 
     private:
         // variables
+
+        internal_encoding_type m_internal_encoding;
+
+        external_encoding_type m_external_encoding;
 
 
     };
