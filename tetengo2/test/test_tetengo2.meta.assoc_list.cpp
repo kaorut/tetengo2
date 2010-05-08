3b7c15bf29c2250daf9081dc9de199043820d8d8
kaoru <kaorut@mc.pikara.ne.jp>
2010-05-09 08:21:19 +0900

    Created tetengo2::meta::assoc_list.

---
+++ tetengo2/test/test_tetengo2.meta.assoc_list.cpp
@@ -0,0 +1,22 @@
+/*! \file
+    \brief Test of class tetengo2::meta::assoc_list.
+
+    Copyright (C) 2007-2010 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include "tetengo2.meta.assoc_list.h"
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo2)
+BOOST_AUTO_TEST_SUITE(meta)
+BOOST_AUTO_TEST_SUITE(assoc_list)
+    // test cases
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
