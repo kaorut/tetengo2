579d7ea1c353367ec67a36a8758f52b9aa56127c
kaoru <kaorut@mc.pikara.ne.jp>
2010-07-30 07:34:55 +0900

    Created tetengo2::gui::paint_observer_set.

---
+++ tetengo2/include/tetengo2.gui.paint_observer_set.h
@@ -0,0 +1,98 @@
+/*! \file
+    \brief The definition of tetengo2::gui::paint_observer_set.
+
+    Copyright (C) 2007-2010 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_GUI_PAINTOBSERVERSET_H)
+#define TETENGO2_GUI_PAINTOBSERVERSET_H
+
+#include <boost/noncopyable.hpp>
+#include <boost/signals2.hpp>
+
+
+namespace tetengo2 { namespace gui
+{
+    /*!
+        \brief The class template for a paint observer set.
+
+        \tparam Canvas A canvas type.
+    */
+    template <typename Canvas>
+    class paint_observer_set : private boost::noncopyable
+    {
+    public:
+        // types
+
+        //! \return The canvas type.
+        typedef Canvas canvas_type;
+
+        /*!
+            \brief The observer type of paint.
+
+            \param canvas A canvas.
+        */
+        typedef void paint_type(canvas_type& canvas);
+
+        //! \return The signal type of paint.
+        typedef boost::signals2::signal<paint_type> paint_signal_type;
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a paint observer set.
+        */
+        paint_observer_set()
+        :
+        m_paint()
+        {}
+
+        /*!
+            \brief Destroys the paint observer set.
+        */
+        ~paint_observer_set()
+        throw ()
+        {}
+
+
+        // functions
+
+        /*!
+            \brief Returns the observer called when a canvas needs to be
+                   painted.
+
+            \return The observer called when a canvas needs to be painted.
+        */
+        const paint_signal_type& paint()
+        const
+        {
+            return m_paint;
+        }
+
+        /*!
+            \brief Returns the observer called when a canvas needs to be
+                   painted.
+
+            \return The observer called when a canvas needs to be painted.
+        */
+        paint_signal_type& paint()
+        {
+            return m_paint;
+        }
+
+
+    private:
+        // variables
+
+        paint_signal_type m_paint;
+
+
+    };
+
+
+}}
+
+#endif
---
+++ tetengo2/test/test_tetengo2.gui.paint_observer_set.cpp
@@ -0,0 +1,78 @@
+/*! \file
+    \brief Test of class tetengo2::gui::paint_observer_set.
+
+    Copyright (C) 2007-2010 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include "stub_tetengo2.gui.font.h"
+#include "stub_tetengo2.gui.canvas.h"
+#include "tetengo2.encoder.h"
+#include "tetengo2.encoding.locale.h"
+#include "tetengo2.gui.paint_observer_set.h"
+
+
+namespace
+{
+    // types
+
+    typedef tetengo2::encoding::locale<std::wstring> internal_encoding_type;
+
+    typedef tetengo2::encoding::locale<std::wstring> ui_encoding_type;
+
+    typedef
+        tetengo2::encoder<internal_encoding_type, ui_encoding_type>
+        ui_encoder_type;
+
+    typedef stub_tetengo2::gui::font<std::wstring, std::size_t> font_type;
+
+    typedef
+        stub_tetengo2::gui::canvas<
+            const void*,
+            std::size_t,
+            std::wstring,
+            ui_encoder_type,
+            const void*,
+            font_type
+        >
+        canvas_type;
+
+
+}
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo2)
+BOOST_AUTO_TEST_SUITE(gui)
+BOOST_AUTO_TEST_SUITE(paint_observer_set)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const tetengo2::gui::paint_observer_set<canvas_type> observer_set;
+    }
+
+    BOOST_AUTO_TEST_CASE(destroyed)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const tetengo2::gui::paint_observer_set<canvas_type> observer_set;
+
+            observer_set.paint();
+        }
+        {
+            tetengo2::gui::paint_observer_set<canvas_type> observer_set;
+
+            observer_set.paint();
+        }
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
