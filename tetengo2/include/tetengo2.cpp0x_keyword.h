7b7bb64de12e062f3ba0217ab011a87dfe4e4065
kaoru <kaorut@mc.pikara.ne.jp>
2010-09-08 07:35:19 +0900

    Created tetengo2.cpp0x_keyword.h.

---
+++ tetengo2/include/tetengo2.cpp0x_keyword.h
@@ -0,0 +1,16 @@
+/*! \file
+    \brief The definition of C++0x keywords for tetengo2.
+
+    Copyright (C) 2007-2010 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_CPP0XKEYWORD_H)
+#define TETENGO2_CPP0XKEYWORD_H
+
+//! The alternative to the C++0x keyword noexcept.
+#define TETENGO2_NOEXCEPT throw ()
+
+
+#endif
