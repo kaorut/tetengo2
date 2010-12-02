8288e45f49ceadb3bed038678567588eeeca0239
kaoru <kaorut@mc.pikara.ne.jp>
2010-12-03 05:35:39 +0900

    Created tetengo2::gui::unit::pixel.

---
+++ tetengo2/include/tetengo2.gui.unit.pixel.h
@@ -0,0 +1,152 @@
+/*! \file
+    \brief The definition of tetengo2::gui::unit::pixel.
+
+    Copyright (C) 2007-2010 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_GUI_UNIT_PIXEL_H)
+#define TETENGO2_GUI_UNIT_PIXEL_H
+
+#include <boost/operators.hpp>
+#include <boost/swap.hpp>
+
+#include <tetengo2.operators.h>
+
+
+namespace tetengo2 { namespace gui { namespace unit
+{
+    /*!
+        \brief The class template for a pixel unit.
+ 
+        \tparam Value      A value type.
+        \tparam PixelValue A pixel value type.
+   */
+    template <typename Value, typename PixelValue>
+    class pixel :
+        private boost::totally_ordered<pixel<Value, PixelValue>>,
+        private tetengo2::additive<pixel<Value, PixelValue>>
+    {
+    public:
+        // types
+
+        //! The value type.
+        typedef Value value_type;
+
+        //! The pixel value type.
+        typedef PixelValue pixel_value_type;
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a pixel unit.
+
+            \param value A value.
+        */
+        explicit pixel(const value_type value)
+        :
+        m_value(value)
+        {}
+
+
+        // functions
+
+        /*!
+            \brief Adds another pixel unit.
+
+            \param another Another pixel unit.
+
+            \return This object.
+        */
+        pixel& operator+=(const pixel& another)
+        {
+            pixel temp(*this);
+
+            temp.m_value += another.m_value;
+            
+            boost::swap(temp, *this);
+            return *this;
+        }
+
+        /*!
+            \brief Subtracts another pixel unit.
+
+            \param another Another pixel unit.
+
+            \return This object.
+        */
+        pixel& operator-=(const pixel& another)
+        {
+            pixel temp(*this);
+
+            temp.m_value -= another.m_value;
+            
+            boost::swap(temp, *this);
+            return *this;
+        }
+
+        /*!
+            \brief Checks whether one pixel unit is equal to anther.
+
+            \param one     One pixel unit.
+            \param another Another pixel unit.
+
+            \retval true  When the one is equal to the other.
+            \retval false Otherwise.
+        */
+        friend bool operator==(const pixel& one, const pixel& another)
+        {
+            return one.m_value == another.m_value;
+        }
+
+        /*!
+            \brief Checks whether one pixel unit is less than anther.
+
+            \param one     One pixel unit.
+            \param another Another pixel unit.
+
+            \retval true  When the one is less than the other.
+            \retval false Otherwise.
+        */
+        friend bool operator<(const pixel& one, const pixel& another)
+        {
+            return one.m_value < another.m_value;
+        }
+
+        /*!
+            \brief Returns the value.
+
+            \return The value.
+        */
+        value_type value()
+        const
+        {
+            return m_value;
+        }
+
+        /*!
+            \brief Returns the value in pixels.
+
+            \return The value in pixels.
+        */
+        pixel_value_type to_pixels()
+        const
+        {
+            return m_value;
+        }
+
+
+    private:
+        // variables
+
+        value_type m_value;
+
+
+    };
+
+
+}}}
+
+#endif
---
+++ tetengo2/test/test_tetengo2.gui.unit.pixel.cpp
@@ -0,0 +1,109 @@
+/*! \file
+    \brief Test of class tetengo2::gui::unit::pixel.
+
+    Copyright (C) 2007-2010 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include "tetengo2.gui.unit.pixel.h"
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo2)
+BOOST_AUTO_TEST_SUITE(gui)
+BOOST_AUTO_TEST_SUITE(unit)
+BOOST_AUTO_TEST_SUITE(pixel)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const tetengo2::gui::unit::pixel<int, int> unit(123);
+    }
+
+    BOOST_AUTO_TEST_CASE(operator_plus_assign)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        tetengo2::gui::unit::pixel<int, int> unit1(456);
+        const tetengo2::gui::unit::pixel<int, int> unit2(123);
+
+        unit1 += unit2;
+
+        BOOST_CHECK_EQUAL(unit1.value(), 579);
+    }
+
+    BOOST_AUTO_TEST_CASE(operator_minus_assign)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        tetengo2::gui::unit::pixel<int, int> unit1(456);
+        const tetengo2::gui::unit::pixel<int, int> unit2(123);
+
+        unit1 -= unit2;
+
+        BOOST_CHECK_EQUAL(unit1.value(), 333);
+    }
+
+    BOOST_AUTO_TEST_CASE(operator_equal)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const tetengo2::gui::unit::pixel<int, int> unit1(123);
+            const tetengo2::gui::unit::pixel<int, int> unit2(123);
+
+            BOOST_CHECK(unit1 == unit2);
+        }
+        {
+            const tetengo2::gui::unit::pixel<int, int> unit1(456);
+            const tetengo2::gui::unit::pixel<int, int> unit2(123);
+
+            BOOST_CHECK(unit1 != unit2);
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operator_less)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const tetengo2::gui::unit::pixel<int, int> unit1(123);
+            const tetengo2::gui::unit::pixel<int, int> unit2(456);
+
+            BOOST_CHECK(unit1 < unit2);
+        }
+        {
+            const tetengo2::gui::unit::pixel<int, int> unit1(456);
+            const tetengo2::gui::unit::pixel<int, int> unit2(123);
+
+            BOOST_CHECK(unit1 > unit2);
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(value)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const tetengo2::gui::unit::pixel<int, int> unit(123);
+
+        BOOST_CHECK_EQUAL(unit.value(), 123);
+    }
+
+    BOOST_AUTO_TEST_CASE(to_pixels)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const tetengo2::gui::unit::pixel<int, int> unit(123);
+
+        BOOST_CHECK_EQUAL(unit.to_pixels(), 123);
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
