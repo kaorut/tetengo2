9492c80da286b49769c7bf0f2dbf11eebf79f689
kaoru <kaorut@mc.pikara.ne.jp>
2010-12-10 20:24:43 +0900

    Created tetengo2.gui.measure.h.

---
+++ tetengo2/include/tetengo2.gui.measure.h
@@ -0,0 +1,20 @@
+/*! \file
+    \brief The definition of tetengo2::gui::measure.
+
+    Copyright (C) 2007-2010 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_GUI_MEASURE_H)
+#define TETENGO2_GUI_MEASURE_H
+
+
+namespace tetengo2 { namespace gui
+{
+
+
+}}
+
+
+#endif
---
+++ tetengo2/test/test_tetengo2.gui.measure.cpp
@@ -0,0 +1,27 @@
+/*! \file
+    \brief Test of class tetengo2::gui::measure.
+
+    Copyright (C) 2007-2010 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include "tetengo2.gui.measure.h"
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo2)
+BOOST_AUTO_TEST_SUITE(gui)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(to_pixels)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        // TODO
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
