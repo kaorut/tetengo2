34dd2c364f186c369b9c71f1e06782a52f9c9a4a
kaoru <kaorut@mc.pikara.ne.jp>
2010-12-22 22:32:04 +0900

    Created tetengo2.gui.win32.detail.windows_version.h.

---
+++ tetengo2/include/tetengo2.gui.win32.detail.windows_version.h
@@ -0,0 +1,24 @@
+/*! \file
+    \brief The definition of tetengo2::gui::win32::detail::windows_version.
+
+    Copyright (C) 2007-2010 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_GUI_WIN32_DETAIL_WINDOWSVERSION_H)
+#define TETENGO2_GUI_WIN32_DETAIL_WINDOWSVERSION_H
+
+#define NOMINMAX
+#define OEMRESOURCE
+#include <windows.h>
+
+
+namespace tetengo2 { namespace gui { namespace win32 { namespace detail
+{
+
+
+}}}}
+
+
+#endif
