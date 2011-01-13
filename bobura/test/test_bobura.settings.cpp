29922fa684ca4a48d54349a57df968f37949f823
kaoru <kaorut@mc.pikara.ne.jp>
2011-01-13 20:22:47 +0900

    Implemented test_bobura::settings.

--- bobura/test/test_bobura.settings.cpp
+++ bobura/test/test_bobura.settings.cpp
@@ -31,6 +31,50 @@ BOOST_AUTO_TEST_SUITE(settings)
         settings(std::move(arguments), std::move(path));
     }
 
+    BOOST_AUTO_TEST_CASE(base_path)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        std::vector<std::wstring> arguments;
+        boost::filesystem::path path(L"path/to");
+        const bobura::settings<std::wstring, boost::filesystem::path>
+        settings(std::move(arguments), std::move(path));
+
+        BOOST_CHECK(
+            settings.base_path() == boost::filesystem::path(L"path/to")
+        );
+    }
+
+    BOOST_AUTO_TEST_CASE(message_directory_path)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        std::vector<std::wstring> arguments;
+        boost::filesystem::path path(L"path/to");
+        const bobura::settings<std::wstring, boost::filesystem::path>
+        settings(std::move(arguments), std::move(path));
+
+        BOOST_CHECK(
+            settings.message_directory_path() ==
+            boost::filesystem::path(L"path/to/messages")
+        );
+    }
+
+    BOOST_AUTO_TEST_CASE(image_directory_path)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        std::vector<std::wstring> arguments;
+        boost::filesystem::path path(L"path/to");
+        const bobura::settings<std::wstring, boost::filesystem::path>
+        settings(std::move(arguments), std::move(path));
+
+        BOOST_CHECK(
+            settings.image_directory_path() ==
+            boost::filesystem::path(L"path/to/images")
+        );
+    }
+
 
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
