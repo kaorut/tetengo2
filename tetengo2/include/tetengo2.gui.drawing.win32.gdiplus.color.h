e56287de03e51d1b428749b7870361f89f2cbdcb
kaoru <kaorut@mc.pikara.ne.jp>
2011-01-17 21:17:04 +0900

    Created tetengo2::gui::drawing::win32::gdiplus::color.

---
+++ tetengo2/include/tetengo2.gui.drawing.win32.gdiplus.color.h
@@ -0,0 +1,154 @@
+/*! \file
+    \brief The definition of tetengo2::gui::drawing::win32::gdiplus::color.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_GUI_DRAWING_WIN32_GDIPLUS_COLOR_H)
+#define TETENGO2_GUI_DRAWING_WIN32_GDIPLUS_COLOR_H
+
+#include <limits>
+
+#include <boost/operators.hpp>
+
+#define NOMINMAX
+#define OEMRESOURCE
+#include <Windows.h>
+#if !defined(min) && !defined(DOCUMENTATION)
+#   define min(a, b) ((a) < (b) ? (a) : (b))
+#endif
+#if !defined(max) && !defined(DOCUMENTATION)
+#   define max(a, b) ((a) > (b) ? (a) : (b))
+#endif
+#include <GdiPlus.h>
+#undef min
+#undef max
+
+
+namespace tetengo2 { namespace gui { namespace drawing { namespace win32 {
+namespace gdiplus
+{
+    /*!
+        \brief The class template for a color for Win32 platforms.
+
+        \tparam Value A value type.
+    */
+    template <typename Value>
+    class color : private boost::equality_comparable<color<Value>>
+    {
+    public:
+        // types
+
+        //! The value type.
+        typedef Value value_type;
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a color.
+
+            \param red   A red value.
+            \param green A green value.
+            \param blue  A blue value.
+            \param alpha An alpha channel value.
+        */
+        color(
+            const value_type red,
+            const value_type green,
+            const value_type blue,
+            const value_type alpha = std::numeric_limits<value_type>::max()
+        )
+        :
+        m_red(red),
+        m_green(green),
+        m_blue(blue),
+        m_alpha(alpha)
+        {}
+
+
+        // functions
+
+        /*!
+            \brief Checks whether one color is equal to another.
+
+            \param one     One color.
+            \param another Another color.
+
+            \retval true  When the one is equal to the other.
+            \retval false Otherwise.
+        */
+        friend bool operator==(const color& one, const color& another)
+        {
+            return one.m_red == another.m_red &&
+                   one.m_green == another.m_green &&
+                   one.m_blue == another.m_blue &&
+                   one.m_alpha == another.m_alpha;
+        }
+
+        /*!
+            \brief Returns the red value.
+
+            \return The red value.
+        */
+        value_type red()
+        const
+        {
+            return m_red;
+        }
+
+        /*!
+            \brief Returns the green value.
+
+            \return The green value.
+        */
+        value_type green()
+        const
+        {
+            return m_green;
+        }
+
+        /*!
+            \brief Returns the blue value.
+
+            \return The blue value.
+        */
+        value_type blue()
+        const
+        {
+            return m_blue;
+        }
+
+        /*!
+            \brief Returns the alpha channel value.
+
+            \return The alpha channel value.
+        */
+        value_type alpha()
+        const
+        {
+            return m_alpha;
+        }
+
+
+    private:
+        // variables
+
+        value_type m_red;
+
+        value_type m_green;
+
+        value_type m_blue;
+
+        value_type m_alpha;
+
+
+    };
+
+
+}}}}}
+
+
+#endif
---
+++ tetengo2/test/test_tetengo2.gui.drawing.win32.gdiplus.color.cpp
@@ -0,0 +1,58 @@
+/*! \file
+    \brief Test of class tetengo2::gui::drawing::win32::gdiplus::color.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include "tetengo2.gui.drawing.win32.gdiplus.color.h"
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo2)
+BOOST_AUTO_TEST_SUITE(gui)
+BOOST_AUTO_TEST_SUITE(drawing)
+BOOST_AUTO_TEST_SUITE(win32)
+BOOST_AUTO_TEST_SUITE(gdiplus)
+BOOST_AUTO_TEST_SUITE(color)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+    BOOST_AUTO_TEST_CASE(operator_equal)
+    {
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+    BOOST_AUTO_TEST_CASE(red)
+    {
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+    BOOST_AUTO_TEST_CASE(green)
+    {
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+    BOOST_AUTO_TEST_CASE(blue)
+    {
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+    BOOST_AUTO_TEST_CASE(alpha)
+    {
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
