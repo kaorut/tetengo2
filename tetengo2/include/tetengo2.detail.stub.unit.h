4eefb10619600725bde9d01fd9617e8bdeb6d3dd
kaoru <kaorut@mc.pikara.ne.jp>
2011-01-28 22:18:41 +0900

    Created tetengo2::detail::stub/windows::unit.

---
+++ tetengo2/include/tetengo2.detail.stub.unit.h
@@ -0,0 +1,37 @@
+/*! \file
+    \brief The definition of tetengo2::detail::stub::unit.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_STUB_UNIT_H)
+#define TETENGO2_DETAIL_STUB_UNIT_H
+
+
+namespace tetengo2 { namespace detail { namespace stub
+{
+    /*!
+        \brief The class for a detail implementation of a unit.
+    */
+    class unit
+    {
+    public:
+        // static functions
+
+
+
+    private:
+        // forbidden operations
+
+        unit();
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ tetengo2/include/tetengo2.detail.windows.unit.h
@@ -0,0 +1,43 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::unit.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_WINDOWS_UNIT_H)
+#define TETENGO2_DETAIL_WINDOWS_UNIT_H
+
+//#define NOMINMAX
+//#define OEMRESOURCE
+//#include <Windows.h>
+
+
+namespace tetengo2 { namespace detail { namespace windows
+{
+    /*!
+        \brief The class for a detail implementation of a unit.
+
+        \tparam Pivot A pivot type.
+    */
+    class unit
+    {
+    public:
+        // static functions
+
+
+
+    private:
+        // forbidden operations
+
+        unit();
+
+
+   };
+
+
+}}}
+
+
+#endif
