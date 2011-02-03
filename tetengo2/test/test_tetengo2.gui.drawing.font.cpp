a14e9b50f701e3b1379ac16f4d222c7e1f929088
kaoru <kaorut@mc.pikara.ne.jp>
2011-02-03 23:48:45 +0900

    Implemented test_tetengo2::gui::drawing::font.

--- tetengo2/test/test_tetengo2.gui.drawing.font.cpp
+++ tetengo2/test/test_tetengo2.gui.drawing.font.cpp
@@ -8,7 +8,24 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include "tetengo2.detail.stub.drawing.h"
+#include "tetengo2.text.h"
+
 #include "tetengo2.gui.drawing.font.h"
+
+
+namespace
+{
+    // types
+
+    typedef
+        tetengo2::gui::drawing::font<
+            std::string, std::size_t, tetengo2::detail::stub::drawing
+        >
+        font_type;
+
+
+}
 
 
 BOOST_AUTO_TEST_SUITE(test_tetengo2)
@@ -19,47 +36,170 @@ BOOST_AUTO_TEST_SUITE(font)
 
     BOOST_AUTO_TEST_CASE(dialog_font)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const font_type& font = font_type::dialog_font();
+
+        BOOST_CHECK(
+            font ==
+            font_type(
+                TETENGO2_TEXT("TetengoFont"), 12, false, false, false, false
+            )
+        );
     }
 
     BOOST_AUTO_TEST_CASE(construction)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const font_type font(
+            TETENGO2_TEXT("TetengoFont"), 12, false, false, false, false
+        );
     }
 
     BOOST_AUTO_TEST_CASE(operator_equal)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const font_type font1(
+                TETENGO2_TEXT("TetengoFont"), 12, false, false, false, false
+            );
+            const font_type font2(
+                TETENGO2_TEXT("TetengoFont"), 12, false, false, false, false
+            );
+
+            BOOST_CHECK(font1 == font2);
+        }
+        {
+            const font_type font1(
+                TETENGO2_TEXT("TetengoFont"), 12, false, false, false, false
+            );
+            const font_type font2(
+                TETENGO2_TEXT("TetengoFont2"), 12, false, false, false, false
+            );
+
+            BOOST_CHECK(font1 != font2);
+        }
+        {
+            const font_type font1(
+                TETENGO2_TEXT("TetengoFont"), 12, false, false, false, false
+            );
+            const font_type font2(
+                TETENGO2_TEXT("TetengoFont"), 42, false, false, false, false
+            );
+
+            BOOST_CHECK(font1 != font2);
+        }
+        {
+            const font_type font1(
+                TETENGO2_TEXT("TetengoFont"), 12, false, false, false, false
+            );
+            const font_type font2(
+                TETENGO2_TEXT("TetengoFont"), 12, true, false, false, false
+            );
+
+            BOOST_CHECK(font1 != font2);
+        }
+        {
+            const font_type font1(
+                TETENGO2_TEXT("TetengoFont"), 12, false, false, false, false
+            );
+            const font_type font2(
+                TETENGO2_TEXT("TetengoFont"), 12, false, true, false, false
+            );
+
+            BOOST_CHECK(font1 != font2);
+        }
+        {
+            const font_type font1(
+                TETENGO2_TEXT("TetengoFont"), 12, false, false, false, false
+            );
+            const font_type font2(
+                TETENGO2_TEXT("TetengoFont"), 12, false, false, true, false
+            );
+
+            BOOST_CHECK(font1 != font2);
+        }
+        {
+            const font_type font1(
+                TETENGO2_TEXT("TetengoFont"), 12, false, false, false, false
+            );
+            const font_type font2(
+                TETENGO2_TEXT("TetengoFont"), 12, false, false, false, true
+            );
+
+            BOOST_CHECK(font1 != font2);
+        }
     }
 
     BOOST_AUTO_TEST_CASE(family)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const font_type font(
+            TETENGO2_TEXT("TetengoFont"), 12, false, true, false, true
+        );
+
+        BOOST_CHECK(
+            font.family() ==
+            font_type::string_type(TETENGO2_TEXT("TetengoFont"))
+        );
     }
 
     BOOST_AUTO_TEST_CASE(size)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const font_type font(
+            TETENGO2_TEXT("TetengoFont"), 12, false, true, false, true
+        );
+
+        BOOST_CHECK_EQUAL(font.size(), 12U);
     }
 
     BOOST_AUTO_TEST_CASE(bold)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const font_type font(
+            TETENGO2_TEXT("TetengoFont"), 12, false, true, false, true
+        );
+
+        BOOST_CHECK(!font.bold());
     }
 
     BOOST_AUTO_TEST_CASE(italic)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const font_type font(
+            TETENGO2_TEXT("TetengoFont"), 12, false, true, false, true
+        );
+
+        BOOST_CHECK(font.italic());
     }
 
     BOOST_AUTO_TEST_CASE(underline)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const font_type font(
+            TETENGO2_TEXT("TetengoFont"), 12, false, true, false, true
+        );
+
+        BOOST_CHECK(!font.underline());
     }
 
     BOOST_AUTO_TEST_CASE(strikeout)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const font_type font(
+            TETENGO2_TEXT("TetengoFont"), 12, false, true, false, true
+        );
+
+        BOOST_CHECK(font.strikeout());
     }
 
 BOOST_AUTO_TEST_SUITE_END()
