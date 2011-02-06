cbaf52323dd834432ecd84b167d069c6a7883d75
kaoru <kaorut@mc.pikara.ne.jp>
2011-02-06 19:56:13 +0900

    Implemented test_tetengo2::gui::drawing::picture_reader.

--- tetengo2/test/test_tetengo2.gui.drawing.picture_reader.cpp
+++ tetengo2/test/test_tetengo2.gui.drawing.picture_reader.cpp
@@ -6,9 +6,32 @@
     $Id$
 */
 
+#include <boost/filesystem.hpp>
+#include <boost/scoped_ptr.hpp>
 #include <boost/test/unit_test.hpp>
 
+#include "stub_tetengo2.gui.drawing.picture.h"
+#include "tetengo2.detail.stub.drawing.h"
+
 #include "tetengo2.gui.drawing.picture_reader.h"
+
+
+namespace
+{
+    // types
+
+    typedef stub_tetengo2::gui::drawing::picture<std::size_t> picture_type;
+
+    typedef
+        tetengo2::gui::drawing::picture_reader<
+            picture_type,
+            boost::filesystem::path,
+            tetengo2::detail::stub::drawing
+        >
+        picture_reader_type;
+
+
+}
 
 
 BOOST_AUTO_TEST_SUITE(test_tetengo2)
@@ -19,12 +42,20 @@ BOOST_AUTO_TEST_SUITE(picture_reader)
 
     BOOST_AUTO_TEST_CASE(construction)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const boost::filesystem::path path;
+        const picture_reader_type reader(path);
     }
 
     BOOST_AUTO_TEST_CASE(read)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const boost::filesystem::path path;
+        picture_reader_type reader(path);
+
+        const boost::scoped_ptr<picture_type> p_picture(reader.read());
     }
 
 
