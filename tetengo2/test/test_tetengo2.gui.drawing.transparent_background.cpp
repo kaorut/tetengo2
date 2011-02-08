35e5c5fb04c62f92a4af878ce707e6a534e067a1
kaoru <kaorut@mc.pikara.ne.jp>
2011-02-09 06:31:45 +0900

    Corrected test_tetengo2::gui::drawing::transparent_background.

--- tetengo2/test/test_tetengo2.gui.drawing.transparent_background.cpp
+++ tetengo2/test/test_tetengo2.gui.drawing.transparent_background.cpp
@@ -1,5 +1,5 @@
 /*! \file
-    \brief Test of class tetengo2::gui::drawing::win32::gdiplus::transparent_background.
+    \brief Test of class tetengo2::gui::drawing::transparent_background.
 
     Copyright (C) 2007-2011 kaoru
 
@@ -30,8 +30,6 @@ namespace
 BOOST_AUTO_TEST_SUITE(test_tetengo2)
 BOOST_AUTO_TEST_SUITE(gui)
 BOOST_AUTO_TEST_SUITE(drawing)
-BOOST_AUTO_TEST_SUITE(win32)
-BOOST_AUTO_TEST_SUITE(gdiplus)
 BOOST_AUTO_TEST_SUITE(transparent_background)
     // test cases
 
@@ -43,8 +41,6 @@ BOOST_AUTO_TEST_SUITE(transparent_background)
     }
 
 
-BOOST_AUTO_TEST_SUITE_END()
-BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
