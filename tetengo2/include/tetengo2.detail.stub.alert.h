2d938900a6eb62855f0d9b204dc1910a6db79da4
kaoru <kaorut@mc.pikara.ne.jp>
2011-02-22 06:15:31 +0900

    Created tetengo2::detail::stub/windows::alert.

---
+++ tetengo2/include/tetengo2.detail.stub.alert.h
@@ -0,0 +1,36 @@
+/*! \file
+    \brief The definition of tetengo2::detail::stub::alert.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_STUB_ALERT_H)
+#define TETENGO2_DETAIL_STUB_ALERT_H
+
+
+namespace tetengo2 { namespace detail { namespace stub
+{
+    /*!
+        \brief The class for a detail implementation of an alert.
+    */
+    class alert
+    {
+    public:
+        // static functions
+
+
+    private:
+        // forbidden operations
+
+        alert();
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ tetengo2/include/tetengo2.detail.windows.alert.h
@@ -0,0 +1,40 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::alert.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_WINDOWS_ALERT_H)
+#define TETENGO2_DETAIL_WINDOWS_ALERT_H
+
+#define NOMINMAX
+#define OEMRESOURCE
+#include <Windows.h>
+
+
+namespace tetengo2 { namespace detail { namespace windows
+{
+    /*!
+        \brief The class for a detail implementation of an alert.
+    */
+    class alert
+    {
+    public:
+        // static functions
+
+
+    private:
+        // forbidden operations
+
+        alert();
+
+
+   };
+
+
+}}}
+
+
+#endif
