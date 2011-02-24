9331866d60b01f6a2d05bf7c39a40b722619148b
kaoru <kaorut@mc.pikara.ne.jp>
2011-02-24 20:13:03 +0900

    Created tetengo2::detail::stub/windows/windows::gdiplus::gui_fixture.

---
+++ tetengo2/include/tetengo2.detail.windows.gdiplus.gui_fixture.h
@@ -0,0 +1,44 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::gdiplus::gui_fixture.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_WINDOWS_GDIPLUS_GUIFIXTURE_H)
+#define TETENGO2_DETAIL_WINDOWS_GDIPLUS_GUIFIXTURE_H
+
+#include "tetengo2.cpp0x.h"
+
+
+namespace tetengo2 { namespace detail { namespace windows { namespace gdiplus
+{
+    /*!
+        \brief The class for a detail implementation of a GUI fixture.
+    */
+    class gui_fixture
+    {
+        // constructors and destructor
+
+        /*!
+            \brief Creates a detail implementation of a GUI fixture.
+        */
+        gui_fixture()
+        {}
+
+        /*!
+            \brief Destroys the detail implementation of a GUI fixture.
+        */
+        ~gui_fixture()
+        TETENGO2_CPP0X_NOEXCEPT
+        {}
+
+
+    };
+
+
+}}}}
+
+
+#endif
