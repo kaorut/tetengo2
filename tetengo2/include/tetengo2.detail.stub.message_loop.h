a1705d35419f94efb4147b8e89588739b6340af2
kaoru <kaorut@mc.pikara.ne.jp>
2011-02-26 06:45:31 +0900

    Created tetengo2::detail::stub/windows::message_loop.

---
+++ tetengo2/include/tetengo2.detail.stub.message_loop.h
@@ -0,0 +1,36 @@
+/*! \file
+    \brief The definition of tetengo2::detail::stub::message_loop.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_STUB_MESSAGELOOP_H)
+#define TETENGO2_DETAIL_STUB_MESSAGELOOP_H
+
+
+namespace tetengo2 { namespace detail { namespace stub
+{
+    /*!
+        \brief The class for a detail implementation of a message loop.
+    */
+    class message_loop
+    {
+    public:
+        // static functions
+
+
+    private:
+        // forbidden operations
+
+        message_loop();
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ tetengo2/include/tetengo2.detail.windows.message_loop.h
@@ -0,0 +1,40 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::message_loop.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_WINDOWS_MESSAGELOOP_H)
+#define TETENGO2_DETAIL_WINDOWS_MESSAGELOOP_H
+
+#define NOMINMAX
+#define OEMRESOURCE
+#include <Windows.h>
+
+
+namespace tetengo2 { namespace detail { namespace windows
+{
+    /*!
+        \brief The class for a detail implementation of a message loop.
+    */
+    class message_loop
+    {
+    public:
+        // static functions
+
+
+    private:
+        // forbidden operations
+
+        message_loop();
+
+
+   };
+
+
+}}}
+
+
+#endif
