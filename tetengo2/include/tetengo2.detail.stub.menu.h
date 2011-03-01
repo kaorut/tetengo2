b226f85d0342b51f86177c31647ff34d4035914e
kaoru <kaorut@mc.pikara.ne.jp>
2011-03-02 05:31:30 +0900

    Created tetengo2::detail::stub/windows::menu.

---
+++ tetengo2/include/tetengo2.detail.stub.menu.h
@@ -0,0 +1,36 @@
+/*! \file
+    \brief The definition of tetengo2::detail::stub::menu.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_STUB_MENU_H)
+#define TETENGO2_DETAIL_STUB_MENU_H
+
+
+namespace tetengo2 { namespace detail { namespace stub
+{
+    /*!
+        \brief The class for a detail implementation of a menu.
+    */
+    class menu
+    {
+    public:
+        // static functions
+
+
+    private:
+        // forbidden operations
+
+        menu();
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ tetengo2/include/tetengo2.detail.windows.menu.h
@@ -0,0 +1,39 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::menu.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_WINDOWS_MENU_H)
+#define TETENGO2_DETAIL_WINDOWS_MENU_H
+
+#define NOMINMAX
+#define OEMRESOURCE
+#include <Windows.h>
+
+
+namespace tetengo2 { namespace detail { namespace windows
+{
+    /*!
+        \brief The class for a detail implementation of a menu.
+    */
+    class menu
+    {
+    public:
+        // static functions
+
+
+        // forbidden operations
+
+        menu();
+
+
+   };
+
+
+}}}
+
+
+#endif
