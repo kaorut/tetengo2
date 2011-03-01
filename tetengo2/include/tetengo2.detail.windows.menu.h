3d3d1f777b821272534ce1535940e63afc544f82
kaoru <kaorut@mc.pikara.ne.jp>
2011-03-02 06:07:39 +0900

    Implemented tetengo2::detail::windows::menu.

--- tetengo2/include/tetengo2.detail.windows.menu.h
+++ tetengo2/include/tetengo2.detail.windows.menu.h
@@ -9,22 +9,67 @@
 #if !defined(TETENGO2_DETAIL_WINDOWS_MENU_H)
 #define TETENGO2_DETAIL_WINDOWS_MENU_H
 
+#include <type_traits>
+
 #define NOMINMAX
 #define OEMRESOURCE
 #include <Windows.h>
 
+#include "tetengo2.cpp0x.h"
+
 
 namespace tetengo2 { namespace detail { namespace windows
 {
+#if !defined(DOCUMENTATION)
+    namespace detail
+    {
+        // types
+
+        struct menu_deleter
+        {
+            void operator()(::HMENU gdi_menu_handle)
+            const
+            {
+                if (::IsMenu(gdi_menu_handle))
+                    ::DestroyMenu(gdi_menu_handle);
+            }
+
+
+        };
+
+
+    }
+#endif
+
     /*!
         \brief The class for a detail implementation of a menu.
     */
     class menu
     {
     public:
+        // types
+
+        //! The detail implementation type of a menu.
+        typedef std::remove_pointer< ::HMENU>::type menu_details_type;
+
+        //! The menu handle type.
+        typedef
+            cpp0x::unique_ptr<menu_details_type, detail::menu_deleter>::type
+            menu_handle_type;
+
+
         // static functions
 
+        /*!
+            \brief Creates a men menu.
+        */
+        menu_handle_type create_main_menu()
+        {
+            return menu_handle_type();
+        }
 
+
+    private:
         // forbidden operations
 
         menu();
