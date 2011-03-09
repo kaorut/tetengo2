4e8b1d4896854088ec7124f35df7b58a2133afad
kaoru <kaorut@mc.pikara.ne.jp>
2011-03-09 21:54:12 +0900

    Implemented test_tetengo2::gui::main_menu.

--- tetengo2/test/test_tetengo2.gui.main_menu.cpp
+++ tetengo2/test/test_tetengo2.gui.main_menu.cpp
@@ -6,9 +6,47 @@
     $Id$
 */
 
+#include <string>
+
 #include <boost/test/unit_test.hpp>
 
+#include "tetengo2.cpp0x.h"
+#include "tetengo2.detail.stub.encoding.h"
+#include "tetengo2.detail.stub.menu.h"
+#include "tetengo2.encoder.h"
+#include "tetengo2.encoding.locale.h"
+#include "tetengo2.gui.menu_observer_set.h"
+#include "tetengo2.gui.traits.menu_traits.h"
+
 #include "tetengo2.gui.main_menu.h"
+
+
+namespace
+{
+    // types
+
+    typedef
+        tetengo2::encoding::locale<
+            std::string, tetengo2::detail::stub::encoding
+        >
+        encoding_type;
+
+    typedef tetengo2::encoder<encoding_type, encoding_type> encoder_type;
+
+    typedef
+        tetengo2::gui::traits::menu_traits<
+            std::string, encoder_type, tetengo2::gui::menu_observer_set
+        >
+        menu_traits_type;
+
+    typedef tetengo2::detail::stub::menu menu_details_type;
+
+    typedef
+        tetengo2::gui::main_menu<menu_traits_type, menu_details_type>
+        main_menu_type;
+
+
+}
 
 
 BOOST_AUTO_TEST_SUITE(test_tetengo2)
@@ -18,7 +56,20 @@ BOOST_AUTO_TEST_SUITE(main_menu)
 
     BOOST_AUTO_TEST_CASE(construction)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const main_menu_type main_menu;
+    }
+
+    BOOST_AUTO_TEST_CASE(style)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const main_menu_type main_menu;
+
+        BOOST_CHECK(
+            &main_menu.style() == &menu_details_type::main_menu_style()
+        );
     }
 
 
