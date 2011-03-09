d060e414cfc21f616e430c5e66eed32084aa426f
kaoru <kaorut@mc.pikara.ne.jp>
2011-03-09 21:50:53 +0900

    Implemented test_tetengo2::gui::menu.

--- tetengo2/test/test_tetengo2.gui.menu.cpp
+++ tetengo2/test/test_tetengo2.gui.menu.cpp
@@ -6,9 +6,73 @@
     $Id$
 */
 
+#include <stdexcept>
+#include <string>
+#include <utility>
+
 #include <boost/test/unit_test.hpp>
 
+#include "tetengo2.cpp0x.h"
+#include "tetengo2.detail.stub.encoding.h"
+#include "tetengo2.detail.stub.menu.h"
+#include "tetengo2.encoder.h"
+#include "tetengo2.encoding.locale.h"
+#include "tetengo2.gui.menu_observer_set.h"
+#include "tetengo2.gui.traits.menu_traits.h"
+
 #include "tetengo2.gui.menu.h"
+
+
+namespace
+{
+    // types
+
+    typedef
+        tetengo2::encoding::locale<
+            std::string, tetengo2::detail::stub::encoding
+        >
+        encoding_type;
+
+    typedef tetengo2::encoder<encoding_type, encoding_type> encoder_type;
+
+    typedef
+        tetengo2::gui::traits::menu_traits<
+            std::string, encoder_type, tetengo2::gui::menu_observer_set
+        >
+        menu_traits_type;
+
+    typedef tetengo2::detail::stub::menu menu_details_type;
+
+    typedef
+        tetengo2::gui::menu<menu_traits_type, menu_details_type> menu_type;
+
+    class concrete_menu : public menu_type
+    {
+    public:
+        // constructors and destructors
+
+        concrete_menu(std::string&& text)
+        :
+        menu_type(
+            std::forward<std::string>(text), menu_details_type::create_menu()
+        )
+        {}
+
+        
+    private:
+        // virtual functions
+
+        virtual const style_type& style_impl()
+        const
+        {
+            return menu_details_type::menu_command_style();
+        }
+
+
+    };
+
+
+}
 
 
 BOOST_AUTO_TEST_SUITE(test_tetengo2)
@@ -18,62 +82,141 @@ BOOST_AUTO_TEST_SUITE(menu)
 
     BOOST_AUTO_TEST_CASE(construction)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
-    }
+        BOOST_TEST_PASSPOINT();
 
-    BOOST_AUTO_TEST_CASE(id)
-    {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
-    }
-
-    BOOST_AUTO_TEST_CASE(handle)
-    {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        const concrete_menu menu(std::string("Tetengo"));
     }
 
     BOOST_AUTO_TEST_CASE(text)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const concrete_menu menu(std::string("Tetengo"));
+
+        BOOST_CHECK(menu.text() == "Tetengo");
+    }
+
+    BOOST_AUTO_TEST_CASE(style)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const concrete_menu menu(std::string("Tetengo"));
+
+        BOOST_CHECK(
+            &menu.style() == &menu_details_type::menu_command_style()
+        );
     }
 
     BOOST_AUTO_TEST_CASE(select)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        concrete_menu menu(std::string("Tetengo"));
+
+        menu.select();
     }
 
     BOOST_AUTO_TEST_CASE(menu_observer_set)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const concrete_menu menu(std::string("Tetengo"));
+
+            menu.menu_observer_set();
+        }
+        {
+            concrete_menu menu(std::string("Tetengo"));
+
+            menu.menu_observer_set();
+        }
     }
 
     BOOST_AUTO_TEST_CASE(begin)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const concrete_menu menu(std::string("Tetengo"));
+
+            menu.begin();
+        }
+        {
+            concrete_menu menu(std::string("Tetengo"));
+
+            menu.begin();
+        }
     }
 
     BOOST_AUTO_TEST_CASE(end)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const concrete_menu menu(std::string("Tetengo"));
+
+            menu.end();
+        }
+        {
+            concrete_menu menu(std::string("Tetengo"));
+
+            menu.end();
+        }
     }
 
     BOOST_AUTO_TEST_CASE(recursive_begin)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const concrete_menu menu(std::string("Tetengo"));
+
+            menu.recursive_begin();
+        }
+        {
+            concrete_menu menu(std::string("Tetengo"));
+
+            menu.recursive_begin();
+        }
     }
 
     BOOST_AUTO_TEST_CASE(recursive_end)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const concrete_menu menu(std::string("Tetengo"));
+
+            menu.recursive_end();
+        }
+        {
+            concrete_menu menu(std::string("Tetengo"));
+
+            menu.recursive_end();
+        }
     }
 
     BOOST_AUTO_TEST_CASE(insert)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        concrete_menu menu(std::string("Tetengo"));
+        tetengo2::cpp0x::unique_ptr<menu_type>::type p_child(
+            new concrete_menu(std::string("Hoge"))
+        );
+
+        // Assertion fails.
+        //menu.insert(menu.begin(), std::move(p_child)),
     }
 
     BOOST_AUTO_TEST_CASE(erase)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        concrete_menu menu(std::string("Tetengo"));
+
+        // Assertion fails.
+        //menu.erase(menu.begin(), menu.begin());
     }
 
 
