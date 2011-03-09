8a233280ff49066e83139c3d6d3b53eca9a37e9f
kaoru <kaorut@mc.pikara.ne.jp>
2011-03-09 21:53:11 +0900

    Implemented test_tetengo2::gui::menu_separator.

--- tetengo2/test/test_tetengo2.gui.menu_separator.cpp
+++ tetengo2/test/test_tetengo2.gui.menu_separator.cpp
@@ -6,9 +6,48 @@
     $Id$
 */
 
+#include <string>
+
 #include <boost/test/unit_test.hpp>
 
 #include "tetengo2.gui.menu_separator.h"
+
+#include "tetengo2.cpp0x.h"
+#include "tetengo2.detail.stub.encoding.h"
+#include "tetengo2.detail.stub.menu.h"
+#include "tetengo2.encoder.h"
+#include "tetengo2.encoding.locale.h"
+#include "tetengo2.gui.menu_observer_set.h"
+#include "tetengo2.gui.traits.menu_traits.h"
+
+#include "tetengo2.gui.menu_separator.h"
+
+
+namespace
+{
+    // types
+
+    typedef
+        tetengo2::encoding::locale<
+            std::string, tetengo2::detail::stub::encoding
+        >
+        encoding_type;
+
+    typedef tetengo2::encoder<encoding_type, encoding_type> encoder_type;
+
+    typedef
+        tetengo2::gui::traits::menu_traits<
+            std::string, encoder_type, tetengo2::gui::menu_observer_set
+        >
+        menu_traits_type;
+
+    typedef tetengo2::detail::stub::menu menu_details_type;
+
+    typedef
+        tetengo2::gui::menu_separator<menu_traits_type, menu_details_type>
+        menu_separator_type;
+
+}
 
 
 BOOST_AUTO_TEST_SUITE(test_tetengo2)
@@ -18,7 +57,21 @@ BOOST_AUTO_TEST_SUITE(menu_separator)
 
     BOOST_AUTO_TEST_CASE(construction)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const menu_separator_type menu_separator;
+    }
+
+    BOOST_AUTO_TEST_CASE(style)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const menu_separator_type menu_separator;
+
+        BOOST_CHECK(
+            &menu_separator.style() ==
+            &menu_details_type::menu_separator_style()
+        );
     }
 
 
