48b48a69b59b8d1c0daa063988a3694c15f1f4df
kaoru <kaorut@mc.pikara.ne.jp>
2011-03-17 06:06:38 +0900

    Created tetengo2::detail::stub/windows::widget.

---
+++ tetengo2/include/tetengo2.detail.stub.widget.h
@@ -0,0 +1,39 @@
+/*! \file
+    \brief The definition of tetengo2::detail::stub::widget.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_STUB_WIDGET_H)
+#define TETENGO2_DETAIL_STUB_WIDGET_H
+
+
+namespace tetengo2 { namespace detail { namespace stub
+{
+    /*!
+        \brief The class for a detail implementation of a widget.
+    */
+    class widget
+    {
+    public:
+        // types
+
+
+        // static functions
+
+
+    private:
+        // forbidden operations
+
+        widget();
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ tetengo2/include/tetengo2.detail.windows.widget.h
@@ -0,0 +1,44 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::widget.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_WINDOWS_WIDGET_H)
+#define TETENGO2_DETAIL_WINDOWS_WIDGET_H
+
+#define NOMINMAX
+#define OEMRESOURCE
+#include <Windows.h>
+#include <CommCtrl.h>
+
+
+namespace tetengo2 { namespace detail { namespace windows
+{
+    /*!
+        \brief The class for a detail implementation of a widget.
+    */
+    class widget
+    {
+    public:
+        // types
+
+
+        // static functions
+
+
+    private:
+        // forbidden operations
+
+        widget();
+
+
+   };
+
+
+}}}
+
+
+#endif
