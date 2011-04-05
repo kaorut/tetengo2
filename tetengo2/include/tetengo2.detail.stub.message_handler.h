6609242a44c22424c2188931b1f4ca9bbca00831
kaoru <kaorut@mc.pikara.ne.jp>
2011-04-06 05:59:15 +0900

    Created tetengo2::detail::stub/windows::message_handler.

---
+++ tetengo2/include/tetengo2.detail.stub.message_handler.h
@@ -0,0 +1,36 @@
+/*! \file
+    \brief The definition of tetengo2::detail::stub::message_handler.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_STUB_MESSAGEHANDLER_H)
+#define TETENGO2_DETAIL_STUB_MESSAGEHANDLER_H
+
+
+namespace tetengo2 { namespace detail { namespace stub
+{
+    /*!
+        \brief The class for a detail implementation of a message handler.
+    */
+    class message_handler
+    {
+    public:
+        // static functions
+
+
+    private:
+        // forbidden operations
+
+        message_handler();
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ tetengo2/include/tetengo2.detail.windows.message_handler.h
@@ -0,0 +1,35 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::message_handler.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_WINDOWS_MESSAGEHANDLER_H)
+#define TETENGO2_DETAIL_WINDOWS_MESSAGEHANDLER_H
+
+
+namespace tetengo2 { namespace detail { namespace windows
+{
+    /*!
+        \brief The class for a detail implementation of a message handler.
+    */
+    class message_handler
+    {
+    public:
+        // static functions
+
+
+        // forbidden operations
+
+        message_handler();
+
+
+   };
+
+
+}}}
+
+
+#endif
