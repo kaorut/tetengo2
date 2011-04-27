97c4e0d1f4f1aa0a1936ed509f2440d54a961e10
kaoru <kaorut@mc.pikara.ne.jp>
2011-04-27 23:33:52 +0900

    Implemented tetengo2::detail::stub::message_handler.

--- tetengo2/include/tetengo2.detail.stub.message_handler.h
+++ tetengo2/include/tetengo2.detail.stub.message_handler.h
@@ -9,16 +9,192 @@
 #if !defined(TETENGO2_DETAIL_STUB_MESSAGEHANDLER_H)
 #define TETENGO2_DETAIL_STUB_MESSAGEHANDLER_H
 
+#include <functional>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
 
 namespace tetengo2 { namespace detail { namespace stub
 {
     /*!
-        \brief The class for a detail implementation of a message handler.
+        \brief The class template for a detail implementation of a message
+               handler.
+
+        \tparam WidgetDetails A detail implementation type of a widget.
     */
+    template <typename WidgetDetails>
     class message_handler
     {
     public:
+        // types
+
+        //! The detail implementation type of a widget.
+        typedef WidgetDetails widget_details_type;
+
+        //! The message handler type.
+        typedef std::function<void ()> message_handler_type;
+
+        //! The message handler map type.
+        typedef
+            std::unordered_map<int, std::vector<message_handler_type>>
+            message_handler_map_type;
+
+
         // static functions
+
+        /*!
+            \brief Make a message handler map for a widget.
+
+            \tparam Widget A widget type.
+
+            \param widget      A widget.
+            \param initial_map An initial message handler map.
+
+            \return A message handler map.
+        */
+        template <typename Widget>
+        static message_handler_map_type make_widget_message_handler_map(
+            Widget&                    widget,
+            message_handler_map_type&& initial_map
+        )
+        {
+            return std::forward<message_handler_map_type>(initial_map);
+        }
+
+        /*!
+            \brief Make a message handler map for an abstract window.
+
+            \tparam AbstractWindow An abstract window type.
+
+            \param abstract_window An abstract window.
+            \param initial_map     An initial message handler map.
+
+            \return A message handler map.
+        */
+        template <typename AbstractWindow>
+        static message_handler_map_type
+        make_abstract_window_message_handler_map(
+            AbstractWindow&            abstract_window,
+            message_handler_map_type&& initial_map
+        )
+        {
+            return std::forward<message_handler_map_type>(initial_map);
+        }
+
+        /*!
+            \brief Make a message handler map for a dialog.
+
+            \tparam Dialog A dialog type.
+
+            \param dialog      A dialog.
+            \param initial_map An initial message handler map.
+
+            \return A message handler map.
+        */
+        template <typename Dialog>
+        static message_handler_map_type make_dialog_message_handler_map(
+            Dialog&                    dialog,
+            message_handler_map_type&& initial_map
+        )
+        {
+            return std::forward<message_handler_map_type>(initial_map);
+        }
+
+        /*!
+            \brief Make a message handler map for a window.
+
+            \tparam Window A window type.
+
+            \param window      A window.
+            \param initial_map An initial message handler map.
+
+            \return A message handler map.
+        */
+        template <typename Window>
+        static message_handler_map_type make_window_message_handler_map(
+            Window&                    window,
+            message_handler_map_type&& initial_map
+        )
+        {
+            return std::forward<message_handler_map_type>(initial_map);
+        }
+
+        /*!
+            \brief Make a message handler map for a control.
+
+            \tparam Control A control type.
+
+            \param control     A control.
+            \param initial_map An initial message handler map.
+
+            \return A message handler map.
+        */
+        template <typename Control>
+        static message_handler_map_type make_control_message_handler_map(
+            Control&                   control,
+            message_handler_map_type&& initial_map
+        )
+        {
+            return std::forward<message_handler_map_type>(initial_map);
+        }
+
+        /*!
+            \brief Make a message handler map for a button.
+
+            \tparam Button A button type.
+
+            \param button      A button.
+            \param initial_map An initial message handler map.
+
+            \return A message handler map.
+        */
+        template <typename Button>
+        static message_handler_map_type make_button_message_handler_map(
+            Button&                    button,
+            message_handler_map_type&& initial_map
+        )
+        {
+            return std::forward<message_handler_map_type>(initial_map);
+        }
+
+        /*!
+            \brief Make a message handler map for an image.
+
+            \tparam Image An image type.
+
+            \param image       An image.
+            \param initial_map An initial message handler map.
+
+            \return A message handler map.
+        */
+        template <typename Image>
+        static message_handler_map_type make_image_message_handler_map(
+            Image&                     image,
+            message_handler_map_type&& initial_map
+        )
+        {
+            return std::forward<message_handler_map_type>(initial_map);
+        }
+
+        /*!
+            \brief Make a message handler map for a label.
+
+            \tparam Label A label type.
+
+            \param label       A label.
+            \param initial_map An initial message handler map.
+
+            \return A message handler map.
+        */
+        template <typename Label>
+        static message_handler_map_type make_label_message_handler_map(
+            Label&                     label,
+            message_handler_map_type&& initial_map
+        )
+        {
+            return std::forward<message_handler_map_type>(initial_map);
+        }
 
 
     private:
