0ba207cc30fce90f4de6fa3e6062b547517e660b
kaoru <kaorut@mc.pikara.ne.jp>
2011-04-28 20:11:49 +0900

    Implemented test_tetengo2::gui::window.

--- tetengo2/test/test_tetengo2.gui.window.cpp
+++ tetengo2/test/test_tetengo2.gui.window.cpp
@@ -6,9 +6,183 @@
     $Id$
 */
 
+#include <cstddef>
+#include <string>
+
 #include <boost/test/unit_test.hpp>
 
+#include "tetengo2.detail.stub.encoding.h"
+#include "tetengo2.detail.stub.alert.h"
+#include "tetengo2.detail.stub.drawing.h"
+#include "tetengo2.detail.stub.menu.h"
+#include "tetengo2.detail.stub.message_handler.h"
+#include "tetengo2.detail.stub.unit.h"
+#include "tetengo2.detail.stub.widget.h"
+#include "tetengo2.encoder.h"
+#include "tetengo2.encoding.locale.h"
+#include "tetengo2.gui.alert.h"
+#include "tetengo2.gui.drawing.background.h"
+#include "tetengo2.gui.drawing.font.h"
+#include "tetengo2.gui.drawing.picture.h"
+#include "tetengo2.gui.drawing.widget_canvas.h"
+#include "tetengo2.gui.main_menu.h"
+#include "tetengo2.gui.menu.h"
+#include "tetengo2.gui.menu_observer_set.h"
+#include "tetengo2.gui.mouse_observer_set.h"
+#include "tetengo2.gui.paint_observer_set.h"
+#include "tetengo2.gui.traits.abstract_window_traits.h"
+#include "tetengo2.gui.traits.menu_traits.h"
+#include "tetengo2.gui.traits.widget_traits.h"
+#include "tetengo2.gui.traits.window_traits.h"
+#include "tetengo2.gui.unit.em.h"
+#include "tetengo2.gui.window_observer_set.h"
+
 #include "tetengo2.gui.window.h"
+
+
+namespace
+{
+    // types
+
+    typedef tetengo2::detail::stub::encoding encoding_details_type;
+
+    typedef
+        tetengo2::encoding::locale<std::wstring, encoding_details_type>
+        internal_encoding_type;
+
+    typedef
+        tetengo2::encoding::locale<std::wstring, encoding_details_type>
+        ui_encoding_type;
+
+    typedef
+        tetengo2::encoder<internal_encoding_type, ui_encoding_type>
+        ui_encoder_type;
+
+    typedef
+        tetengo2::encoding::locale<std::string, encoding_details_type>
+        exception_encoding_type;
+
+    typedef
+        tetengo2::encoder<internal_encoding_type, exception_encoding_type>
+        exception_encoder_type;
+
+    typedef tetengo2::detail::stub::drawing drawing_details_type;
+
+    typedef
+        tetengo2::gui::drawing::font<
+            std::wstring, std::size_t, drawing_details_type
+        >
+        font_type;
+
+    typedef
+        tetengo2::gui::drawing::picture<std::size_t, drawing_details_type>
+        picture_type;
+
+    typedef
+        tetengo2::gui::drawing::background<drawing_details_type>
+        background_type;
+
+    typedef tetengo2::detail::stub::unit unit_details_type;
+
+    typedef
+        tetengo2::gui::unit::em<std::size_t, std::size_t, unit_details_type>
+        unit_size_type;
+
+    typedef std::pair<unit_size_type, unit_size_type> dimension_type;
+
+    typedef
+        tetengo2::gui::drawing::widget_canvas<
+            std::size_t,
+            std::wstring,
+            dimension_type,
+            ui_encoder_type,
+            background_type,
+            font_type,
+            picture_type,
+            drawing_details_type
+        >
+        canvas_type;
+
+    typedef
+        tetengo2::detail::stub::alert<std::string, ui_encoder_type>
+        alert_details_type;
+
+    typedef
+        tetengo2::gui::alert<
+            ui_encoder_type, exception_encoder_type, alert_details_type
+        >
+        alert_type;
+
+    typedef
+        tetengo2::gui::unit::em<
+            std::ptrdiff_t, std::ptrdiff_t, unit_details_type
+        >
+        unit_difference_type;
+
+    typedef
+        std::pair<unit_difference_type, unit_difference_type> position_type;
+
+    typedef
+        tetengo2::gui::traits::widget_traits<
+            canvas_type,
+            alert_type,
+            position_type,
+            dimension_type,
+            std::wstring,
+            ui_encoder_type,
+            background_type,
+            font_type,
+            tetengo2::gui::paint_observer_set<canvas_type>,
+            tetengo2::gui::mouse_observer_set
+        >
+        widget_traits_type;
+
+
+    typedef
+        tetengo2::gui::traits::menu_traits<
+            std::wstring,
+            ui_encoder_type,
+            tetengo2::gui::menu_observer_set
+        >
+        menu_traits_type;
+
+    typedef tetengo2::detail::stub::menu menu_details_type;
+
+    typedef
+        tetengo2::gui::menu<menu_traits_type, menu_details_type> menu_type;
+
+    typedef
+        tetengo2::gui::main_menu<menu_traits_type, menu_details_type>
+        main_menu_type;
+
+    typedef
+        tetengo2::gui::traits::abstract_window_traits<
+            widget_traits_type,
+            main_menu_type,
+            tetengo2::gui::window_observer_set
+        >
+        abstract_window_traits_type;
+
+    typedef
+        tetengo2::gui::traits::window_traits<abstract_window_traits_type>
+        window_traits_type;
+
+    typedef tetengo2::detail::stub::widget widget_details_type;
+
+    typedef
+        tetengo2::detail::stub::message_handler<widget_details_type>
+        message_handler_details_type;
+
+    typedef
+        tetengo2::gui::window<
+            window_traits_type,
+            widget_details_type,
+            message_handler_details_type
+        >
+        window_type;
+
+
+}
 
 
 BOOST_AUTO_TEST_SUITE(test_tetengo2)
@@ -18,12 +192,17 @@ BOOST_AUTO_TEST_SUITE(window)
 
     BOOST_AUTO_TEST_CASE(construction)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
-    }
+        BOOST_TEST_PASSPOINT();
 
-    BOOST_AUTO_TEST_CASE(handle)
-    {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        {
+            const window_type window;
+        }
+        {
+            window_type parent;
+            const window_type window(
+                static_cast<window_type::base_type&>(parent)
+            );
+        }
     }
 
 
