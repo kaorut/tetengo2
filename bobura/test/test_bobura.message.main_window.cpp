89989619b065ce109ce85d875da7648d6a5c9bd0
kaoru <kaorut@mc.pikara.ne.jp>
2011-05-31 19:58:52 +0900

    Implemented test_bobura::message::main_window.

--- bobura/test/test_bobura.message.main_window.cpp
+++ bobura/test/test_bobura.message.main_window.cpp
@@ -6,7 +6,10 @@
     $Id$
 */
 
+#include <boost/mpl/at.hpp>
 #include <boost/test/unit_test.hpp>
+
+#include "test_bobura.types.h"
 
 #include "bobura.message.main_window.h"
 
@@ -19,12 +22,32 @@ BOOST_AUTO_TEST_SUITE(menu)
 
     BOOST_AUTO_TEST_CASE(construction)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        typedef
+            boost::mpl::at<
+                command_type_list_type, bobura::command::type::nop
+            >::type
+            command_type;
+
+        const command_type command = command_type();
+        const bobura::message::main_window::menu<command_type> menu(command);
     }
 
     BOOST_AUTO_TEST_CASE(operator_paren)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        typedef
+            boost::mpl::at<
+                command_type_list_type, bobura::command::type::nop
+            >::type
+            command_type;
+
+        const command_type command = command_type();
+        const bobura::message::main_window::menu<command_type> menu(command);
+
+        menu();
     }
 
 
@@ -32,9 +55,28 @@ BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE(paint)
     // test cases
 
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const bobura::message::main_window::paint<
+            canvas_type, position_type, picture_reader_type
+        > paint(boost::filesystem::path("image_path"));
+    }
+
     BOOST_AUTO_TEST_CASE(operator_paren)
     {
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        BOOST_TEST_PASSPOINT();
+
+        const bobura::message::main_window::paint<
+            canvas_type, position_type, picture_reader_type
+        > paint(boost::filesystem::path("image_path"));
+
+        const window_type window;
+        tetengo2::cpp0x::unique_ptr<canvas_type>::type p_canvas(
+            window.create_canvas()
+        );
+        paint(*p_canvas);
     }
 
 
