d17bc5b2e12fed9ddedcab553b7df5005acdff47
kaoru <kaorut@mc.pikara.ne.jp>
2011-08-23 00:53:19 +0900

    Implemented tetengo2::make_unique_ptr().

--- tetengo2/include/tetengo2.unique.h
+++ tetengo2/include/tetengo2.unique.h
@@ -46,6 +46,438 @@ namespace tetengo2
     }
 
     /*!
+        \brief Makes a unique pointer.
+
+        \tparam T  A type.
+        \tparam A1 An argument type #1.
+        \tparam A2 An argument type #2.
+
+        \param arg1 An argument #1.
+        \param arg2 An argument #2.
+
+        \return A unique pointer
+    */
+    template <typename T, typename A1, typename A2>
+    typename tetengo2::cpp0x::unique_ptr<T>::type make_unique(
+        A1&& arg1,
+        A2&& arg2
+    )
+    {
+        return typename tetengo2::cpp0x::unique_ptr<T>::type(
+            new T(std::forward<A1>(arg1), std::forward<A2>(arg2))
+        );
+    }
+
+    /*!
+        \brief Makes a unique pointer.
+
+        \tparam T  A type.
+        \tparam A1 An argument type #1.
+        \tparam A2 An argument type #2.
+        \tparam A3 An argument type #3.
+
+        \param arg1 An argument #1.
+        \param arg2 An argument #2.
+        \param arg3 An argument #3.
+
+        \return A unique pointer
+    */
+    template <typename T, typename A1, typename A2, typename A3>
+    typename tetengo2::cpp0x::unique_ptr<T>::type make_unique(
+        A1&& arg1,
+        A2&& arg2,
+        A3&& arg3
+    )
+    {
+        return typename tetengo2::cpp0x::unique_ptr<T>::type(
+            new T(
+                std::forward<A1>(arg1),
+                std::forward<A2>(arg2),
+                std::forward<A3>(arg3)
+            )
+        );
+    }
+
+    /*!
+        \brief Makes a unique pointer.
+
+        \tparam T  A type.
+        \tparam A1 An argument type #1.
+        \tparam A2 An argument type #2.
+        \tparam A3 An argument type #3.
+        \tparam A4 An argument type #4.
+
+        \param arg1 An argument #1.
+        \param arg2 An argument #2.
+        \param arg3 An argument #3.
+        \param arg4 An argument #4.
+
+        \return A unique pointer
+    */
+    template <typename T, typename A1, typename A2, typename A3, typename A4>
+    typename tetengo2::cpp0x::unique_ptr<T>::type make_unique(
+        A1&& arg1,
+        A2&& arg2,
+        A3&& arg3,
+        A4&& arg4
+    )
+    {
+        return typename tetengo2::cpp0x::unique_ptr<T>::type(
+            new T(
+                std::forward<A1>(arg1),
+                std::forward<A2>(arg2),
+                std::forward<A3>(arg3),
+                std::forward<A4>(arg4)
+            )
+        );
+    }
+
+    /*!
+        \brief Makes a unique pointer.
+
+        \tparam T  A type.
+        \tparam A1 An argument type #1.
+        \tparam A2 An argument type #2.
+        \tparam A3 An argument type #3.
+        \tparam A4 An argument type #4.
+        \tparam A5 An argument type #5.
+
+        \param arg1 An argument #1.
+        \param arg2 An argument #2.
+        \param arg3 An argument #3.
+        \param arg4 An argument #4.
+        \param arg5 An argument #5.
+
+        \return A unique pointer
+    */
+    template <
+        typename T,
+        typename A1,
+        typename A2,
+        typename A3,
+        typename A4,
+        typename A5
+    >
+    typename tetengo2::cpp0x::unique_ptr<T>::type make_unique(
+        A1&& arg1,
+        A2&& arg2,
+        A3&& arg3,
+        A4&& arg4,
+        A5&& arg5
+    )
+    {
+        return typename tetengo2::cpp0x::unique_ptr<T>::type(
+            new T(
+                std::forward<A1>(arg1),
+                std::forward<A2>(arg2),
+                std::forward<A3>(arg3),
+                std::forward<A4>(arg4),
+                std::forward<A5>(arg5)
+            )
+        );
+    }
+
+    /*!
+        \brief Makes a unique pointer.
+
+        \tparam T  A type.
+        \tparam A1 An argument type #1.
+        \tparam A2 An argument type #2.
+        \tparam A3 An argument type #3.
+        \tparam A4 An argument type #4.
+        \tparam A5 An argument type #5.
+        \tparam A6 An argument type #6.
+
+        \param arg1 An argument #1.
+        \param arg2 An argument #2.
+        \param arg3 An argument #3.
+        \param arg4 An argument #4.
+        \param arg5 An argument #5.
+        \param arg6 An argument #6.
+
+        \return A unique pointer
+    */
+    template <
+        typename T,
+        typename A1,
+        typename A2,
+        typename A3,
+        typename A4,
+        typename A5,
+        typename A6
+    >
+    typename tetengo2::cpp0x::unique_ptr<T>::type make_unique(
+        A1&& arg1,
+        A2&& arg2,
+        A3&& arg3,
+        A4&& arg4,
+        A5&& arg5,
+        A6&& arg6
+    )
+    {
+        return typename tetengo2::cpp0x::unique_ptr<T>::type(
+            new T(
+                std::forward<A1>(arg1),
+                std::forward<A2>(arg2),
+                std::forward<A3>(arg3),
+                std::forward<A4>(arg4),
+                std::forward<A5>(arg5),
+                std::forward<A6>(arg6)
+            )
+        );
+    }
+
+    /*!
+        \brief Makes a unique pointer.
+
+        \tparam T  A type.
+        \tparam A1 An argument type #1.
+        \tparam A2 An argument type #2.
+        \tparam A3 An argument type #3.
+        \tparam A4 An argument type #4.
+        \tparam A5 An argument type #5.
+        \tparam A6 An argument type #6.
+        \tparam A7 An argument type #7.
+
+        \param arg1 An argument #1.
+        \param arg2 An argument #2.
+        \param arg3 An argument #3.
+        \param arg4 An argument #4.
+        \param arg5 An argument #5.
+        \param arg6 An argument #6.
+        \param arg7 An argument #7.
+
+        \return A unique pointer
+    */
+    template <
+        typename T,
+        typename A1,
+        typename A2,
+        typename A3,
+        typename A4,
+        typename A5,
+        typename A6,
+        typename A7
+    >
+    typename tetengo2::cpp0x::unique_ptr<T>::type make_unique(
+        A1&& arg1,
+        A2&& arg2,
+        A3&& arg3,
+        A4&& arg4,
+        A5&& arg5,
+        A6&& arg6,
+        A7&& arg7
+    )
+    {
+        return typename tetengo2::cpp0x::unique_ptr<T>::type(
+            new T(
+                std::forward<A1>(arg1),
+                std::forward<A2>(arg2),
+                std::forward<A3>(arg3),
+                std::forward<A4>(arg4),
+                std::forward<A5>(arg5),
+                std::forward<A6>(arg6),
+                std::forward<A7>(arg7)
+            )
+        );
+    }
+
+    /*!
+        \brief Makes a unique pointer.
+
+        \tparam T  A type.
+        \tparam A1 An argument type #1.
+        \tparam A2 An argument type #2.
+        \tparam A3 An argument type #3.
+        \tparam A4 An argument type #4.
+        \tparam A5 An argument type #5.
+        \tparam A6 An argument type #6.
+        \tparam A7 An argument type #7.
+        \tparam A8 An argument type #8.
+
+        \param arg1 An argument #1.
+        \param arg2 An argument #2.
+        \param arg3 An argument #3.
+        \param arg4 An argument #4.
+        \param arg5 An argument #5.
+        \param arg6 An argument #6.
+        \param arg7 An argument #7.
+        \param arg8 An argument #8.
+
+        \return A unique pointer
+    */
+    template <
+        typename T,
+        typename A1,
+        typename A2,
+        typename A3,
+        typename A4,
+        typename A5,
+        typename A6,
+        typename A7,
+        typename A8
+    >
+    typename tetengo2::cpp0x::unique_ptr<T>::type make_unique(
+        A1&& arg1,
+        A2&& arg2,
+        A3&& arg3,
+        A4&& arg4,
+        A5&& arg5,
+        A6&& arg6,
+        A7&& arg7,
+        A8&& arg8
+    )
+    {
+        return typename tetengo2::cpp0x::unique_ptr<T>::type(
+            new T(
+                std::forward<A1>(arg1),
+                std::forward<A2>(arg2),
+                std::forward<A3>(arg3),
+                std::forward<A4>(arg4),
+                std::forward<A5>(arg5),
+                std::forward<A6>(arg6),
+                std::forward<A7>(arg7),
+                std::forward<A8>(arg8)
+            )
+        );
+    }
+
+    /*!
+        \brief Makes a unique pointer.
+
+        \tparam T  A type.
+        \tparam A1 An argument type #1.
+        \tparam A2 An argument type #2.
+        \tparam A3 An argument type #3.
+        \tparam A4 An argument type #4.
+        \tparam A5 An argument type #5.
+        \tparam A6 An argument type #6.
+        \tparam A7 An argument type #7.
+        \tparam A8 An argument type #8.
+        \tparam A9 An argument type #9.
+
+        \param arg1 An argument #1.
+        \param arg2 An argument #2.
+        \param arg3 An argument #3.
+        \param arg4 An argument #4.
+        \param arg5 An argument #5.
+        \param arg6 An argument #6.
+        \param arg7 An argument #7.
+        \param arg8 An argument #8.
+        \param arg9 An argument #9.
+
+        \return A unique pointer
+    */
+    template <
+        typename T,
+        typename A1,
+        typename A2,
+        typename A3,
+        typename A4,
+        typename A5,
+        typename A6,
+        typename A7,
+        typename A8,
+        typename A9
+    >
+    typename tetengo2::cpp0x::unique_ptr<T>::type make_unique(
+        A1&& arg1,
+        A2&& arg2,
+        A3&& arg3,
+        A4&& arg4,
+        A5&& arg5,
+        A6&& arg6,
+        A7&& arg7,
+        A8&& arg8,
+        A9&& arg9
+    )
+    {
+        return typename tetengo2::cpp0x::unique_ptr<T>::type(
+            new T(
+                std::forward<A1>(arg1),
+                std::forward<A2>(arg2),
+                std::forward<A3>(arg3),
+                std::forward<A4>(arg4),
+                std::forward<A5>(arg5),
+                std::forward<A6>(arg6),
+                std::forward<A7>(arg7),
+                std::forward<A8>(arg8),
+                std::forward<A9>(arg9)
+            )
+        );
+    }
+
+    /*!
+        \brief Makes a unique pointer.
+
+        \tparam T   A type.
+        \tparam A1  An argument type #1.
+        \tparam A2  An argument type #2.
+        \tparam A3  An argument type #3.
+        \tparam A4  An argument type #4.
+        \tparam A5  An argument type #5.
+        \tparam A6  An argument type #6.
+        \tparam A7  An argument type #7.
+        \tparam A8  An argument type #8.
+        \tparam A9  An argument type #9.
+        \tparam A10 An argument type #10.
+
+        \param arg1  An argument #1.
+        \param arg2  An argument #2.
+        \param arg3  An argument #3.
+        \param arg4  An argument #4.
+        \param arg5  An argument #5.
+        \param arg6  An argument #6.
+        \param arg7  An argument #7.
+        \param arg8  An argument #8.
+        \param arg9  An argument #9.
+        \param arg10 An argument #10.
+
+        \return A unique pointer
+    */
+    template <
+        typename T,
+        typename A1,
+        typename A2,
+        typename A3,
+        typename A4,
+        typename A5,
+        typename A6,
+        typename A7,
+        typename A8,
+        typename A9,
+        typename A10
+    >
+    typename tetengo2::cpp0x::unique_ptr<T>::type make_unique(
+        A1&&  arg1,
+        A2&&  arg2,
+        A3&&  arg3,
+        A4&&  arg4,
+        A5&&  arg5,
+        A6&&  arg6,
+        A7&&  arg7,
+        A8&&  arg8,
+        A9&&  arg9,
+        A10&& arg10
+    )
+    {
+        return typename tetengo2::cpp0x::unique_ptr<T>::type(
+            new T(
+                std::forward<A1>(arg1),
+                std::forward<A2>(arg2),
+                std::forward<A3>(arg3),
+                std::forward<A4>(arg4),
+                std::forward<A5>(arg5),
+                std::forward<A6>(arg6),
+                std::forward<A7>(arg7),
+                std::forward<A8>(arg8),
+                std::forward<A9>(arg9),
+                std::forward<A10>(arg10)
+            )
+        );
+    }
+
+    /*!
         \brief Upcasts a unique pointer.
 
         This cast operation is not necessary for std::unique_ptr but
--- tetengo2/test/test_tetengo2.unique.cpp
+++ tetengo2/test/test_tetengo2.unique.cpp
@@ -21,7 +21,247 @@ namespace
     struct Class1
     {
         int m_value1;
-        Class1(int value1) : m_value1(value1) {}
+        
+        Class1(const int value1)
+        :
+        m_value1(value1)
+        {}
+    };
+
+    struct Class2
+    {
+        int m_value1;
+        int m_value2;
+        
+        Class2(const int value1, const int value2)
+        :
+        m_value1(value1),
+        m_value2(value2)
+        {}
+    };
+
+    struct Class3
+    {
+        int m_value1;
+        int m_value2;
+        int m_value3;
+        
+        Class3(const int value1, const int value2, const int value3)
+        :
+        m_value1(value1),
+        m_value2(value2),
+        m_value3(value3)
+        {}
+    };
+
+    struct Class4
+    {
+        int m_value1;
+        int m_value2;
+        int m_value3;
+        int m_value4;
+        
+        Class4(
+            const int value1,
+            const int value2,
+            const int value3,
+            const int value4
+        )
+        :
+        m_value1(value1),
+        m_value2(value2),
+        m_value3(value3),
+        m_value4(value4)
+        {}
+    };
+
+    struct Class5
+    {
+        int m_value1;
+        int m_value2;
+        int m_value3;
+        int m_value4;
+        int m_value5;
+        
+        Class5(
+            const int value1,
+            const int value2,
+            const int value3,
+            const int value4,
+            const int value5
+        )
+        :
+        m_value1(value1),
+        m_value2(value2),
+        m_value3(value3),
+        m_value4(value4),
+        m_value5(value5)
+        {}
+    };
+
+    struct Class6
+    {
+        int m_value1;
+        int m_value2;
+        int m_value3;
+        int m_value4;
+        int m_value5;
+        int m_value6;
+        
+        Class6(
+            const int value1,
+            const int value2,
+            const int value3,
+            const int value4,
+            const int value5,
+            const int value6
+        )
+        :
+        m_value1(value1),
+        m_value2(value2),
+        m_value3(value3),
+        m_value4(value4),
+        m_value5(value5),
+        m_value6(value6)
+        {}
+    };
+
+    struct Class7
+    {
+        int m_value1;
+        int m_value2;
+        int m_value3;
+        int m_value4;
+        int m_value5;
+        int m_value6;
+        int m_value7;
+        
+        Class7(
+            const int value1,
+            const int value2,
+            const int value3,
+            const int value4,
+            const int value5,
+            const int value6,
+            const int value7
+        )
+        :
+        m_value1(value1),
+        m_value2(value2),
+        m_value3(value3),
+        m_value4(value4),
+        m_value5(value5),
+        m_value6(value6),
+        m_value7(value7)
+        {}
+    };
+
+    struct Class8
+    {
+        int m_value1;
+        int m_value2;
+        int m_value3;
+        int m_value4;
+        int m_value5;
+        int m_value6;
+        int m_value7;
+        int m_value8;
+        
+        Class8(
+            const int value1,
+            const int value2,
+            const int value3,
+            const int value4,
+            const int value5,
+            const int value6,
+            const int value7,
+            const int value8
+        )
+        :
+        m_value1(value1),
+        m_value2(value2),
+        m_value3(value3),
+        m_value4(value4),
+        m_value5(value5),
+        m_value6(value6),
+        m_value7(value7),
+        m_value8(value8)
+        {}
+    };
+
+    struct Class9
+    {
+        int m_value1;
+        int m_value2;
+        int m_value3;
+        int m_value4;
+        int m_value5;
+        int m_value6;
+        int m_value7;
+        int m_value8;
+        int m_value9;
+        
+        Class9(
+            const int value1,
+            const int value2,
+            const int value3,
+            const int value4,
+            const int value5,
+            const int value6,
+            const int value7,
+            const int value8,
+            const int value9
+        )
+        :
+        m_value1(value1),
+        m_value2(value2),
+        m_value3(value3),
+        m_value4(value4),
+        m_value5(value5),
+        m_value6(value6),
+        m_value7(value7),
+        m_value8(value8),
+        m_value9(value9)
+        {}
+    };
+
+    struct Class10
+    {
+        int m_value1;
+        int m_value2;
+        int m_value3;
+        int m_value4;
+        int m_value5;
+        int m_value6;
+        int m_value7;
+        int m_value8;
+        int m_value9;
+        int m_value10;
+        
+        Class10(
+            const int value1,
+            const int value2,
+            const int value3,
+            const int value4,
+            const int value5,
+            const int value6,
+            const int value7,
+            const int value8,
+            const int value9,
+            const int value10
+        )
+        :
+        m_value1(value1),
+        m_value2(value2),
+        m_value3(value3),
+        m_value4(value4),
+        m_value5(value5),
+        m_value6(value6),
+        m_value7(value7),
+        m_value8(value8),
+        m_value9(value9),
+        m_value10(value10)
+        {}
     };
 
     struct Base
@@ -54,6 +294,127 @@ BOOST_AUTO_TEST_SUITE(unique)
 
             BOOST_CHECK(p);
             BOOST_CHECK_EQUAL(p->m_value1, 42);
+        }
+        {
+            const tetengo2::cpp0x::unique_ptr<Class2>::type p(
+                tetengo2::make_unique<Class2>(12, 34)
+            );
+
+            BOOST_CHECK(p);
+            BOOST_CHECK_EQUAL(p->m_value1, 12);
+            BOOST_CHECK_EQUAL(p->m_value2, 34);
+        }
+        {
+            const tetengo2::cpp0x::unique_ptr<Class3>::type p(
+                tetengo2::make_unique<Class3>(12, 34, 56)
+            );
+
+            BOOST_CHECK(p);
+            BOOST_CHECK_EQUAL(p->m_value1, 12);
+            BOOST_CHECK_EQUAL(p->m_value2, 34);
+            BOOST_CHECK_EQUAL(p->m_value3, 56);
+        }
+        {
+            const tetengo2::cpp0x::unique_ptr<Class4>::type p(
+                tetengo2::make_unique<Class4>(12, 34, 56, 78)
+            );
+
+            BOOST_CHECK(p);
+            BOOST_CHECK_EQUAL(p->m_value1, 12);
+            BOOST_CHECK_EQUAL(p->m_value2, 34);
+            BOOST_CHECK_EQUAL(p->m_value3, 56);
+            BOOST_CHECK_EQUAL(p->m_value4, 78);
+        }
+        {
+            const tetengo2::cpp0x::unique_ptr<Class5>::type p(
+                tetengo2::make_unique<Class5>(12, 34, 56, 78, 90)
+            );
+
+            BOOST_CHECK(p);
+            BOOST_CHECK_EQUAL(p->m_value1, 12);
+            BOOST_CHECK_EQUAL(p->m_value2, 34);
+            BOOST_CHECK_EQUAL(p->m_value3, 56);
+            BOOST_CHECK_EQUAL(p->m_value4, 78);
+            BOOST_CHECK_EQUAL(p->m_value5, 90);
+        }
+        {
+            const tetengo2::cpp0x::unique_ptr<Class6>::type p(
+                tetengo2::make_unique<Class6>(12, 34, 56, 78, 90, 12)
+            );
+
+            BOOST_CHECK(p);
+            BOOST_CHECK_EQUAL(p->m_value1, 12);
+            BOOST_CHECK_EQUAL(p->m_value2, 34);
+            BOOST_CHECK_EQUAL(p->m_value3, 56);
+            BOOST_CHECK_EQUAL(p->m_value4, 78);
+            BOOST_CHECK_EQUAL(p->m_value5, 90);
+            BOOST_CHECK_EQUAL(p->m_value6, 12);
+        }
+        {
+            const tetengo2::cpp0x::unique_ptr<Class7>::type p(
+                tetengo2::make_unique<Class7>(12, 34, 56, 78, 90, 12, 34)
+            );
+
+            BOOST_CHECK(p);
+            BOOST_CHECK_EQUAL(p->m_value1, 12);
+            BOOST_CHECK_EQUAL(p->m_value2, 34);
+            BOOST_CHECK_EQUAL(p->m_value3, 56);
+            BOOST_CHECK_EQUAL(p->m_value4, 78);
+            BOOST_CHECK_EQUAL(p->m_value5, 90);
+            BOOST_CHECK_EQUAL(p->m_value6, 12);
+            BOOST_CHECK_EQUAL(p->m_value7, 34);
+        }
+        {
+            const tetengo2::cpp0x::unique_ptr<Class8>::type p(
+                tetengo2::make_unique<Class8>(12, 34, 56, 78, 90, 12, 34, 56)
+            );
+
+            BOOST_CHECK(p);
+            BOOST_CHECK_EQUAL(p->m_value1, 12);
+            BOOST_CHECK_EQUAL(p->m_value2, 34);
+            BOOST_CHECK_EQUAL(p->m_value3, 56);
+            BOOST_CHECK_EQUAL(p->m_value4, 78);
+            BOOST_CHECK_EQUAL(p->m_value5, 90);
+            BOOST_CHECK_EQUAL(p->m_value6, 12);
+            BOOST_CHECK_EQUAL(p->m_value7, 34);
+            BOOST_CHECK_EQUAL(p->m_value8, 56);
+        }
+        {
+            const tetengo2::cpp0x::unique_ptr<Class9>::type p(
+                tetengo2::make_unique<Class9>(
+                    12, 34, 56, 78, 90, 12, 34, 56, 78
+                )
+            );
+
+            BOOST_CHECK(p);
+            BOOST_CHECK_EQUAL(p->m_value1, 12);
+            BOOST_CHECK_EQUAL(p->m_value2, 34);
+            BOOST_CHECK_EQUAL(p->m_value3, 56);
+            BOOST_CHECK_EQUAL(p->m_value4, 78);
+            BOOST_CHECK_EQUAL(p->m_value5, 90);
+            BOOST_CHECK_EQUAL(p->m_value6, 12);
+            BOOST_CHECK_EQUAL(p->m_value7, 34);
+            BOOST_CHECK_EQUAL(p->m_value8, 56);
+            BOOST_CHECK_EQUAL(p->m_value9, 78);
+        }
+        {
+            const tetengo2::cpp0x::unique_ptr<Class10>::type p(
+                tetengo2::make_unique<Class10>(
+                    12, 34, 56, 78, 90, 12, 34, 56, 78, 90
+                )
+            );
+
+            BOOST_CHECK(p);
+            BOOST_CHECK_EQUAL(p->m_value1, 12);
+            BOOST_CHECK_EQUAL(p->m_value2, 34);
+            BOOST_CHECK_EQUAL(p->m_value3, 56);
+            BOOST_CHECK_EQUAL(p->m_value4, 78);
+            BOOST_CHECK_EQUAL(p->m_value5, 90);
+            BOOST_CHECK_EQUAL(p->m_value6, 12);
+            BOOST_CHECK_EQUAL(p->m_value7, 34);
+            BOOST_CHECK_EQUAL(p->m_value8, 56);
+            BOOST_CHECK_EQUAL(p->m_value9, 78);
+            BOOST_CHECK_EQUAL(p->m_value10, 90);
         }
     }
 
