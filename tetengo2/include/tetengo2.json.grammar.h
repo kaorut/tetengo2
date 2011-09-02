bbe4397df51cc0aaf504aa7c9e2285fdd0bf0af6
kaoru <kaorut@mc.pikara.ne.jp>
2011-09-03 00:23:59 +0900

    Created tetengo2::json::grammar.

---
+++ tetengo2/include/tetengo2.json.grammar.h
@@ -0,0 +1,45 @@
+/*! \file
+    \brief The definition of tetengo2::json::grammar.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_JSON_GRAMMAR_H)
+#define TETENGO2_JSON_GRAMMAR_H
+
+
+namespace tetengo2 { namespace json
+{
+    /*!
+        \brief The class template for a grammar.
+    */
+    class grammar
+    {
+    public:
+        // types
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a grammar.
+        */
+        grammar()
+        {}
+
+
+        // functions
+
+
+    private:
+
+
+    };
+
+
+}}
+
+
+#endif
