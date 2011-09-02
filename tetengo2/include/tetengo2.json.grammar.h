80443764f64f80e292bb2208f0583ef9307b4a37
kaoru <kaorut@mc.pikara.ne.jp>
2011-09-03 00:24:42 +0900

    Implemented tetengo2::json::grammar.

--- tetengo2/include/tetengo2.json.grammar.h
+++ tetengo2/include/tetengo2.json.grammar.h
@@ -9,16 +9,47 @@
 #if !defined(TETENGO2_JSON_GRAMMAR_H)
 #define TETENGO2_JSON_GRAMMAR_H
 
+#include <cassert>
+#include <string>
+
+#include <boost/spirit/include/qi.hpp>
+#include <boost/utility.hpp>
+
+#include "tetengo2.cpp0x.h"
+#include "tetengo2.text.h"
+
 
 namespace tetengo2 { namespace json
 {
     /*!
         \brief The class template for a grammar.
+
+        The grammar of JSON is based on RFC4627.
+        http://www.ietf.org/rfc/rfc4627.txt
+
+        \tparam ForwardIterator A forward iterator type.
     */
-    class grammar
+    template <typename ForwardIterator>
+    class grammar :
+        public boost::spirit::qi::grammar<
+            ForwardIterator,
+            std::basic_string<typename ForwardIterator::value_type> ()
+        >
     {
     public:
         // types
+
+        //! The iterator type.
+        typedef ForwardIterator iterator;
+
+        //! The character type.
+        typedef typename iterator::value_type char_type;
+
+        //! The string type.
+        typedef std::basic_string<char_type> string_type;
+
+        //! The rule type.
+        typedef boost::spirit::qi::rule<iterator, string_type ()> rule_type;
 
 
         // constructors and destructor
@@ -27,13 +58,74 @@ namespace tetengo2 { namespace json
             \brief Creates a grammar.
         */
         grammar()
-        {}
+        :
+        grammar::base_type(m_json, "json")
+        {
+            namespace qi = boost::spirit::qi;
+
+            m_json = qi::string("dummy");
+
+            m_string =
+                m_quotation_mark >> *m_char >> m_quotation_mark;
+            m_char =
+                m_unescaped |
+                (
+                    m_escape >> (
+                        qi::char_(char_type(TETENGO2_TEXT('"'))) |
+                        qi::char_(char_type(TETENGO2_TEXT('\\'))) |
+                        qi::char_(char_type(TETENGO2_TEXT('/'))) |
+                        qi::char_(char_type(TETENGO2_TEXT('b'))) |
+                        qi::char_(char_type(TETENGO2_TEXT('f'))) |
+                        qi::char_(char_type(TETENGO2_TEXT('n'))) |
+                        qi::char_(char_type(TETENGO2_TEXT('r'))) |
+                        qi::char_(char_type(TETENGO2_TEXT('t'))) |
+                        (
+                            qi::char_(char_type(TETENGO2_TEXT('n'))) >>
+                            qi::repeat(4)[qi::xdigit]
+                        )
+                    )
+                );
+            m_escape =
+                char_type(TETENGO2_TEXT('\\'));
+            m_quotation_mark =
+                char_type(TETENGO2_TEXT('"'));
+            m_unescaped =
+                qi::char_ -
+                qi::char_(char_type(0x00), char_type(0x19)) -
+                m_quotation_mark -
+                m_escape;
+        }
 
 
         // functions
 
+        const rule_type& string()
+        const
+        {
+            return m_string;
+        }
+
 
     private:
+        // types
+
+        typedef
+            boost::spirit::qi::rule<iterator, char_type ()> char_rule_type;
+
+
+        // variables
+
+        rule_type m_json;
+
+        rule_type m_string;
+
+        rule_type m_char;
+
+        rule_type m_escape;
+
+        char_rule_type m_quotation_mark;
+
+        char_rule_type m_unescaped;
 
 
     };
--- tetengo2/test/test_tetengo2.json.grammar.cpp
+++ tetengo2/test/test_tetengo2.json.grammar.cpp
@@ -6,9 +6,34 @@
     $Id$
 */
 
+#include <string>
+
+#include <boost/spirit/include/qi.hpp>
 #include <boost/test/unit_test.hpp>
 
 #include "tetengo2.json.grammar.h"
+
+
+namespace
+{
+    // functions
+
+    template <typename ForwardIterator, typename Parser, typename Attribute>
+    bool full_match(
+        const ForwardIterator first,
+        const ForwardIterator last,
+        const Parser&         parser,
+        Attribute&            output
+    )
+    {
+        ForwardIterator mutable_first = first;
+        const bool result =
+            boost::spirit::qi::phrase_parse(
+                mutable_first, last, parser, boost::spirit::qi::space, output
+            );
+        return result && mutable_first == last;
+    }
+}
 
 
 BOOST_AUTO_TEST_SUITE(test_tetengo2)
@@ -20,7 +45,31 @@ BOOST_AUTO_TEST_SUITE(grammar)
     {
         BOOST_TEST_PASSPOINT();
 
+        const tetengo2::json::grammar<std::string::const_iterator> grammar;
+    }
+
+    BOOST_AUTO_TEST_CASE(parse)
+    {
+        BOOST_TEST_PASSPOINT();
+
         BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+    BOOST_AUTO_TEST_CASE(string)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const std::string input("\"hoge\"");
+
+            const tetengo2::json::grammar<std::string::const_iterator> g;
+            std::string output;
+            const bool result =
+                full_match(input.begin(), input.end(), g.string(), output);
+
+            BOOST_CHECK(result);
+            BOOST_CHECK(output == "\"hoge\"");
+        }
     }
 
 
