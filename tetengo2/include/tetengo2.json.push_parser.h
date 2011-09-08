94378b1361aad785ecc8ad4bf0d53c95007a0b3f
kaoru <kaorut@mc.pikara.ne.jp>
2011-09-09 00:54:39 +0900

    Created tetengo2::json::push_parser.

---
+++ tetengo2/include/tetengo2.json.push_parser.h
@@ -0,0 +1,48 @@
+/*! \file
+    \brief The definition of tetengo2::json::push_parser.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_JSON_PUSHPARSER_H)
+#define TETENGO2_JSON_PUSHPARSER_H
+
+
+namespace tetengo2 { namespace json
+{
+    /*!
+        \brief The class template for a push parser.
+
+        \tparam ForwardIterator A forward iterator type.
+    */
+    template <typename ForwardIterator>
+    class push_parser
+    {
+    public:
+        // types
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a push parser.
+        */
+        push_parser()
+        {}
+
+
+        // functions
+        
+    private:
+        // variables
+
+    
+    };
+
+
+}}
+
+
+#endif
