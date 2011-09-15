34d973a9a7247c89d4a1e9fd178242d26eef3e42
kaoru <kaorut@mc.pikara.ne.jp>
2011-09-16 00:08:50 +0900

    Implemented tetengo2::json::push_parser.

--- tetengo2/include/tetengo2.json.push_parser.h
+++ tetengo2/include/tetengo2.json.push_parser.h
@@ -9,6 +9,10 @@
 #if !defined(TETENGO2_JSON_PUSHPARSER_H)
 #define TETENGO2_JSON_PUSHPARSER_H
 
+#include <utility>
+
+#include <boost/noncopyable.hpp>
+
 
 namespace tetengo2 { namespace json
 {
@@ -16,27 +20,61 @@ namespace tetengo2 { namespace json
         \brief The class template for a push parser.
 
         \tparam ForwardIterator A forward iterator type.
+        \tparam Grammar         A grammar type.
     */
-    template <typename ForwardIterator>
-    class push_parser
+    template <typename ForwardIterator, typename Grammar>
+    class push_parser : private boost::noncopyable
     {
     public:
         // types
+
+        //! The iterator type.
+        typedef ForwardIterator iterator;
+
+        //! The grammar type.
+        typedef Grammar grammar_type;
 
 
         // constructors and destructor
 
         /*!
             \brief Creates a push parser.
+
+            \tparam G A grammar type.
+
+            \param first   A first iterator to a parsed range.
+            \param last    A last iterator to a parsed range.
+            \param grammar A grammer.
         */
-        push_parser()
+        template <typename G>
+        push_parser(const iterator first, const iterator last, G&& grammar)
+        :
+        m_first(first),
+        m_last(last),
+        m_grammer(std::forward<G>(grammar))
         {}
 
 
         // functions
         
+        /*!
+            \brief Parses the range.
+        */
+        void parse()
+        const
+        {
+
+        }
+
+
     private:
         // variables
+
+        const iterator m_first;
+
+        const iterator m_last;
+
+        grammar_type m_grammar;
 
     
     };
