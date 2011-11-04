a634e3bb3f6108258162aad95391a311e9a0c81c
kaoru <kaorut@mc.pikara.ne.jp>
2011-11-05 07:45:36 +0900

    Created tetengo2::json::pull_parser.

---
+++ tetengo2/include/tetengo2.json.pull_parser.h
@@ -0,0 +1,42 @@
+/*! \file
+    \brief The definition of tetengo2::json::pull_parser.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_JSON_PULLPARSER_H)
+#define TETENGO2_JSON_PULLPARSER_H
+
+#include <boost/noncopyable.hpp>
+
+
+namespace tetengo2 { namespace json
+{
+    /*!
+        \brief The class template for a pull parser.
+    */
+    class pull_parser : private boost::noncopyable
+    {
+    public:
+        // types
+
+
+        // constructors and destructor
+
+
+        // functions
+
+
+    private:
+        // variables
+
+
+    };
+
+
+}}
+
+
+#endif
---
+++ tetengo2/test/test_tetengo2.json.pull_parser.cpp
@@ -0,0 +1,37 @@
+/*! \file
+    \brief Test of class tetengo2::json::pull_parser.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include "tetengo2.json.pull_parser.h"
+
+
+namespace
+{
+    // types
+
+
+}
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo2)
+BOOST_AUTO_TEST_SUITE(json)
+BOOST_AUTO_TEST_SUITE(pull_parser)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+    
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
