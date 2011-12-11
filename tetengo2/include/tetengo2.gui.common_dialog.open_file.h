e3a6a363b3a5028d0feaf071322b4f922d286f89
kaoru <kaorut@mc.pikara.ne.jp>
2011-12-11 17:43:33 +0900

    Created tetengo2::gui::common_dialog::open_file.

---
+++ tetengo2/include/tetengo2.gui.common_dialog.open_file.h
@@ -0,0 +1,60 @@
+/*! \file
+    \brief The definition of tetengo2::gui::common_dialog::open_file.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_GUI_COMMONDIALOG_OPENFILE_H)
+#define TETENGO2_GUI_COMMONDIALOG_OPENFILE_H
+
+#include <boost/noncopyable.hpp>
+
+
+namespace tetengo2 { namespace gui { namespace common_dialog
+{
+    /*!
+        \brief The class template for a open-file dialog box.
+
+        \tparam String A string type.
+        \tparam Path   A path type.
+    */
+    template <typename String, typename Path>
+    class open_file : private boost::noncopyable
+    {
+    public:
+        // types
+
+        //! The string type.
+        typedef String string_type;
+
+        //! The path type.
+        typedef Path path_type;
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a open-file dialog box.
+        */
+        open_file()
+        {}
+
+
+        // functions
+
+
+
+    private:
+        // variables
+
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ tetengo2/test/test_tetengo2.gui.common_dialog.open_file.cpp
@@ -0,0 +1,39 @@
+/*! \file
+    \brief Test of class tetengo2::gui::common_dialog::open_file.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include "tetengo2.gui.common_dialog.open_file.h"
+
+
+namespace
+{
+    // types
+
+
+}
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo2)
+BOOST_AUTO_TEST_SUITE(gui)
+BOOST_AUTO_TEST_SUITE(common_dialog)
+BOOST_AUTO_TEST_SUITE(open_file)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
