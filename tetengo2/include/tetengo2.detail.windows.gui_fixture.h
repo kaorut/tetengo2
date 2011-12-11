04cb29ad620ec247b935721edb3643ab533d5927
kaoru <kaorut@mc.pikara.ne.jp>
2011-12-11 17:08:01 +0900

    Added NDEBUG macro definitions. Modified tetengo2::detail::windows::gui_fixture.

--- tetengo2/include/tetengo2.detail.windows.gui_fixture.h
+++ tetengo2/include/tetengo2.detail.windows.gui_fixture.h
@@ -22,6 +22,7 @@
 #include <ObjBase.h>
 
 #include "tetengo2.cpp11.h"
+#include "tetengo2.detail.windows.windows_version.h"
 
 
 namespace tetengo2 { namespace detail { namespace windows
@@ -39,6 +40,7 @@ namespace tetengo2 { namespace detail { namespace windows
         */
         gui_fixture()
         {
+            check_platform();
             setup_common_controls();
             setup_com();
         }
@@ -46,6 +48,18 @@ namespace tetengo2 { namespace detail { namespace windows
 
     private:
         // static functions
+
+        static void check_platform()
+        {
+            if (!on_windows_vista_or_later())
+            {
+                BOOST_THROW_EXCEPTION(
+                    std::runtime_error(
+                        "This program can't run on this platform."
+                    )
+                );
+            }
+        }
 
         static void setup_common_controls()
         {
