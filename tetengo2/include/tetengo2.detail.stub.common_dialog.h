3136a4956ba84a34921c70e16358de7ea2cb2db6
kaoru <kaorut@mc.pikara.ne.jp>
2011-12-11 17:57:03 +0900

    Created tetengo2::detail::stub/windows::common_dialog.

---
+++ tetengo2/include/tetengo2.detail.stub.common_dialog.h
@@ -0,0 +1,34 @@
+/*! \file
+    \brief The definition of tetengo2::detail::stub::common_dialog.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_STUB_COMMONDIALOG_H)
+#define TETENGO2_DETAIL_STUB_COMMONDIALOG_H
+
+
+namespace tetengo2 { namespace detail { namespace stub
+{
+    /*!
+        \brief The class for a detail implementation of common dialog boxes.
+    */
+    class common_dialog
+    {
+    public:
+        // types
+
+
+        // static functions
+
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ tetengo2/include/tetengo2.detail.windows.common_dialog.h
@@ -0,0 +1,40 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::common_dialog.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_WINDOWS_COMMONDIALOG_H)
+#define TETENGO2_DETAIL_WINDOWS_COMMONDIALOG_H
+
+#define NOMINMAX
+#define OEMRESOURCE
+#include <Windows.h>
+
+
+namespace tetengo2 { namespace detail { namespace windows
+{
+    /*!
+        \brief The class for a detail implementation of common dialog boxes.
+    */
+    class common_dialog
+    {
+    public:
+        // types
+
+
+        // static functions
+
+
+    private:
+
+
+    };
+
+
+}}}
+
+
+#endif
