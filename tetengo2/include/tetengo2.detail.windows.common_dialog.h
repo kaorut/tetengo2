6c6074e41560b1a8db7ba7d5bd057f01adea8a6a
kaoru <kaorut@mc.pikara.ne.jp>
2011-12-16 04:55:14 +0900

    Implemented tetengo2::detail::stub/windows::common_dialog.

--- tetengo2/include/tetengo2.detail.windows.common_dialog.h
+++ tetengo2/include/tetengo2.detail.windows.common_dialog.h
@@ -17,18 +17,76 @@
 namespace tetengo2 { namespace detail { namespace windows
 {
     /*!
-        \brief The class for a detail implementation of common dialog boxes.
+        \brief The class template for a detail implementation of a file open
+                   dialog box.
+        
+        \tparam Widget  A widget type.
+        \tparam String  A string type.
+        \tparam Path    A path type.
+        \tparam Encoder An encoder type.
     */
-    class common_dialog
+    template <
+        typename Widget,
+        typename String,
+        typename Path,
+        typename Encoder
+    >
+    class file_open
     {
     public:
         // types
 
+        //! The widget type.
+        typedef Widget widget_type;
 
-        // static functions
+        //! The string type.
+        typedef String string_type;
+
+        //! The path type.
+        typedef Path path_type;
+
+        //! The encoder type.
+        typedef Encoder encoder_type;
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a detail implementation of a file open dialog box.
+
+            \param parent A parent widget.
+        */
+        file_open(widget_type& parent)
+        :
+        m_parent(parent),
+        m_path()
+        {}
+
+
+        // functions
+
+        /*!
+            \brief Returns the result.
+        */
+        const path_type& result()
+        const
+        {
+            return m_result;
+        }
+
+        /*!
+            \brief Shows the dialog box as modal.
+        */
+        void do_modal()
+        {}
 
 
     private:
+        // variables
+
+        widget_type& m_parent;
+
+        path_type m_path;
 
 
     };
