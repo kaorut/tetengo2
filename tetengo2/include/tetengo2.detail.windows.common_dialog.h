0d4be14b30ec21364463750b39c4b1f92e92f404
kaoru <kaorut@mc.pikara.ne.jp>
2011-12-18 13:56:33 +0900

    Implemented tetengo2::detail::windiws::common_dialog.

--- tetengo2/include/tetengo2.detail.windows.common_dialog.h
+++ tetengo2/include/tetengo2.detail.windows.common_dialog.h
@@ -11,7 +11,9 @@
 
 //#include <memory>
 #include <stdexcept>
+#include <utility>
 
+#include <boost/scope_exit.hpp>
 #include <boost/throw_exception.hpp>
 
 #define NOMINMAX
@@ -34,10 +36,12 @@ namespace tetengo2 { namespace detail { namespace windows
         // types
 
         //! The file open dialog details type.
-        typedef ::IFileOpenDialog file_open_dialog_details_type;
+        typedef
+            std::pair<ATL::CComPtr< ::IFileOpenDialog>, ::HWND>
+            file_open_dialog_details_type;
 
         //! The file open dialog details pointer type.
-        typedef ATL::CComPtr<file_open_dialog_details_type>
+        typedef std::unique_ptr<file_open_dialog_details_type>
             file_open_dialog_details_ptr_type;
 
         
@@ -57,7 +61,7 @@ namespace tetengo2 { namespace detail { namespace windows
             Widget& parent
         )
         {
-            file_open_dialog_details_ptr_type p_dialog;
+            ATL::CComPtr< ::IFileOpenDialog> p_dialog;
 
             const ::HRESULT creation_result =
                 p_dialog.CoCreateInstance(__uuidof(::FileOpenDialog));
@@ -68,7 +72,9 @@ namespace tetengo2 { namespace detail { namespace windows
                 );
             }
 
-            return p_dialog;
+            return tetengo2::make_unique<file_open_dialog_details_type>(
+                p_dialog, parent.details()->first.get()
+            );
         }
 
         /*!
@@ -85,15 +91,35 @@ namespace tetengo2 { namespace detail { namespace windows
             file_open_dialog_details_type& dialog
         )
         {
-            const ::HRESULT showing_result = dialog.Show(NULL);
+            const ::HRESULT showing_result =
+                dialog.first->Show(dialog.second);
             if (!SUCCEEDED(showing_result))
+                return Path();
+
+            ATL::CComPtr< ::IShellItem> p_item;
+            const ::HRESULT result_result = dialog.first->GetResult(&p_item);
+            if (!SUCCEEDED(result_result))
             {
                 BOOST_THROW_EXCEPTION(
-                    std::runtime_error("Can't show the file open dialog.")
+                    std::runtime_error("Can't get the result.")
                 );
             }
 
-            return Path(L"hoge.txt");
+            wchar_t* file_name = NULL;
+            const ::HRESULT file_title_result =
+                p_item->GetDisplayName(SIGDN_FILESYSPATH, &file_name);
+            if (!SUCCEEDED(file_title_result))
+            {
+                BOOST_THROW_EXCEPTION(
+                    std::runtime_error("Can't get the file path.")
+                );
+            }
+            BOOST_SCOPE_EXIT((file_name))
+            {
+                ::CoTaskMemFree(file_name);
+            } BOOST_SCOPE_EXIT_END;
+
+            return Path(file_name);
         }
 
 
