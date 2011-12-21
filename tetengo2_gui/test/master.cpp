01575c4de3681f639eb6c55e243cf88836024499
kaoru <kaorut@mc.pikara.ne.jp>
2011-12-22 03:01:02 +0900

    Created an empty project tetengo2_gui.

---
+++ tetengo2_gui/test/master.cpp
@@ -0,0 +1,10 @@
+/*! \file
+    \brief The master test suite for tetengo2_gui.
+
+    Copyright (C) 2007-2011 kaoru
+
+    $Id$
+*/
+
+#define BOOST_TEST_MODULE test_tetengo2_gui
+#include <boost/test/unit_test.hpp>
