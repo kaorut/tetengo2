e27b4092b14b0cdcd1a34cd4a50ecd5747430420
kaoru <kaorut@mc.pikara.ne.jp>
2012-01-14 16:56:24 +0900

    Created bobura::command::set.

---
+++ bobura/src/bobura.command.set.h
@@ -0,0 +1,36 @@
+/*! \file
+    \brief The definition of bobura::command::set.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_COMMAND_SET_H)
+#define BOBURA_COMMAND_SET_H
+
+
+namespace bobura { namespace command
+{
+    /*!
+        \brief The class for a command set.
+    */
+    class set
+    {
+    public:
+        // constructors
+
+        /*!
+            \brief Creates a command set.
+        */
+        set()
+        {}
+
+
+    };
+
+
+}}
+
+
+#endif
---
+++ bobura/test/test_bobura.command.set.cpp
@@ -0,0 +1,29 @@
+/*! \file
+    \brief Test of class bobura::command::set.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include "bobura.command.set.h"
+
+
+BOOST_AUTO_TEST_SUITE(test_bobura)
+BOOST_AUTO_TEST_SUITE(command)
+BOOST_AUTO_TEST_SUITE(set)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
