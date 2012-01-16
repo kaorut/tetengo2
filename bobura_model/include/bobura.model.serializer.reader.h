f5befc9b0b1a957be3be85663a0ce642d9a7001a
kaoru <kaorut@mc.pikara.ne.jp>
2012-01-16 22:50:24 +0900

    Created bobura::model::serializer::reader.

---
+++ bobura_model/include/bobura.model.serializer.reader.h
@@ -0,0 +1,113 @@
+/*! \file
+    \brief The definition of bobura::model::station.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_MODEL_STATION_H)
+#define BOBURA_MODEL_STATION_H
+
+#include <utility>
+
+#include <boost/operators.hpp>
+
+#include <tetengo2.cpp11.h>
+
+
+namespace bobura { namespace model
+{
+    /*!
+        \brief The class template for a station.
+
+        \tparam Name  A name type.
+        \tparam Grade A grade type.
+    */
+    template <typename Name, typename Grade>
+    class station : private boost::equality_comparable<station<Name, Grade>>
+    {
+    public:
+        // types
+
+        //! The name type.
+        typedef Name name_type;
+
+        //! The grade type.
+        typedef Grade grade_type;
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a station.
+
+            \tparam N A name type.
+
+            \param name  A name.
+            \param grade A grade.
+        */
+        template <typename N>
+        station(N&& name, const grade_type& grade)
+        :
+        m_name(std::forward<N>(name)),
+        m_grade(grade)
+        {}
+
+
+        // functions
+
+        /*!
+            \brief Checks whether one station is equal to another.
+
+            \param one     One station.
+            \param another Another station.
+
+            \retval true  When the one is equal to the other.
+            \retval false Otherwise.
+        */
+        friend bool operator==(const station& one, const station& another)
+        {
+            return one.m_name == another.m_name &&
+                   &one.m_grade.get() == &another.m_grade.get();
+        }
+
+        /*!
+            \brief Returns the name.
+
+            \return The name.
+        */
+        const name_type& name()
+        const
+        {
+            return m_name;
+        }
+
+        /*!
+            \brief Returns the grade.
+
+            \return The grade.
+        */
+        const grade_type& grade()
+        const
+        {
+            return m_grade;
+        }
+
+
+    private:
+        // variables
+
+        name_type m_name;
+
+        typename tetengo2::cpp11::reference_wrapper<const grade_type>::type
+        m_grade;
+
+
+    };
+
+
+}}
+
+
+#endif
---
+++ bobura_model/test/test_bobura.model.serializer.reader.cpp
@@ -0,0 +1,207 @@
+/*! \file
+    \brief Test of class bobura::model::station.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#include <string>
+
+#include <boost/test/unit_test.hpp>
+
+#include "bobura.model.station_info.grade.h"
+
+#include "bobura.model.station.h"
+
+
+BOOST_AUTO_TEST_SUITE(test_bobura)
+BOOST_AUTO_TEST_SUITE(model)
+BOOST_AUTO_TEST_SUITE(station)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        typedef bobura::model::station_info::grade<std::wstring> grade_type;
+        typedef bobura::model::station_info::local<std::wstring> local_type;
+        typedef
+            bobura::model::station_info::principal<std::wstring>
+            principal_type;
+
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                L"", local_type::instance()
+            );
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                L"A", local_type::instance()
+            );
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                L"AB", local_type::instance()
+            );
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                std::wstring(1024 * 1024, L'X'), local_type::instance()
+            );
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                L"", principal_type::instance()
+            );
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                L"A", principal_type::instance()
+            );
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                L"AB", principal_type::instance()
+            );
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                std::wstring(1024 * 1024, L'X'), local_type::instance()
+            );
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(operator_equal)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        typedef bobura::model::station_info::grade<std::wstring> grade_type;
+        typedef bobura::model::station_info::local<std::wstring> local_type;
+        typedef
+            bobura::model::station_info::principal<std::wstring>
+            principal_type;
+
+        {
+            const bobura::model::station<std::wstring, grade_type> station1(
+                L"A", local_type::instance()
+            );
+            const bobura::model::station<std::wstring, grade_type> station2(
+                L"A", local_type::instance()
+            );
+
+            BOOST_CHECK(station1 == station2);
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station1(
+                L"B", principal_type::instance()
+            );
+            const bobura::model::station<std::wstring, grade_type> station2(
+                L"B", principal_type::instance()
+            );
+
+            BOOST_CHECK(station1 == station2);
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station1(
+                L"A", local_type::instance()
+            );
+            const bobura::model::station<std::wstring, grade_type> station2(
+                L"B", principal_type::instance()
+            );
+
+            BOOST_CHECK(station1 != station2);
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(name)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        typedef bobura::model::station_info::grade<std::wstring> grade_type;
+        typedef bobura::model::station_info::local<std::wstring> local_type;
+
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                L"", local_type::instance()
+            );
+
+            BOOST_CHECK(station.name() == std::wstring(L""));
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                L"A", local_type::instance()
+            );
+
+            BOOST_CHECK(station.name() == std::wstring(L"A"));
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                L"AB", local_type::instance()
+            );
+
+            BOOST_CHECK(station.name() == std::wstring(L"AB"));
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                std::wstring(1024 * 1024, L'X'), local_type::instance()
+            );
+
+            BOOST_CHECK(station.name() == std::wstring(1024 * 1024, L'X'));
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(grade)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        typedef bobura::model::station_info::grade<std::wstring> grade_type;
+        typedef bobura::model::station_info::local<std::wstring> local_type;
+        typedef
+            bobura::model::station_info::principal<std::wstring>
+            principal_type;
+        typedef
+            bobura::model::station_info::local_terminal<std::wstring>
+            local_terminal_type;
+        typedef
+            bobura::model::station_info::principal_terminal<std::wstring>
+            principal_terminal_type;
+
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                L"", local_type::instance()
+            );
+
+            BOOST_CHECK_EQUAL(&station.grade(), &local_type::instance());
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                L"", principal_type::instance()
+            );
+
+            BOOST_CHECK_EQUAL(&station.grade(), &principal_type::instance());
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                L"", local_terminal_type::instance()
+            );
+
+            BOOST_CHECK_EQUAL(
+                &station.grade(), &local_terminal_type::instance()
+            );
+        }
+        {
+            const bobura::model::station<std::wstring, grade_type> station(
+                L"", principal_terminal_type::instance()
+            );
+
+            BOOST_CHECK_EQUAL(
+                &station.grade(), &principal_terminal_type::instance()
+            );
+        }
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
