8e3301cf5d130aff882419c239d5729042bf036b
kaoru <kaorut@mc.pikara.ne.jp>
2012-01-16 22:57:10 +0900

    Created bobura::model::serializer::json_reader.

---
+++ bobura_model/include/bobura.model.serializer.json_reader.h
@@ -0,0 +1,73 @@
+/*! \file
+    \brief The definition of bobura::model::serializer::json_reader.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_MODEL_SERIALIZER_JSONREADER_H)
+#define BOBURA_MODEL_SERIALIZER_JSONREADER_H
+
+#include <memory>
+
+#include <boost/noncopyable.hpp>
+#include <tetengo2.cpp11.h>
+
+#include "bobura.model.serializer.reader.h"
+
+
+namespace bobura { namespace model { namespace serializer
+{
+    /*!
+        \brief The class template for a JSON reader.
+
+        \tparam Timetable A timetable type.
+    */
+    template <typename Timetable>
+    class json_reader : public reader<Timetable>
+    {
+    public:
+        // types
+
+        //! The timetable type.
+        typedef Timetable timetable_type;
+
+        //! The base type.
+        typedef reader<timetable_type> base_type;
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a JSON reader.
+        */
+        json_reader()
+        :
+        base_type()
+        {}
+
+        /*!
+            \brief Destroys the json_reader.
+        */
+        virtual ~json_reader()
+        TETENGO2_CPP11_NOEXCEPT
+        {}
+
+
+    private:
+        // virtual functions
+
+        virtual std::unique_ptr<timetable_type> read_impl()
+        {
+            return std::unique_ptr<timetable_type>();
+        }
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ bobura_model/test/test_bobura.model.serializer.json_reader.cpp
@@ -0,0 +1,83 @@
+/*! \file
+    \brief Test of class bobura::model::serializer::json_reader.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#include <cstddef>
+#include <memory>
+#include <string>
+
+#include <boost/test/unit_test.hpp>
+
+#include <tetengo2.cpp11.h>
+
+#include "bobura.model.station.h"
+#include "bobura.model.station_info.grade.h"
+#include "bobura.model.timetable.h"
+#include "bobura.model.timetable_info.station_location.h"
+#include "bobura.model.train.h"
+#include "bobura.model.train_info.stop.h"
+#include "bobura.model.train_info.time.h"
+#include "bobura.model.train_info.time_span.h"
+
+#include "bobura.model.serializer.json_reader.h"
+
+
+namespace
+{
+    // types
+
+    typedef bobura::model::station_info::local<std::wstring> local_type;
+    typedef bobura::model::station<std::wstring, local_type> station_type;
+    typedef
+        bobura::model::timetable_info::station_location<
+            station_type, std::size_t
+        >
+        station_location_type;
+    typedef
+        bobura::model::train_info::time<
+            std::size_t, bobura::model::train_info::time_span<std::ptrdiff_t>
+        >
+        time_type;
+    typedef bobura::model::train_info::stop<time_type, std::string> stop_type;
+    typedef
+        bobura::model::train<std::string, std::string, stop_type> train_type;
+    typedef
+        bobura::model::timetable<
+            std::string, station_location_type, train_type
+        >
+        timetable_type;
+    typedef
+        bobura::model::serializer::json_reader<timetable_type> reader_type;
+
+}
+
+BOOST_AUTO_TEST_SUITE(test_bobura)
+BOOST_AUTO_TEST_SUITE(model)
+BOOST_AUTO_TEST_SUITE(serializer)
+BOOST_AUTO_TEST_SUITE(json_reader)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const concrete_reader json_reader;
+    }
+
+    BOOST_AUTO_TEST_CASE(read)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        concrete_reader json_reader;
+        const std::unique_ptr<timetable_type> p_timetable = json_reader.read();
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
