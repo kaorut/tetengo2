19ba4957b5f6d3d18b07a0adb1b2068e04e5f6e0
kaoru <kaorut@mc.pikara.ne.jp>
2012-01-31 23:24:07 +0900

    Created tetengo2::gui::drawing::system_color_set.

---
+++ tetengo2_gui/include/tetengo2.gui.drawing.system_color_set.h
@@ -0,0 +1,105 @@
+/*! \file
+    \brief The definition of tetengo2::gui::drawing::background.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_GUI_DRAWING_BACKGROUND_H)
+#define TETENGO2_GUI_DRAWING_BACKGROUND_H
+
+#include <boost/noncopyable.hpp>
+#include <boost/optional.hpp>
+
+#include "tetengo2.cpp11.h"
+
+
+namespace tetengo2 { namespace gui { namespace drawing
+{
+    /*!
+        \brief The class template for a background.
+
+        \tparam DrawingDetails A detail implementation type of a drawing.
+    */
+    template <typename DrawingDetails>
+    class background : private boost::noncopyable
+    {
+    public:
+        // types
+
+        //! The detail implementation type of a drawing.
+        typedef DrawingDetails drawing_details_type;
+
+        //! The detail implementation type.
+        typedef
+            typename drawing_details_type::background_details_type
+            details_type;
+
+        //! The detail implementation pointer type.
+        typedef
+            typename drawing_details_type::background_details_ptr_type
+            details_ptr_type;
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Destroys the background.
+        */
+        virtual ~background()
+        TETENGO2_CPP11_NOEXCEPT
+        {}
+
+
+        // functions
+
+        /*!
+            \brief Returns the detail implementation;
+
+            \return The detail implementation.
+        */
+        boost::optional<const details_type&> details()
+        const
+        {
+            return details_impl();
+        }
+
+        /*!
+            \brief Returns the detail implementation;
+
+            \return The detail implementation.
+        */
+        boost::optional<details_type&> details()
+        {
+            return details_impl();
+        }
+
+
+    protected:
+        // constructors and destructor
+
+        /*!
+            \brief Creates a background.
+        */
+        background()
+        {}
+
+
+    private:
+        // virtual functions
+
+        virtual boost::optional<details_type&> details_impl()
+        = 0;
+
+        virtual boost::optional<const details_type&> details_impl()
+        const = 0;
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ tetengo2_gui/test/test_tetengo2.gui.drawing.system_color_set.cpp
@@ -0,0 +1,87 @@
+/*! \file
+    \brief Test of class tetengo2::gui::drawing::background.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+//#include <boost/optional.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "tetengo2.detail.stub.drawing.h"
+
+#include "tetengo2.gui.drawing.background.h"
+
+
+namespace
+{
+    // types
+
+    typedef
+        tetengo2::gui::drawing::background<tetengo2::detail::stub::drawing>
+        background_type;
+
+    struct concrete_background : public background_type
+    {
+        concrete_background()
+        :
+        background_type()
+        {}
+
+
+    private:
+        virtual boost::optional<const background_type::details_type&>
+        details_impl()
+        const
+        {
+            return boost::none;
+        }
+
+        virtual boost::optional<background_type::details_type&>
+        details_impl()
+        {
+            return boost::none;
+        }
+
+
+    };
+
+
+}
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo2)
+BOOST_AUTO_TEST_SUITE(gui)
+BOOST_AUTO_TEST_SUITE(drawing)
+BOOST_AUTO_TEST_SUITE(background)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const concrete_background background;
+    }
+
+    BOOST_AUTO_TEST_CASE(details)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const concrete_background background;
+
+            background.details();
+        }
+        {
+            concrete_background background;
+
+            background.details();
+        }
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
