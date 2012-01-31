3a3378611146c92ceef057612f054d1756b104cf
kaoru <kaorut@mc.pikara.ne.jp>
2012-01-31 23:30:20 +0900

    Created tetengo2::gui::message::keyboard_observer_set.

---
+++ tetengo2_gui/include/tetengo2.gui.message.keyboard_observer_set.h
@@ -0,0 +1,71 @@
+/*! \file
+    \brief The definition of tetengo2::gui::message::mouse_observer_set.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_GUI_MESSAGE_MOUSEOBSERVERSET_H)
+#define TETENGO2_GUI_MESSAGE_MOUSEOBSERVERSET_H
+
+#include <boost/noncopyable.hpp>
+#include <boost/signals2.hpp>
+
+
+namespace tetengo2 { namespace gui { namespace message
+{
+    /*!
+        \brief The class for a mouse observer set.
+    */
+    class mouse_observer_set : private boost::noncopyable
+    {
+    public:
+        // types
+
+        /*!
+            \brief The observer type of clicked.
+        */
+        typedef void clicked_type();
+
+        //! The signal type of clicked.
+        typedef boost::signals2::signal<clicked_type> clicked_signal_type;
+
+
+        // functions
+
+        /*!
+            \brief Returns the observer called when a mouse button is clicked.
+
+            \return The observer called when a mouse button is clicked.
+        */
+        const clicked_signal_type& clicked()
+        const
+        {
+            return m_clicked;
+        }
+
+        /*!
+            \brief Returns the observer called when a mouse button is clicked.
+
+            \return The observer called when a mouse button is clicked.
+        */
+        clicked_signal_type& clicked()
+        {
+            return m_clicked;
+        }
+
+
+    private:
+        // variables
+
+        clicked_signal_type m_clicked;
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ tetengo2_gui/test/test_tetengo2.gui.message.keyboard_observer_set.cpp
@@ -0,0 +1,40 @@
+/*! \file
+    \brief Test of class tetengo2::gui::message::mouse_observer_set.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include "tetengo2.gui.message.mouse_observer_set.h"
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo2)
+BOOST_AUTO_TEST_SUITE(gui)
+BOOST_AUTO_TEST_SUITE(message)
+BOOST_AUTO_TEST_SUITE(mouse_observer_set)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(clicked)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const tetengo2::gui::message::mouse_observer_set observer_set;
+
+            observer_set.clicked();
+        }
+        {
+            tetengo2::gui::message::mouse_observer_set observer_set;
+
+            observer_set.clicked();
+        }
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
