eb7ed80fd8c424f15827366d0824c14901b4a763
kaoru <kaorut@mc.pikara.ne.jp>
2012-02-09 06:01:57 +0900

    Created bobura::model::message::timetable_observer_set.

---
+++ bobura_model/include/bobura.model.message.timetable_observer_set.h
@@ -0,0 +1,90 @@
+/*! \file
+    \brief The definition of bobura::message::timetable_model_observer_set.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_MESSAGE_TIMETABLEMODELOBSERVERSET_H)
+#define TETENGO2_MESSAGE_TIMETABLEMODELOBSERVERSET_H
+
+#include <boost/noncopyable.hpp>
+#include <boost/signals2.hpp>
+
+
+namespace bobura { namespace message
+{
+    /*!
+        \brief The class template for a timetable model observer set.
+
+        \tparam Timetable A timetable type.
+        \tparam Path      A path type.
+    */
+    template <typename Timetable, typename Path>
+    class timetable_model_observer_set : private boost::noncopyable
+    {
+    public:
+        // types
+
+        //! The timetable type.
+        typedef Timetable timetable_type;
+
+        //! The path type.
+        typedef Path path_type;
+
+        /*!
+            \brief The observer type of reset.
+
+            \param timetable A timetable.
+            \param path      A path.
+        */
+        typedef
+            void reset_type(
+                const timetable_type& timetable,
+                const path_type&      path
+            );
+
+        //! The signal type of reset.
+        typedef boost::signals2::signal<reset_type> reset_signal_type;
+
+
+        // functions
+
+        /*!
+            \brief Returns the observer called when a timetable model is
+                   reset.
+
+            \return The observer called when a a timetable model is reset.
+        */
+        const reset_signal_type& reset()
+        const
+        {
+            return m_reset;
+        }
+
+        /*!
+            \brief Returns the observer called when a timetable model is
+                   reset.
+
+            \return The observer called when a a timetable model is reset.
+        */
+        reset_signal_type& reset()
+        {
+            return m_reset;
+        }
+
+
+    private:
+        // variables
+
+        reset_signal_type m_reset;
+
+
+    };
+
+
+}}
+
+
+#endif
