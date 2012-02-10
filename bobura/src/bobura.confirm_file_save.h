c84529559c5cd1a849ffbdc5ce062790364becbb
kaoru <kaorut@mc.pikara.ne.jp>
2012-02-11 07:50:10 +0900

    Created bobura::confirm_file_save.

---
+++ bobura/src/bobura.confirm_file_save.h
@@ -0,0 +1,154 @@
+/*! \file
+    \brief The definition of bobura::main_window.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_MAINWINDOW_H)
+#define BOBURA_MAINWINDOW_H
+
+#include <boost/mpl/at.hpp>
+#include <boost/optional.hpp>
+
+#include <tetengo2.text.h>
+#include <tetengo2.unique.h>
+
+#include "bobura.message.type_list.h"
+
+
+namespace bobura
+{
+    /*!
+        \brief The class template for the main window.
+
+        \tparam Window                    A window type.
+        \tparam MessageCatalog            A message catalog type.
+        \tparam Settings                  A settings type.
+        \tparam MessageLoopBreak          A message loop break type.
+        \tparam MainWindowMessageTypeList A message type.
+    */
+    template <
+        typename Window,
+        typename MessageCatalog,
+        typename Settings,
+        typename MessageLoopBreak,
+        typename MainWindowMessageTypeList
+    >
+    class main_window : public Window
+    {
+    public:
+        // types
+
+        //! The base type.
+        typedef Window base_type;
+
+        //! The string type.
+        typedef typename base_type::string_type string_type;
+
+        //! The message catalog type.
+        typedef MessageCatalog message_catalog_type;
+
+        //! The settings type.
+        typedef Settings settings_type;
+
+        //! The message loop break type.
+        typedef MessageLoopBreak message_loop_break_type;
+
+        //! The message type list type.
+        typedef MainWindowMessageTypeList main_window_message_type_list_type;
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a main window.
+
+            \param message_catalog A message catalog.
+            \param settings        Settings.
+        */
+        main_window(
+            const message_catalog_type& message_catalog,
+            const settings_type&        settings
+        )
+        :
+        base_type(),
+        m_message_catalog(message_catalog),
+        m_settings(settings)
+        {
+            initialize_window();
+        }
+
+        /*!
+            \brief Destroys the main window.
+        */
+        virtual ~main_window()
+        TETENGO2_CPP11_NOEXCEPT
+        {}
+
+
+        // functions
+
+        /*!
+            \brief Sets a title.
+
+            \param document_name A document name.
+            \param changed       A changed status.
+        */
+        void set_title(
+            const boost::optional<string_type>& document_name,
+            const bool                          changed
+        )
+        {
+            string_type title =
+                document_name ?
+                *document_name :
+                m_message_catalog.get(TETENGO2_TEXT("Common:Untitled"));
+            if (changed)
+                title += string_type(TETENGO2_TEXT(" *"));
+            title += string_type(TETENGO2_TEXT(" - "));
+            title += m_message_catalog.get(TETENGO2_TEXT("App:Bobura"));
+
+            set_text(title);
+        }
+
+
+    private:
+        // variables
+
+        const message_catalog_type& m_message_catalog;
+
+        const settings_type& m_settings;
+
+
+        // functions
+
+        void initialize_window()
+        {
+            set_message_observers();
+
+            set_title(boost::none, false);
+        }
+
+        void set_message_observers()
+        {
+            this->window_observer_set().destroyed().connect(
+                TETENGO2_CPP11_BIND(message_loop_break_type(), 0)
+            );
+
+            this->paint_observer_set().paint().connect(
+                typename boost::mpl::at<
+                    main_window_message_type_list_type,
+                    message::main_window::type::paint
+                >::type(m_settings.image_directory_path())
+            );
+        }
+
+
+    };
+
+
+}
+
+#endif
---
+++ bobura/test/test_bobura.confirm_file_save.cpp
@@ -0,0 +1,83 @@
+/*! \file
+    \brief Test of class bobura::main_window.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+//#include <string>
+//#include <utility>
+//#include <vector>
+
+//#include <boost/filesystem.hpp>
+//#include <boost/optional.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "test_bobura.types.h"
+
+
+BOOST_AUTO_TEST_SUITE(test_bobura)
+BOOST_AUTO_TEST_SUITE(main_window)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const message_catalog_type message_catalog;
+        std::vector<std::wstring> arguments;
+        boost::filesystem::path path;
+        const settings_type settings(std::move(arguments), std::move(path));
+        const main_window_type main_window(message_catalog, settings);
+    }
+
+    BOOST_AUTO_TEST_CASE(set_title)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const message_catalog_type message_catalog;
+            std::vector<std::wstring> arguments;
+            boost::filesystem::path path;
+            const settings_type settings(std::move(arguments), std::move(path));
+            main_window_type main_window(message_catalog, settings);
+
+            main_window.set_title(boost::none, false);
+        }
+        {
+            const message_catalog_type message_catalog;
+            std::vector<std::wstring> arguments;
+            boost::filesystem::path path;
+            const settings_type settings(std::move(arguments), std::move(path));
+            main_window_type main_window(message_catalog, settings);
+
+            main_window.set_title(boost::none, true);
+        }
+        {
+            const message_catalog_type message_catalog;
+            std::vector<std::wstring> arguments;
+            boost::filesystem::path path;
+            const settings_type settings(std::move(arguments), std::move(path));
+            main_window_type main_window(message_catalog, settings);
+
+            main_window.set_title(
+                boost::make_optional<std::wstring>(L"hoge"), false
+            );
+        }
+        {
+            const message_catalog_type message_catalog;
+            std::vector<std::wstring> arguments;
+            boost::filesystem::path path;
+            const settings_type settings(std::move(arguments), std::move(path));
+            main_window_type main_window(message_catalog, settings);
+
+            main_window.set_title(
+                boost::make_optional<std::wstring>(L"hoge"), true
+            );
+        }
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
