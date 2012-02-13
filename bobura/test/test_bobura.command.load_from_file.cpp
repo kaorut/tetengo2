1d350709cb2a523c84ad56e324bce04cad746d5d
kaoru <kaorut@mc.pikara.ne.jp>
2012-02-14 00:31:36 +0900

    Modified test_bobura::command::load_from_file.

--- bobura/test/test_bobura.command.load_from_file.cpp
+++ bobura/test/test_bobura.command.load_from_file.cpp
@@ -21,6 +21,30 @@ BOOST_AUTO_TEST_SUITE(command)
 BOOST_AUTO_TEST_SUITE(load_from_file)
     // test cases
 
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        window_type parent;
+        model_type model;
+        const message_catalog_type message_catalog;
+        const confirm_file_save_type confirm_file_save(
+            model, message_catalog
+        );
+        reader_type reader;
+        const bobura::command::load_from_file<
+            message_box_type,
+            file_open_type,
+            model_type,
+            confirm_file_save_type,
+            reader_type,
+            message_catalog_type
+        >
+        load_from_file(
+            parent, confirm_file_save, model, reader, message_catalog
+        );
+    }
+
     BOOST_AUTO_TEST_CASE(operator_paren)
     {
         BOOST_TEST_PASSPOINT();
