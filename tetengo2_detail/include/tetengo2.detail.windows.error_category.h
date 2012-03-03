334a16ec690982d812d327839f86dd3d9fae3e61
kaoru <kaorut@mc.pikara.ne.jp>
2012-03-04 01:57:36 +0900

    Implemented tetengo2::detail::windows::error_category.

--- tetengo2_detail/include/tetengo2.detail.windows.error_category.h
+++ tetengo2_detail/include/tetengo2.detail.windows.error_category.h
@@ -10,16 +10,40 @@
 #define TETENGO2_DETAIL_WINDOWS_ERRORCATEGORY_H
 
 #include <system_error>
+#include <vector>
 
 #define NOMINMAX
 #define OEMRESOURCE
 #include <Windows.h>
 
 #include "tetengo2.cpp11.h"
+#include "tetengo2.detail.windows.encoding.h"
+#include "tetengo2.text.encoder.h"
+#include "tetengo2.text.encoding.locale.h"
+#include "tetengo2.text.encoding.utf8.h"
 
 
 namespace tetengo2 { namespace detail { namespace windows
 {
+    namespace detail
+    {
+        typedef
+            text::encoder<
+                text::encoding::utf8<encoding>,
+                text::encoding::locale<std::wstring, encoding>
+            >
+            encoder_type;
+
+        inline const encoder_type& encoder()
+        {
+            static const encoder_type singleton;
+            return singleton;
+        }
+
+
+    }
+
+
     /*!
         \brief The class for a detail implementation of an error category.
     */
@@ -67,7 +91,36 @@ namespace tetengo2 { namespace detail { namespace windows
         virtual std::string message(const value_type error_value)
         const
         {
-            return "test";
+            const std::size_t message_capacity = 64 * 1024;
+            std::vector<wchar_t> message(message_capacity, 0);
+            ::DWORD message_length =
+                ::FormatMessageW(
+                    FORMAT_MESSAGE_FROM_SYSTEM,
+                    NULL,
+                    error_value,
+                    MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                    &message[0],
+                    message_capacity,
+                    NULL
+                );
+            if (message_length == 0)
+                return "Unknown Error";
+
+            while (
+                message_length > 0 && (
+                    message[message_length - 1] == L'\r' ||
+                    message[message_length - 1] == L'\n'
+                )
+            )
+            {
+                --message_length;
+            }
+
+            return detail::encoder().decode(
+                std::wstring(
+                    message.begin(), message.begin() + message_length
+                )
+            );
         }
 
 
