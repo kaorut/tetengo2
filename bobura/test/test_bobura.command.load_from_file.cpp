ed9b32d155cad43e8f1f829060b175a3b6b81dfe
kaoru <kaorut@mc.pikara.ne.jp>
2012-03-16 05:39:53 +0900

    Modified test_bobura::command::load_from_file.

--- bobura/test/test_bobura.command.load_from_file.cpp
+++ bobura/test/test_bobura.command.load_from_file.cpp
@@ -15,7 +15,7 @@
 #include <tetengo2.detail.stub.common_dialog.h>
 #include <tetengo2.gui.common_dialog.file_open.h>
 
-#include "test_bobura.types.h"
+#include "bobura.type_list.h"
 
 #include "bobura.command.load_from_file.h"
 
@@ -23,6 +23,60 @@
 namespace
 {
     // types
+
+    typedef
+        boost::mpl::at<
+            bobura::model_type_list, bobura::type::model::model
+        >::type
+        model_type;
+
+    typedef
+        boost::mpl::at<
+            bobura::ui_type_list, bobura::type::ui::abstract_window
+        >::type
+        abstract_window_type;
+
+    typedef
+        boost::mpl::at<
+            bobura::load_save_type_list,
+            bobura::type::load_save::load_from_file
+        >::type
+        load_from_file_type;
+
+    typedef
+        boost::mpl::at<
+            bobura::model_type_list, bobura::type::model::writer
+        >::type
+        writer_type;
+
+    typedef
+        boost::mpl::at<
+            bobura::locale_type_list, bobura::type::locale::message_catalog
+        >::type
+        message_catalog_type;
+
+    typedef
+        boost::mpl::at<
+            bobura::load_save_type_list, bobura::type::load_save::save_to_file
+        >::type
+        save_to_file_type;
+
+    typedef
+        boost::mpl::at<
+            bobura::load_save_type_list,
+            bobura::type::load_save::confirm_file_save
+        >::type
+        confirm_file_save_type;
+
+    typedef
+        boost::mpl::at<
+            bobura::model_type_list, bobura::type::model::reader
+        >::type
+        reader_type;
+
+    typedef
+        boost::mpl::at<bobura::ui_type_list, bobura::type::ui::window>::type
+        window_type;
 
     typedef
         bobura::command::load_from_file<
