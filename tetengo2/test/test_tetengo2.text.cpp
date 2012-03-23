21749c3cf8984435ab36b1bc02134698dfaa256e
kaoru <kaorut@mc.pikara.ne.jp>
2012-03-24 07:08:55 +0900

    Implemented test_tetengo2::text.

--- tetengo2/test/test_tetengo2.text.cpp
+++ tetengo2/test/test_tetengo2.text.cpp
@@ -6,15 +6,27 @@
     $Id$
 */
 
+#include <string>
+
 #include <boost/test/unit_test.hpp>
 
 #include "tetengo2.text.h"
 
 
 BOOST_AUTO_TEST_SUITE(test_tetengo2)
-BOOST_AUTO_TEST_SUITE(text)
     // test cases
 
+    BOOST_AUTO_TEST_CASE(text)
+    {
+        {
+            const std::string string(TETENGO2_TEXT("hoge"));
+            BOOST_CHECK(string == "hoge");
+        }
+        {
+            const std::wstring string(TETENGO2_TEXT("hoge"));
+            BOOST_CHECK(string == L"hoge");
+        }
+    }
 
-BOOST_AUTO_TEST_SUITE_END()
+
 BOOST_AUTO_TEST_SUITE_END()
