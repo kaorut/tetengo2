c43b44cf406b331986ebf4d2865564b3cde50caf
kaoru <kaorut@mc.pikara.ne.jp>
2012-04-03 02:01:14 +0900

    Implemented test_bobura::model::serializer::writer_set.

--- bobura_model/test/test_bobura.model.serializer.writer_set.cpp
+++ bobura_model/test/test_bobura.model.serializer.writer_set.cpp
@@ -7,15 +7,9 @@
 */
 
 //#include <memory>
-//#include <sstream>
-//#include <string>
-//#include <utility>
 
 //#include <boost/mpl/at.hpp>
 #include <boost/test/unit_test.hpp>
-
-#include <tetengo2.text.h>
-#include <tetengo2.unique.h>
 
 #include "test_bobura.model.type_list.h"
 
@@ -23,6 +17,13 @@
 namespace
 {
     // types
+
+    typedef
+        boost::mpl::at<
+            test_bobura::model::serialization_type_list,
+            test_bobura::model::type::serialization::writer
+        >::type
+        writer_type;
 
     typedef
         boost::mpl::at<
@@ -41,10 +42,15 @@ BOOST_AUTO_TEST_SUITE(serializer)
 BOOST_AUTO_TEST_SUITE(writer_set)
     // test cases
 
-    BOOST_AUTO_TEST_CASE(make_writers)
+    BOOST_AUTO_TEST_CASE(create_writers)
     {
         BOOST_TEST_PASSPOINT();
 
+        const std::vector<std::unique_ptr<writer_type>> p_writers =
+            writer_set_type::create_writers();
+
+        BOOST_CHECK(!p_writers.empty());
+        BOOST_CHECK(p_writers[0]);
     }
 
 
