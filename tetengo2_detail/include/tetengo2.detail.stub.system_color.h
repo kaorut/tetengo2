ef8d311efc986e1a54d16a71bff240770c7d0715
kaoru <kaorut@mc.pikara.ne.jp>
2012-04-27 01:08:49 +0900

    Created tetengo2.detail.stub.system_color.h.

---
+++ tetengo2_detail/include/tetengo2.detail.stub.system_color.h
@@ -0,0 +1,27 @@
+/*! \file
+    \brief The definition of tetengo2::detail::stub::gui_fixture.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_STUB_GUIFIXTURE_H)
+#define TETENGO2_DETAIL_STUB_GUIFIXTURE_H
+
+#include <boost/noncopyable.hpp>
+
+
+namespace tetengo2 { namespace detail { namespace stub
+{
+    /*!
+        \brief The class for a detail implementation of a GUI fixture.
+    */
+    class gui_fixture : private boost::noncopyable
+    {};
+
+
+}}}
+
+
+#endif
