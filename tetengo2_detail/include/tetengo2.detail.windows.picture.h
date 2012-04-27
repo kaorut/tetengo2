47ef6fb8d7e5613efed1104dafd043e06f627984
kaoru <kaorut@mc.pikara.ne.jp>
2012-04-27 23:35:15 +0900

    Created tetengo2.detail.windows.picture.h.

---
+++ tetengo2_detail/include/tetengo2.detail.windows.picture.h
@@ -0,0 +1,65 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::font.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_WINDOWS_FONT_H)
+#define TETENGO2_DETAIL_WINDOWS_FONT_H
+
+//#include <system_error>
+
+//#include <boost/throw_exception.hpp>
+
+//#define NOMINMAX
+//#define OEMRESOURCE
+//#include <Windows.h>
+
+#include "tetengo2.detail.windows.error_category.h"
+#include "tetengo2.detail.windows.windows_version.h"
+
+
+namespace tetengo2 { namespace detail { namespace windows
+{
+    /*!
+        \brief Returns a nonclient metrics.
+
+        \param metrics A metrics where the result is stored.
+
+        \throw std::system_error When a nonclient metrics cannot be obtained.
+    */
+    inline void get_nonclient_metrics(::NONCLIENTMETRICSW& metrics)
+    {
+        const ::UINT metrics_size =
+            on_windows_vista_or_later() ? sizeof(::NONCLIENTMETRICSW) : sizeof(::NONCLIENTMETRICSW) - sizeof(int);
+        metrics.cbSize = metrics_size;
+        if (::SystemParametersInfoW(SPI_GETNONCLIENTMETRICS, metrics_size, &metrics, 0) == 0)
+        {
+            BOOST_THROW_EXCEPTION(
+                std::system_error(
+                    std::error_code(ERROR_FUNCTION_FAILED, win32_category()), "Can't get non-client metrics."
+                )
+            );
+        }
+    }
+
+    /*!
+        \brief Returns the message font.
+
+        \return The message font.
+    */
+    inline ::LOGFONTW get_message_font()
+    {
+        ::NONCLIENTMETRICSW metrics;
+        get_nonclient_metrics(metrics);
+
+        return metrics.lfMessageFont;
+    }
+
+
+}}}
+
+
+#endif
