40194f2e7dac51729d070f546b787b55e1359fea
kaoru <kaorut@mc.pikara.ne.jp>
2012-05-15 01:07:02 +0900

    Created tetengo2::detail::stub/windows::scroll.

---
+++ tetengo2_detail/include/tetengo2.detail.stub.scroll.h
@@ -0,0 +1,44 @@
+/*! \file
+    \brief The definition of tetengo2::detail::stub::scroll.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_STUB_SCROLL_H)
+#define TETENGO2_DETAIL_STUB_SCROLL_H
+
+#include <boost/noncopyable.hpp>
+
+
+namespace tetengo2 { namespace detail { namespace stub
+{
+    /*!
+        \brief The class for a detail implementation of a scroll.
+    */
+    class scroll : private boost::noncopyable
+    {
+    public:
+        // types
+
+
+        // static functions
+
+
+    private:
+        // static functions
+
+
+        // forbidden operations
+
+        scroll();
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ tetengo2_detail/include/tetengo2.detail.windows.scroll.h
@@ -0,0 +1,48 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::scroll.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_WINDOWS_SCROLL_H)
+#define TETENGO2_DETAIL_WINDOWS_SCROLL_H
+
+//#include <boost/noncopyable.hpp>
+
+//#define NOMINMAX
+//#define OEMRESOURCE
+//#include <Windows.h>
+
+
+namespace tetengo2 { namespace detail { namespace windows
+{
+    /*!
+        \brief The class for a detail implementation of a scroll.
+    */
+    class scroll : private boost::noncopyable
+    {
+    public:
+        // types
+
+
+        // static functions
+
+
+    private:
+        // static functions
+
+
+        // forbidden operations
+
+        scroll();
+
+
+    };
+
+
+}}}
+
+
+#endif
