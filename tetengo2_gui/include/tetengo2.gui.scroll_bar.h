8438ad58d2b96323a52a6488c68fec5c0630af1a
kaoru <kaorut@mc.pikara.ne.jp>
2012-05-14 23:40:56 +0900

    Implemented tetengo2::gui::scroll_bar.

--- tetengo2_gui/include/tetengo2.gui.scroll_bar.h
+++ tetengo2_gui/include/tetengo2.gui.scroll_bar.h
@@ -9,7 +9,10 @@
 #if !defined(TETENGO2_GUI_SCROLLBAR_H)
 #define TETENGO2_GUI_SCROLLBAR_H
 
-#include <boost/noncopyable.hpp>
+#include <stdexcept>
+#include <utility>
+
+#include <boost/throw_exception.hpp>
 
 
 namespace tetengo2 { namespace gui
@@ -20,7 +23,7 @@ namespace tetengo2 { namespace gui
         \tparam Size A size type.
     */
     template <typename Size>
-    class scroll_bar : private boost::noncopyable
+    class scroll_bar
     {
     public:
         // types
@@ -28,14 +31,121 @@ namespace tetengo2 { namespace gui
         //! The size type.
         typedef Size size_type;
 
+        //! The range type.
+        typedef std::pair<size_type, size_type> range_type;
+
 
         // constructors and destructor
 
         /*!
             \brief Creates a scroll bar.
+
+            The position, the range and the page size are initialized to 0.
         */
         scroll_bar()
+        :
+        m_position(0),
+        m_range(0, 0),
+        m_page_size(0)
         {}
+
+
+        // functions
+
+        /*!
+            \brief Returns the position.
+
+            \return The position.
+        */
+        size_type position()
+        const
+        {
+            return m_position;
+        }
+
+        /*!
+            \brief Sets a position
+
+            The position must be inside the range; i.e., range().first <= position < range().second.
+
+            \param position A position.
+
+            \throw std::out_of_range When the position is outside the range.
+        */
+        void set_position(const size_type position)
+        {
+            if (position < m_range.first || m_range.second < position)
+                BOOST_THROW_EXCEPTION(std::out_of_range("The position is outside the range."));
+
+            m_position = position;
+        }
+
+        /*!
+            \brief Returns the range.
+
+            \return The range.
+        */
+        const range_type& range()
+        const
+        {
+            return m_range;
+        }
+
+        /*!
+            \brief Sets a range
+
+            The range.first must be smaller than the range.second, and the position must be inside the range;
+            i.e., range.first <= position() <= range.second.
+
+            \tparam R A range type.
+
+            \param range A range.
+
+            \throw std::out_of_range When the position is outside the range.
+        */
+        template <typename R>
+        void set_range(R&& range)
+        {
+            if (range.first > range.second)
+                BOOST_THROW_EXCEPTION(std::out_of_range("Reversed range is not allowed."));
+            if (m_position < range.first || range.second <= m_position)
+                BOOST_THROW_EXCEPTION(std::out_of_range("The position is outside the range."));
+
+            m_range = std::forward<R>(range);
+        }
+
+        /*!
+            \brief Returns the page size.
+
+            \return The page size.
+        */
+        size_type page_size()
+        const
+        {
+            return m_page_size;
+        }
+
+        /*!
+            \brief Sets a page size
+
+            The page size may be larger than the size of the range.
+
+            \param page_size A page size.
+        */
+        void set_page_size(const size_type page_size)
+        {
+            m_page_size = page_size;
+        }
+
+
+    private:
+        // variables
+
+        size_type m_position;
+
+        range_type m_range;
+
+        size_type m_page_size;
 
 
     };
--- tetengo2_gui/test/test_tetengo2.gui.scroll_bar.cpp
+++ tetengo2_gui/test/test_tetengo2.gui.scroll_bar.cpp
@@ -6,9 +6,23 @@
     $Id$
 */
 
+#include <cassert>
+#include <cstddef>
+#include <stdexcept>
+
 #include <boost/test/unit_test.hpp>
 
 #include "tetengo2.gui.scroll_bar.h"
+
+
+namespace
+{
+    // types
+
+    typedef tetengo2::gui::scroll_bar<std::size_t> scroll_bar_type;
+
+
+}
 
 
 BOOST_AUTO_TEST_SUITE(test_tetengo2)
@@ -20,7 +34,84 @@ BOOST_AUTO_TEST_SUITE(scroll_bar)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        const scroll_bar_type scroll_bar;
+    }
+
+    BOOST_AUTO_TEST_CASE(position)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const scroll_bar_type scroll_bar;
+
+        BOOST_CHECK_EQUAL(scroll_bar.position(), 0U);
+    }
+
+    BOOST_AUTO_TEST_CASE(set_position)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        {
+            scroll_bar_type scroll_bar;
+            scroll_bar.set_range(scroll_bar_type::range_type(0, 100));
+
+            scroll_bar.set_position(42);
+
+            BOOST_CHECK_EQUAL(scroll_bar.position(), 42U);
+        }
+        {
+            scroll_bar_type scroll_bar;
+            scroll_bar.set_range(scroll_bar_type::range_type(0, 100));
+
+            BOOST_CHECK_THROW(scroll_bar.set_position(128), std::out_of_range);
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(range)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const scroll_bar_type scroll_bar;
+
+        BOOST_CHECK(scroll_bar.range() == scroll_bar_type::range_type(0, 0));
+    }
+
+    BOOST_AUTO_TEST_CASE(set_range)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        {
+            scroll_bar_type scroll_bar;
+
+            scroll_bar.set_range(scroll_bar_type::range_type(0, 42));
+
+            BOOST_CHECK(scroll_bar.range() == scroll_bar_type::range_type(0, 42));
+        }
+        {
+            scroll_bar_type scroll_bar;
+            assert(scroll_bar.position() == 0);
+
+            BOOST_CHECK_THROW(scroll_bar.set_range(scroll_bar_type::range_type(10, 42)), std::out_of_range);
+        }
+    }
+
+    BOOST_AUTO_TEST_CASE(page_size)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const scroll_bar_type scroll_bar;
+
+        BOOST_CHECK_EQUAL(scroll_bar.page_size(), 0U);
+    }
+
+    BOOST_AUTO_TEST_CASE(set_page_size)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        scroll_bar_type scroll_bar;
+
+        scroll_bar.set_page_size(42);
+
+        BOOST_CHECK_EQUAL(scroll_bar.page_size(), 42U);
     }
 
 
