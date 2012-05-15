74fe8a87a79b2181f6131a6182f1d2b977286e8c
kaoru <kaorut@mc.pikara.ne.jp>
2012-05-16 05:48:04 +0900

    Created tetengo2::gui::message::scroll_bar_observer_set.

---
+++ tetengo2_gui/include/tetengo2.gui.message.scroll_bar_observer_set.h
@@ -0,0 +1,71 @@
+/*! \file
+    \brief The definition of tetengo2::gui::message::menu_observer_set.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_GUI_MESSAGE_MENUOBSERVERSET_H)
+#define TETENGO2_GUI_MESSAGE_MENUOBSERVERSET_H
+
+#include <boost/noncopyable.hpp>
+#include <boost/signals2.hpp>
+
+
+namespace tetengo2 { namespace gui { namespace message
+{
+    /*!
+        \brief The class for a paint observer set.
+    */
+    class menu_observer_set : private boost::noncopyable
+    {
+    public:
+        // types
+
+        /*!
+            \brief The observer type of selected.
+        */
+        typedef void selected_type();
+
+        //! The signal type of selected.
+        typedef boost::signals2::signal<selected_type> selected_signal_type;
+
+
+        // functions
+
+        /*!
+            \brief Returns the observer called when a menu item is selected.
+
+            \return The observer called when a menu item is selected.
+        */
+        const selected_signal_type& selected()
+        const
+        {
+            return m_selected;
+        }
+
+        /*!
+            \brief Returns the observer called when a menu item is selected.
+
+            \return The observer called when a menu item is selected.
+        */
+        selected_signal_type& selected()
+        {
+            return m_selected;
+        }
+
+
+    private:
+        // variables
+
+        selected_signal_type m_selected;
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ tetengo2_gui/test/test_tetengo2.gui.message.scroll_bar_observer_set.cpp
@@ -0,0 +1,55 @@
+/*! \file
+    \brief Test of class tetengo2::gui::message::menu_observer_set.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+//#include <boost/mpl/at.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "test_tetengo2.gui.type_list.h"
+
+
+namespace
+{
+    // types
+
+    typedef
+        boost::mpl::at<
+            test_tetengo2::gui::observer_set_type_list, test_tetengo2::gui::type::observer_set::menu_observer_set
+        >::type
+        menu_observer_set_type;
+
+
+}
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo2)
+BOOST_AUTO_TEST_SUITE(gui)
+BOOST_AUTO_TEST_SUITE(message)
+BOOST_AUTO_TEST_SUITE(menu_observer_set)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(selected)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        {
+            const menu_observer_set_type observer_set;
+
+            observer_set.selected();
+        }
+        {
+            menu_observer_set_type observer_set;
+
+            observer_set.selected();
+        }
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
