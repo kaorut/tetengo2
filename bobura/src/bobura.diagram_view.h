63c4db6f9e66f66636b06cff7b92eb2d1c66593d
kaoru <kaorut@mc.pikara.ne.jp>
2012-05-19 07:15:47 +0900

    Created bobura::diagram_view.

---
+++ bobura/src/bobura.diagram_view.h
@@ -0,0 +1,45 @@
+/*! \file
+    \brief The definition of bobura::diagram_view.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_DIAGRAMVIEW_H)
+#define BOBURA_DIAGRAMVIEW_H
+
+#include <boost/noncopyable.hpp>
+
+namespace bobura
+{
+   /*!
+        \brief The class for a diagram view.
+    */
+    class diagram_view : private boost::noncopyable
+    {
+    public:
+        // types
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a diagram view.
+        */
+        diagram_view()
+        {}
+
+
+        // functions
+
+
+    private:
+
+
+    };
+
+
+}
+
+#endif
---
+++ bobura/test/test_bobura.diagram_view.cpp
@@ -0,0 +1,25 @@
+/*! \file
+    \brief Test of class bobura::diagram_view.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_SUITE(test_bobura)
+BOOST_AUTO_TEST_SUITE(diagram_view)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
