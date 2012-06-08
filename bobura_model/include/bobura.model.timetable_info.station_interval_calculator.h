9140f963cc1834bc9f6df2a7dfa7b1fdc42703fc
kaoru <kaorut@mc.pikara.ne.jp>
2012-06-09 07:14:13 +0900

    Created bobura::model::timetable_info::station_interval_calculator.

---
+++ bobura_model/include/bobura.model.timetable_info.station_interval_calculator.h
@@ -0,0 +1,40 @@
+/*! \file
+    \brief The definition of bobura::model::timetable_info::station_interval_calculator.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_MODEL_TIMETABLEINFO_STATIONINTERVALCALCULATOR_H)
+#define BOBURA_MODEL_TIMETABLEINFO_STATIONINTERVALCALCULATOR_H
+
+
+namespace bobura { namespace model { namespace timetable_info
+{
+    /*!
+        \brief The class template for a station interval calculator.
+
+        \tparam Station  A station type.
+        \tparam Meterage A meterage type.
+    */
+    template <typename Station>
+    class station_interval_calculator
+    {
+    public:
+        // types
+
+
+        // constructors and destructor
+
+
+        // functions
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ bobura_model/test/test_bobura.model.timetable_info.station_interval_calculator.cpp
@@ -0,0 +1,40 @@
+/*! \file
+    \brief Test of class bobura::model::timetable_info::station_interval_calculator.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+//#include <boost/mpl/at.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "test_bobura.model.type_list.h"
+
+
+namespace
+{
+    // types
+
+
+}
+
+
+BOOST_AUTO_TEST_SUITE(test_bobura)
+BOOST_AUTO_TEST_SUITE(model)
+BOOST_AUTO_TEST_SUITE(timetable_info)
+BOOST_AUTO_TEST_SUITE(station_interval_calculator)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
