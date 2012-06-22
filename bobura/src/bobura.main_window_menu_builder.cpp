bc596779df4ada73caa90895a05f040c3f9599c6
kaoru <kaorut@mc.pikara.ne.jp>
2012-06-22 21:02:34 +0900

    Created bobura.main_window_menu_builder.cpp. Removed test_bobura::main_window_menu_builder.

---
+++ bobura/src/bobura.main_window_menu_builder.cpp
@@ -0,0 +1,16 @@
+/*! \file
+    \brief The definition of bobura::main_window_menu_builder.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#include "bobura.main_window_menu_builder.h"
+
+
+namespace bobura
+{
+
+
+}
