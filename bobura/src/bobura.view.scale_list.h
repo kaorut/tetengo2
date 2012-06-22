7626571892f23d4aba1259235ec361e6c0aba0dc
kaoru <kaorut@mc.pikara.ne.jp>
2012-06-22 20:52:27 +0900

    Implemented bobura::view::scale_list.

--- bobura/src/bobura.view.scale_list.h
+++ bobura/src/bobura.view.scale_list.h
@@ -9,9 +9,17 @@
 #if !defined(BOBURA_VIEW_SCALELIST_H)
 #define BOBURA_VIEW_SCALELIST_H
 
+#include <algorithm>
+#include <functional>
+#include <stdexcept>
 #include <vector>
 
 #include <boost/noncopyable.hpp>
+#include <boost/optional.hpp>
+#include <boost/throw_exception.hpp>
+
+#include <tetengo2.cpp11.h>
+#include <tetengo2.text.h>
 
 
 namespace bobura { namespace view
@@ -37,6 +45,163 @@ namespace bobura { namespace view
 
         //! The string type.
         typedef String string_type;
+
+
+        // functions
+
+        /*!
+            \brief Returns the scale count.
+
+            \return The scale count.
+        */
+        size_type size()
+        const
+        {
+            assert(scales().size() == labels().size());
+            return scales().size();
+        }
+
+        /*!
+            \brief Returns the scale at the specified index.
+
+            \param index.
+
+            \return The scale.
+
+            \throw std::out_of_range When index is out of range.
+        */
+        const scale_type& at(const size_type index)
+        const
+        {
+            if (index >= scales().size())
+                BOOST_THROW_EXCEPTION(std::out_of_range("index is out of range."));
+
+            return scales()[index];
+        }
+
+        /*!
+            \brief Returns the scale label at the specified index.
+
+            \param index.
+
+            \return The scale label.
+
+            \throw std::out_of_range When index is out of range.
+        */
+        const string_type& label_at(const size_type index)
+        const
+        {
+            if (index >= labels().size())
+                BOOST_THROW_EXCEPTION(std::out_of_range("index is out of range."));
+
+            return labels()[index];
+        }
+
+        /*!
+            \brief Returns the same scale index.
+
+            \param scale A scale.
+
+            \return The same scale index.
+        */
+        boost::optional<size_type> index_of(const scale_type& scale)
+        const
+        {
+            for (size_type i = 0; i < scales().size(); ++i)
+            {
+                if (scales()[i] == scale)
+                    return boost::make_optional(i);
+            }
+            return boost::none;
+        }
+
+        /*!
+            \brief Returns the nearest larger scale.
+        
+            \param scale A scale.
+
+            \return The nearest larger scale.
+        */
+        const scale_type& larger(const scale_type& scale)
+        const
+        {
+            const std::vector<scale_type>::const_iterator found =
+                std::find_if(
+                    scales().begin(),
+                    scales().end(),
+                    TETENGO2_CPP11_BIND(
+                        std::greater<scale_type>(), tetengo2::cpp11::placeholders_1(), tetengo2::cpp11::cref(scale)
+                    )
+                );
+            return found != scales().end() ? *found : scales().back();
+        }
+
+        /*!
+            \brief Returns the nearest smaller scale.
+        
+            \param scale A scale.
+
+            \return The nearest smaller scale.
+        */
+        const scale_type& smaller(const scale_type& scale)
+        const
+        {
+            const std::vector<scale_type>::const_reverse_iterator found =
+                std::find_if(
+                    scales().rbegin(),
+                    scales().rend(),
+                    TETENGO2_CPP11_BIND(
+                        std::less<scale_type>(), tetengo2::cpp11::placeholders_1(), tetengo2::cpp11::cref(scale)
+                    )
+                );
+            return found != scales().rend() ? *found : scales().front();
+        }
+
+
+    private:
+        // static functions
+
+        static const std::vector<scale_type>& scales()
+        {
+            static const std::vector<scale_type> singleton(make_scales());
+            return singleton;
+        }
+
+        static const std::vector<string_type>& labels()
+        {
+            static const std::vector<string_type> singleton(make_labels());
+            return singleton;
+        }
+
+        static std::vector<scale_type> make_scales()
+        {
+            std::vector<scale_type> scales;
+
+            scales.push_back(scale_type(1, 2));
+            scales.push_back(scale_type(3, 4));
+            scales.push_back(scale_type(1));
+            scales.push_back(scale_type(3, 2));
+            scales.push_back(scale_type(2));
+            scales.push_back(scale_type(4));
+
+            assert(std::is_sorted(scales.begin(), scales.end(), std::less<scale_type>()));
+            return scales;
+        }
+
+        static std::vector<string_type> make_labels()
+        {
+            std::vector<string_type> labels;
+
+            labels.push_back(string_type(TETENGO2_TEXT("50%")));
+            labels.push_back(string_type(TETENGO2_TEXT("75%")));
+            labels.push_back(string_type(TETENGO2_TEXT("100%")));
+            labels.push_back(string_type(TETENGO2_TEXT("150%")));
+            labels.push_back(string_type(TETENGO2_TEXT("200%")));
+            labels.push_back(string_type(TETENGO2_TEXT("400%")));
+
+            assert(labels.size() == scales().size());
+            return labels;
+        }
 
 
     };
