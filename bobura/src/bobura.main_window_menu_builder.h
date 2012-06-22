1e7eb8eb7aab66c87a25409c072db633eaefdeb9
kaoru <kaorut@mc.pikara.ne.jp>
2012-06-22 21:00:58 +0900

    Create bobura::main_window_menu_builder.

---
+++ bobura/src/bobura.main_window_menu_builder.h
@@ -0,0 +1,46 @@
+/*! \file
+    \brief The definition of bobura::main_window_menu_builder.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_MAINWINDOWMENUBUILDER_H)
+#define BOBURA_MAINWINDOWMENUBUILDER_H
+
+
+namespace bobura
+{
+    /*!
+        \brief The class template for the main window menu builder.
+
+        \tparam String A string type.
+        \tparam Path   A path type.
+    */
+    template <typename String, typename Path>
+    class main_window_menu_builder : private boost::noncopyable
+    {
+    public:
+        // types
+
+        //! The string type.
+        typedef String string_type;
+
+        //! The path type.
+        typedef Path path_type;
+
+
+        // constructors and destructor
+
+
+        // functions
+
+
+    };
+
+
+}
+
+
+#endif
