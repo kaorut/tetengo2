b8a160868c9611dce926739480609831a8cb1336
kaoru <kaorut@mc.pikara.ne.jp>
2012-06-24 06:33:10 +0900

    Created bobura::command::horizontally_zoom_in.

---
+++ bobura/src/bobura.command.horizontally_zoom_in.h
@@ -0,0 +1,59 @@
+/*! \file
+    \brief The definition of bobura::command::horizontally_zoom_in.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_COMMAND_NOP_H)
+#define BOBURA_COMMAND_NOP_H
+
+#include "bobura.command.command_base.h"
+
+
+namespace bobura { namespace command
+{
+    /*!
+        \brief The class template for a no-operation command.
+
+        \tparam Model          A model type.
+        \tparam AbstractWindow An abstract window type.
+    */
+    template <typename Model, typename AbstractWindow>
+    class horizontally_zoom_in : public command_base<Model, AbstractWindow>
+    {
+    public:
+        // types
+
+        //! The model type.
+        typedef Model model_type;
+
+        //! The abstract window type.
+        typedef AbstractWindow abstract_window_type;
+
+        //! The base type.
+        typedef command_base<model_type, abstract_window_type> base_type;
+
+
+    private:
+        // virtual functions
+
+        virtual bool enabled_impl(const model_type& model)
+        const
+        {
+            return true;
+        }
+
+        virtual void execute_impl(model_type& model, abstract_window_type& parent)
+        const
+        {}
+
+
+    };
+
+
+}}
+
+
+#endif
---
+++ bobura/test/test_bobura.command.horizontally_zoom_in.cpp
@@ -0,0 +1,22 @@
+/*! \file
+    \brief Test of class bobura::command::horizontally_zoom_in.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include "bobura.command.horizontally_zoom_in.h"
+
+
+BOOST_AUTO_TEST_SUITE(test_bobura)
+BOOST_AUTO_TEST_SUITE(command)
+BOOST_AUTO_TEST_SUITE(horizontally_zoom_in)
+    // test cases
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
