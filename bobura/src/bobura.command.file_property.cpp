f41759450749331d32fc3f9609f5acb1044c2738
kaoru <kaorut@mc.pikara.ne.jp>
2012-06-26 01:00:58 +0900

    Created bobura.command.file_property.cpp.

---
+++ bobura/src/bobura.command.file_property.cpp
@@ -0,0 +1,97 @@
+/*! \file
+    \brief The definition of bobura::command::file_property.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#include "bobura.command.file_property.h"
+
+
+namespace bobura { namespace command
+{
+    class file_property::impl
+    {
+    public:
+        // types
+
+        typedef file_property::model_type model_type;
+
+        typedef file_property::abstract_window_type abstract_window_type;
+
+        typedef file_property::timetable_type timetable_type;
+
+        typedef file_property::file_property_dialog_type file_property_dialog_type;
+
+        typedef file_property::dialog_base_type dialog_base_type;
+
+        typedef file_property::string_type string_type;
+
+        typedef file_property::message_catalog_type message_catalog_type;
+
+
+        // constructors and destructor
+
+        explicit impl(const message_catalog_type& message_catalog)
+        :
+        m_message_catalog(message_catalog)
+        {}
+
+
+        // functions
+
+        bool enabled(const model_type& model)
+        const
+        {
+            return true;
+        }
+
+        void execute(model_type& model, abstract_window_type& parent)
+        const
+        {
+            file_property_dialog_type dialog(parent, m_message_catalog);
+
+            dialog.set_line_name(model.timetable().title());
+            if (model.has_path())
+                dialog.set_file_name(model.path().template string<string_type>());
+
+            dialog.do_modal();
+            if (dialog.result() != dialog_base_type::result_accepted)
+                return;
+
+            model.timetable().set_title(dialog.line_name());
+        }
+
+
+    private:
+        // variables
+
+        const message_catalog_type& m_message_catalog;
+
+
+    };
+
+
+    file_property::file_property(const message_catalog_type& message_catalog)
+    :
+    m_p_impl(tetengo2::make_unique<impl>(message_catalog))
+    {}
+
+    file_property::~file_property()
+    {}
+    
+    bool file_property::enabled_impl(const model_type& model)
+    const
+    {
+        return m_p_impl->enabled(model);
+    }
+
+    void file_property::execute_impl(model_type& model, abstract_window_type& parent)
+    const
+    {
+        m_p_impl->execute(model, parent);
+    }
+
+
+}}
--- bobura/src/bobura.command.set.h
+++ bobura/src/bobura.command.set.h
@@ -19,6 +19,7 @@
 #include "bobura.command.about.h"
 #include "bobura.command.command_base.h"
 #include "bobura.command.exit.h"
+#include "bobura.command.file_property.h"
 #include "bobura.command.nop.h"
 
 
@@ -98,7 +99,6 @@ namespace bobura { namespace command
             const message_catalog_type&   message_catalog
         )
         :
-        //m_p_file_property(create_file_property(message_catalog)),
         //m_p_horizontally_zoom_in(create_horizontally_zoom_in(diagram_view)),
         //m_p_horizontally_zoom_out(create_horizontally_zoom_out(diagram_view)),
         //m_p_load_from_file(create_load_from_file(load_from_file)),
@@ -108,7 +108,7 @@ namespace bobura { namespace command
         //m_p_ask_file_path_and_save_to_file(create_save_to_file(ask_file_path_save_to_file))
         m_p_about(create_about(message_catalog, settings)),
         m_p_exit(create_exit()),
-        m_p_file_property(create_nop()),
+        m_p_file_property(create_file_property(message_catalog)),
         m_p_horizontally_zoom_in(create_nop()),
         m_p_horizontally_zoom_out(create_nop()),
         m_p_load_from_file(create_nop()),
@@ -261,12 +261,10 @@ namespace bobura { namespace command
             return tetengo2::make_unique<bobura::command::exit>();
         }
 
-        //static command_ptr_type create_file_property(const message_catalog_type& message_catalog)
-        //{
-        //    return tetengo2::make_unique<typename boost::mpl::at<type_list_type, type::file_property>::type>(
-        //        message_catalog
-        //    );
-        //}
+        static command_ptr_type create_file_property(const message_catalog_type& message_catalog)
+        {
+            return tetengo2::make_unique<bobura::command::file_property>(message_catalog);
+        }
 
         //static command_ptr_type create_horizontally_zoom_in(diagram_view_type& diagram_view)
         //{
