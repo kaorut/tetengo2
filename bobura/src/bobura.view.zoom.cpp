abdb0ce6e5122f7845e1e2ea5caf9f3f1611230b
kaoru <kaorut@mc.pikara.ne.jp>
2012-07-03 05:42:13 +0900

    Created bobura::view::zoom.

---
+++ bobura/src/bobura.view.zoom.cpp
@@ -0,0 +1,16 @@
+/*! \file
+    \brief The definition of bobura::view::zoom.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#include "bobura.view.zoom.h"
+
+
+namespace bobura { namespace view
+{
+
+
+}}
---
+++ bobura/src/bobura.view.zoom.h
@@ -0,0 +1,41 @@
+/*! \file
+    \brief The definition of bobura::view::zoom.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_VIEW_ZOOM_H)
+#define BOBURA_VIEW_ZOOM_H
+
+#include <boost/noncopyable.hpp>
+
+
+namespace bobura { namespace view
+{
+    /*!
+        \brief The class for a zoom of a view.
+    */
+    class zoom : boost::noncopyable
+    {
+    public:
+        // types
+
+
+        // constructors and destructor
+
+
+        // functions
+
+
+    private:
+
+
+    };
+
+
+}}
+
+
+#endif
