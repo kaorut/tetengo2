d90826cb90e5aac6cb5d2af544e377ac95ba9691
kaoru <kaorut@mc.pikara.ne.jp>
2012-07-14 15:38:34 +0900

    Modified the bzip2ed file extension.

--- bobura/src/bobura.load_save.save_to_file.h
+++ bobura/src/bobura.load_save.save_to_file.h
@@ -219,7 +219,7 @@ namespace bobura { namespace load_save
             filters.push_back(
                 std::make_pair(
                     m_message_catalog.get(TETENGO2_TEXT("Dialog:FileOpenSave:Timetable Files (Compressed)")),
-                    string_type(TETENGO2_TEXT("btt.bz2"))
+                    string_type(TETENGO2_TEXT("btt_bz2"))
                 )
             );
             filters.push_back(
