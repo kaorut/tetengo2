d0e317de61042be9f3cefede50045215fae6591c
kaoru <kaorut@mc.pikara.ne.jp>
2012-08-26 13:23:22 +0900

    Implemented test_bobura::font_color_dialog.

--- bobura/test/test_bobura.font_color_dialog.cpp
+++ bobura/test/test_bobura.font_color_dialog.cpp
@@ -80,112 +80,244 @@ BOOST_AUTO_TEST_SUITE(font_color_dialog)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        const font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        BOOST_CHECK_THROW(font_color_dialog.company_line_name(), std::logic_error);
     }
 
     BOOST_AUTO_TEST_CASE(set_company_line_name)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        font_color_dialog.set_company_line_name(
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true),
+            color_type(12, 34, 56)
+        );
+
+        BOOST_CHECK(
+            font_color_dialog.company_line_name().first ==
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true)
+        );
+        BOOST_CHECK(font_color_dialog.company_line_name().second == color_type(12, 34, 56));
     }
 
     BOOST_AUTO_TEST_CASE(note)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        const font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        BOOST_CHECK_THROW(font_color_dialog.note(), std::logic_error);
     }
 
     BOOST_AUTO_TEST_CASE(set_note)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        font_color_dialog.set_note(
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true),
+            color_type(12, 34, 56)
+        );
+
+        BOOST_CHECK(
+            font_color_dialog.note().first ==
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true)
+        );
+        BOOST_CHECK(font_color_dialog.note().second == color_type(12, 34, 56));
     }
 
     BOOST_AUTO_TEST_CASE(time_line)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        const font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        BOOST_CHECK_THROW(font_color_dialog.time_line(), std::logic_error);
     }
 
     BOOST_AUTO_TEST_CASE(set_time_line)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        font_color_dialog.set_time_line(
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true),
+            color_type(12, 34, 56)
+        );
+
+        BOOST_CHECK(
+            font_color_dialog.time_line().first ==
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true)
+        );
+        BOOST_CHECK(font_color_dialog.time_line().second == color_type(12, 34, 56));
     }
 
     BOOST_AUTO_TEST_CASE(local_station)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        const font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        BOOST_CHECK_THROW(font_color_dialog.local_station(), std::logic_error);
     }
 
     BOOST_AUTO_TEST_CASE(set_local_station)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        font_color_dialog.set_local_station(
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true),
+            color_type(12, 34, 56)
+        );
+
+        BOOST_CHECK(
+            font_color_dialog.local_station().first ==
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true)
+        );
+        BOOST_CHECK(font_color_dialog.local_station().second == color_type(12, 34, 56));
     }
 
     BOOST_AUTO_TEST_CASE(principal_station)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        const font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        BOOST_CHECK_THROW(font_color_dialog.principal_station(), std::logic_error);
     }
 
     BOOST_AUTO_TEST_CASE(set_principal_station)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        font_color_dialog.set_principal_station(
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true),
+            color_type(12, 34, 56)
+        );
+
+        BOOST_CHECK(
+            font_color_dialog.principal_station().first ==
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true)
+        );
+        BOOST_CHECK(font_color_dialog.principal_station().second == color_type(12, 34, 56));
     }
 
-    BOOST_AUTO_TEST_CASE(local_terminal_staiton)
+    BOOST_AUTO_TEST_CASE(local_terminal_station)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        const font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        BOOST_CHECK_THROW(font_color_dialog.local_terminal_station(), std::logic_error);
     }
 
     BOOST_AUTO_TEST_CASE(set_local_terminal_station)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        font_color_dialog.set_local_terminal_station(
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true),
+            color_type(12, 34, 56)
+        );
+
+        BOOST_CHECK(
+            font_color_dialog.local_terminal_station().first ==
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true)
+        );
+        BOOST_CHECK(font_color_dialog.local_terminal_station().second == color_type(12, 34, 56));
     }
 
     BOOST_AUTO_TEST_CASE(principal_terminal_station)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        const font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        BOOST_CHECK_THROW(font_color_dialog.principal_terminal_station(), std::logic_error);
     }
 
-    BOOST_AUTO_TEST_CASE(set_principal_terminal_staiton)
+    BOOST_AUTO_TEST_CASE(set_principal_terminal_station)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        font_color_dialog.set_principal_terminal_station(
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true),
+            color_type(12, 34, 56)
+        );
+
+        BOOST_CHECK(
+            font_color_dialog.principal_terminal_station().first ==
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true)
+        );
+        BOOST_CHECK(font_color_dialog.principal_terminal_station().second == color_type(12, 34, 56));
     }
 
     BOOST_AUTO_TEST_CASE(train_name)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        const font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        BOOST_CHECK_THROW(font_color_dialog.train_name(), std::logic_error);
     }
 
     BOOST_AUTO_TEST_CASE(set_train_name)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type window;
+        const message_catalog_type message_catalog;
+        font_color_dialog_type font_color_dialog(window, message_catalog);
+
+        font_color_dialog.set_train_name(font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true));
+
+        BOOST_CHECK(
+            font_color_dialog.train_name() ==
+            font_type(string_type(TETENGO2_TEXT("hoge")), 42, false, true, false, true)
+        );
     }
 
 
