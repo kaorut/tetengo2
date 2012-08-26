e4423dbe65b452cbd07364bde81aad143dcfc7a3
kaoru <kaorut@mc.pikara.ne.jp>
2012-08-27 07:00:21 +0900

    Created tetengo2::detail::windows::message_handler_detail::button and list_box.

---
+++ tetengo2_detail/include/tetengo2.detail.windows.message_handler_detail.button.h
@@ -0,0 +1,45 @@
+/*! \file
+    \brief The definition of
+           tetengo2::detail::windows::message_handler_detail::button.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(DOCUMENTATION)
+#if !defined(TETENGO2_DETAIL_WINDOWS_MESSAGEHANDLERDETAIL_BUTTON_H)
+#define TETENGO2_DETAIL_WINDOWS_MESSAGEHANDLERDETAIL_BUTTON_H
+
+#include <memory>
+#include <system_error>
+#include <tuple>
+
+#include <boost/optional.hpp>
+#include <boost/scope_exit.hpp>
+#include <boost/throw_exception.hpp>
+
+#pragma warning (push)
+#pragma warning (disable: 4005)
+#include <intsafe.h>
+#include <stdint.h>
+#pragma warning(pop)
+#define NOMINMAX
+#define OEMRESOURCE
+#include <Windows.h>
+
+
+namespace tetengo2 { namespace detail { namespace windows { namespace message_handler_detail
+{
+    namespace button
+    {
+
+
+    }
+
+
+}}}}
+
+
+#endif
+#endif
---
+++ tetengo2_detail/include/tetengo2.detail.windows.message_handler_detail.list_box.h
@@ -0,0 +1,45 @@
+/*! \file
+    \brief The definition of
+           tetengo2::detail::windows::message_handler_detail::list_box.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(DOCUMENTATION)
+#if !defined(TETENGO2_DETAIL_WINDOWS_MESSAGEHANDLERDETAIL_LISTBOX_H)
+#define TETENGO2_DETAIL_WINDOWS_MESSAGEHANDLERDETAIL_LISTBOX_H
+
+#include <memory>
+#include <system_error>
+#include <tuple>
+
+#include <boost/optional.hpp>
+#include <boost/scope_exit.hpp>
+#include <boost/throw_exception.hpp>
+
+#pragma warning (push)
+#pragma warning (disable: 4005)
+#include <intsafe.h>
+#include <stdint.h>
+#pragma warning(pop)
+#define NOMINMAX
+#define OEMRESOURCE
+#include <Windows.h>
+
+
+namespace tetengo2 { namespace detail { namespace windows { namespace message_handler_detail
+{
+    namespace list_box
+    {
+
+
+    }
+
+
+}}}}
+
+
+#endif
+#endif
