0e803d8a6f81a8db9eb1576420b6007e0c1a1c44
kaoru <kaorut@mc.pikara.ne.jp>
2012-11-22 06:35:40 +0900

    Created bobura.view.diagram.utility.h.

---
+++ bobura/src/bobura.view.diagram.utility.h
@@ -0,0 +1,20 @@
+/*! \file
+    \brief The definition of bobura::view::diagram::utility.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_VIEW_DIAGRAM_UTILITY_H)
+#define BOBURA_VIEW_DIAGRAM_UTILITY_H
+
+
+namespace bobura { namespace view { namespace diagram
+{
+
+
+}}}
+
+
+#endif
---
+++ bobura/test/test_bobura.view.diagram.utility.cpp
@@ -0,0 +1,23 @@
+/*! \file
+    \brief Test of class bobura::view::diagram::utility.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include "bobura.view.diagram.utility.h"
+
+
+
+BOOST_AUTO_TEST_SUITE(test_bobura)
+BOOST_AUTO_TEST_SUITE(view)
+BOOST_AUTO_TEST_SUITE(diagram)
+    // test cases
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
