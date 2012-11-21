86960201f0647cdad27693c7229ea18f61228a80
kaoru <kaorut@mc.pikara.ne.jp>
2012-11-22 05:59:30 +0900

    Created bobura::view::diagram::time_line_list.

---
+++ bobura/src/bobura.view.diagram.time_line.h
@@ -0,0 +1,120 @@
+/*! \file
+    \brief The definition of bobura::view::diagram::time_line.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_VIEW_DIAGRAM_TIMELINE_H)
+#define BOBURA_VIEW_DIAGRAM_TIMELINE_H
+
+#include <algorithm>
+#include <cassert>
+//#include <memory>
+//#include <utility>
+
+#include <tetengo2.cpp11.h>
+#include <tetengo2.gui.measure.h>
+#include <tetengo2.text.h>
+#include <tetengo2.unique.h>
+
+#include "bobura.view.diagram.item.h"
+
+
+namespace bobura { namespace view { namespace diagram
+{
+     /*!
+        \brief The class template for a time line list in the diagram view.
+
+        \tparam Model  A model type.
+        \tparam Canvas A canvas type.
+    */
+    template <typename Model, typename Canvas>
+    class time_line_list : public item<Canvas>
+    {
+    public:
+        // types
+
+        //! The model type.
+        typedef Model model_type;
+
+        //! The canvas type.
+        typedef Canvas canvas_type;
+
+        //! The position type.
+        typedef typename canvas_type::position_type position_type;
+
+        //! The dimension type.
+        typedef typename canvas_type::dimension_type dimension_type;
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a time line list.
+
+            \param model            A model.
+            \param canvas           A canvas.
+            \param canvas_dimension A canvas dimension.
+        */
+        time_line_list(const model_type& model, canvas_type& canvas, const dimension_type& canvas_dimension)
+        :
+        m_position(left_type(0), top_type(0)),
+        m_dimension(width_type(0), height_type(0))
+        {
+
+        }
+
+        /*!
+            \brief Destroys the time line list.
+        */
+        virtual ~time_line_list()
+        TETENGO2_CPP11_NOEXCEPT
+        {}
+
+
+    private:
+        // types
+
+        typedef typename canvas_type::string_type string_type;
+
+        typedef typename canvas_type::font_type font_type;
+
+        typedef typename canvas_type::color_type color_type;
+
+        typedef typename tetengo2::gui::position<position_type>::left_type left_type;
+
+        typedef typename tetengo2::gui::position<position_type>::top_type top_type;
+
+        typedef typename tetengo2::gui::dimension<dimension_type>::width_type width_type;
+
+        typedef typename tetengo2::gui::dimension<dimension_type>::height_type height_type;
+
+
+        // static functions
+
+
+        // variables
+
+        position_type m_position;
+
+        dimension_type m_dimension;
+
+
+        // virtual functions
+
+        virtual void draw_on_impl(canvas_type& canvas)
+        const
+        {
+
+        }
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ bobura/test/test_bobura.view.diagram.time_line.cpp
@@ -0,0 +1,84 @@
+/*! \file
+    \brief Test of class bobura::view::diagram::time_line.
+
+    Copyright (C) 2007-2012 kaoru
+
+    $Id$
+*/
+
+//#include <memory>
+
+//#include <boost/mpl/at.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <tetengo2.gui.measure.h>
+#include <tetengo2.text.h>
+
+#include "bobura.type_list.h"
+
+#include "bobura.view.diagram.time_line.h"
+
+
+namespace
+{
+    // types
+
+    typedef boost::mpl::at<bobura::common_type_list, bobura::type::string>::type string_type;
+
+    typedef boost::mpl::at<bobura::model_type_list, bobura::type::model::model>::type model_type;
+
+    typedef boost::mpl::at<bobura::ui_type_list, bobura::type::ui::window>::type window_type;
+
+    typedef boost::mpl::at<bobura::ui_type_list, bobura::type::ui::picture_box>::type picture_box_type;
+
+    typedef picture_box_type::position_type position_type;
+
+    typedef tetengo2::gui::position<position_type>::left_type left_type;
+
+    typedef tetengo2::gui::position<position_type>::top_type top_type;
+
+    typedef picture_box_type::dimension_type dimension_type;
+
+    typedef tetengo2::gui::dimension<dimension_type>::width_type width_type;
+
+    typedef tetengo2::gui::dimension<dimension_type>::height_type height_type;
+
+    typedef boost::mpl::at<bobura::ui_type_list, bobura::type::ui::canvas>::type canvas_type;
+
+    typedef canvas_type::font_type font_type;
+
+    typedef canvas_type::color_type color_type;
+
+    //typedef bobura::view::diagram::company_line_name_header<canvas_type> company_line_name_header_type;
+
+    //typedef bobura::view::diagram::note_header<canvas_type> note_header_type;
+
+    //typedef bobura::view::diagram::time_line<model_type, canvas_type> header_type;
+
+
+}
+
+
+BOOST_AUTO_TEST_SUITE(test_bobura)
+BOOST_AUTO_TEST_SUITE(view)
+BOOST_AUTO_TEST_SUITE(diagram)
+BOOST_AUTO_TEST_SUITE(time_line_list)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        //const model_type model;
+        //window_type window;
+        //const picture_box_type picture_box(window, picture_box_type::scroll_bar_style_type::both);
+        //const std::unique_ptr<canvas_type> p_canvas(picture_box.create_canvas());
+
+        //const header_type time_line(model, *p_canvas, dimension_type(width_type(42), height_type(24)));
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
