eaf79829b4bc25458f36d23ee902fc470806495f
kaoru <kaorut@mc.pikara.ne.jp>
2013-01-15 06:12:45 +0900

    Created bobura::load_save::select_oudia_diagram.

---
+++ bobura/src/bobura.load_save.select_oudia_diagram.h
@@ -0,0 +1,65 @@
+/*! \file
+    \brief The definition of bobura::load_save::select_oudia_diagram.
+
+    Copyright (C) 2007-2013 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_LOADSAVE_SELECTOUDIADIAGRAM_H)
+#define BOBURA_LOADSAVE_SELECTOUDIADIAGRAM_H
+
+
+namespace bobura { namespace load_save
+{
+    /*!
+        \brief The class template for an OuDia diagram selecting.
+
+        \tparam String A string type.
+    */
+    template <typename String>
+    class select_oudia_diagram
+    {
+    public:
+        // types
+
+        //! The string type.
+        typedef String string_type;
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates an OuDia diagram selecting.
+        */
+        select_oudia_diagram()
+        {}
+
+
+        // functions
+
+        /*!
+            \brief Selects an OuDia diagram.
+
+            \tparam FowardIterator A forward iterator type.
+
+            \param first The first position of the diagrams.
+            \param last  The last position of the diagrams.
+
+            \return The selected position.
+        */
+        template <typename ForwardIterator>
+        ForwardIterator operator()(const ForwardIterator first, const ForwardIterator last)
+        const
+        {
+            return first;
+        }
+
+
+    };
+
+
+}}
+
+
+#endif
---
+++ bobura/test/test_bobura.load_save.select_oudia_diagram.cpp
@@ -0,0 +1,51 @@
+/*! \file
+    \brief Test of class bobura::load_save::select_oudia_diagram.
+
+    Copyright (C) 2007-2013 kaoru
+
+    $Id$
+*/
+
+//#include <boost/mpl/at.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include "bobura.type_list.h"
+
+#include "bobura.load_save.select_oudia_diagram.h"
+
+
+namespace
+{
+    // types
+
+    typedef boost::mpl::at<bobura::common_type_list, bobura::type::string>::type string_type;
+
+    typedef bobura::load_save::select_oudia_diagram<string_type> select_oudia_diagram_type;
+
+
+}
+
+
+BOOST_AUTO_TEST_SUITE(test_bobura)
+BOOST_AUTO_TEST_SUITE(load_save)
+BOOST_AUTO_TEST_SUITE(select_oudia_diagram)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+    BOOST_AUTO_TEST_CASE(operator_paren)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
