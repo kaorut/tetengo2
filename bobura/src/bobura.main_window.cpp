445407526684f919d6e307a9b6a630f50a07f26d
kaoru <kaorut@mc.pikara.ne.jp>
2013-02-05 06:32:35 +0900

    Modified bobura::main_window.

---
+++ bobura/src/bobura.main_window.cpp
@@ -0,0 +1,24 @@
+/*! \file
+    \brief The definition of bobura::main_window.
+
+    Copyright (C) 2007-2013 kaoru
+
+    $Id$
+*/
+
+
+#include "bobura.type_list.h"
+
+#include "bobura.main_window.h"
+
+
+namespace bobura
+{
+    namespace
+    {
+
+
+    }
+
+
+}
