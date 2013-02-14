5f0625057d1624acce55ba881dc7798ffa7d7649
kaoru <kaorut@mc.pikara.ne.jp>
2013-02-15 06:58:45 +0900

    Implemented tetengo2::config::config_base.

--- tetengo2/include/tetengo2.config.config_base.h
+++ tetengo2/include/tetengo2.config.config_base.h
@@ -9,6 +9,12 @@
 #if !defined(TETENGO2_CONFIG_CONFIGBASE_H)
 #define TETENGO2_CONFIG_CONFIGBASE_H
 
+#include <utility>
+
+#include <boost/optional.hpp>
+
+#include "tetengo2.cpp11.h"
+
 
 namespace tetengo2 { namespace config
 {
@@ -16,8 +22,9 @@ namespace tetengo2 { namespace config
         \brief The class template for a configuration base.
 
         \tparam String A string type.
+        \tparam UInt   An unsigned integer type.
     */
-    template <typename String>
+    template <typename String, typename UInt>
     class config_base : private boost::noncopyable
     {
     public:
@@ -25,6 +32,9 @@ namespace tetengo2 { namespace config
 
         //! The string type.
         typedef String string_type;
+
+        //! The unsigned integer type.
+        typedef UInt uint_type;
 
 
         // constructors and destructor
@@ -45,6 +55,70 @@ namespace tetengo2 { namespace config
 
         // functions
         
+        /*!
+            \brief Returns the string value.
+
+            \param key A key.
+
+            \return The value.
+        */
+        boost::optional<string_type> get_string(const string_type& key)
+        const
+        {
+            return get_string_impl(key);
+        }
+
+        /*!
+            \brief Returns the unsigned integer value.
+
+            \param key A key.
+
+            \return The value.
+        */
+        boost::optional<uint_type> get_uint(const string_type& key)
+        const
+        {
+            return get_uint_impl(key);
+        }
+
+        /*!
+            \brief Sets a string value.
+
+            \param key   A key.
+            \param value A value.
+        */
+        void set(const string_type& key, string_type value)
+        {
+            set_impl(key, std::move(value));
+        }
+
+        /*!
+            \brief Sets a unsigned integer value.
+
+            \param key   A key.
+            \param value A value.
+        */
+        void set(const string_type& key, const uint_type value)
+        {
+            set_impl(key, value);
+        }
+
+
+    private:
+        // virtual functions
+
+        virtual boost::optional<string_type> get_string_impl(const string_type& key)
+        const = 0;
+
+        virtual boost::optional<uint_type> get_uint_impl(const string_type& key)
+        const = 0;
+
+        virtual void set_impl(const string_type& key, string_type value)
+        = 0;
+
+        virtual void set_impl(const string_type& key, const uint_type value)
+        = 0;
+
 
     };
 
--- tetengo2/test/test_tetengo2.config.config_base.cpp
+++ tetengo2/test/test_tetengo2.config.config_base.cpp
@@ -6,9 +6,11 @@
     $Id$
 */
 
+#include <boost/optional.hpp>
 #include <boost/test/unit_test.hpp>
 
 #include "test_tetengo2.type_list.h"
+#include "tetengo2.text.h"
 
 #include "tetengo2.config.config_base.h"
 
@@ -20,10 +22,37 @@ namespace
     typedef
         boost::mpl::at<test_tetengo2::type_list, test_tetengo2::type::string>::type string_type;
 
-    typedef tetengo2::config::config_base<string_type> config_base_type;
+    typedef
+        boost::mpl::at<test_tetengo2::type_list, test_tetengo2::type::size>::type uint_type;
 
-    struct concrete_config : public config_base_type
+    typedef tetengo2::config::config_base<string_type, uint_type> config_base_type;
+
+    class concrete_config : public config_base_type
     {
+    public:
+        concrete_config()
+        :
+        config_base_type()
+        {}
+
+    private:
+        virtual boost::optional<string_type> get_string_impl(const string_type& key)
+        const
+        {
+            return boost::make_optional(string_type(TETENGO2_TEXT("hoge")));
+        }
+
+        virtual boost::optional<uint_type> get_uint_impl(const string_type& key)
+        const
+        {
+            return boost::make_optional<uint_type>(42);
+        }
+
+        virtual void set_impl(const string_type& key, string_type value)
+        {}
+
+        virtual void set_impl(const string_type& key, const uint_type value)
+        {}
 
     };
 
@@ -40,6 +69,46 @@ BOOST_AUTO_TEST_SUITE(config_base)
         BOOST_TEST_PASSPOINT();
 
         const concrete_config config;
+    }
+
+    BOOST_AUTO_TEST_CASE(get_string)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const concrete_config config;
+
+        const boost::optional<string_type> value = config.get_string(string_type(TETENGO2_TEXT("foo")));
+        BOOST_REQUIRE(value);
+        BOOST_CHECK(*value == string_type(TETENGO2_TEXT("hoge")));
+    }
+
+    BOOST_AUTO_TEST_CASE(get_uint)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        const concrete_config config;
+
+        const boost::optional<uint_type> value = config.get_uint(string_type(TETENGO2_TEXT("bar")));
+        BOOST_REQUIRE(value);
+        BOOST_CHECK_EQUAL(*value, 42U);
+    }
+
+    BOOST_AUTO_TEST_CASE(set_string)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        concrete_config config;
+
+        config.set(string_type(TETENGO2_TEXT("foo")), string_type(TETENGO2_TEXT("hoge")));
+    }
+
+    BOOST_AUTO_TEST_CASE(set_uint)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        concrete_config config;
+
+        config.set(string_type(TETENGO2_TEXT("bar")), 42);
     }
 
     
