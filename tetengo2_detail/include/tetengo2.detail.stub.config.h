b3bdf3a7da243aacff73eb9b9654f57da14c3890
kaoru <kaorut@mc.pikara.ne.jp>
2013-02-17 09:32:45 +0900

    Created tetengo2::detail::stub/windows::config.

---
+++ tetengo2_detail/include/tetengo2.detail.stub.config.h
@@ -0,0 +1,35 @@
+/*! \file
+    \brief The definition of tetengo2::detail::stub::config.
+
+    Copyright (C) 2007-2013 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_STUB_CONFIG_H)
+#define TETENGO2_DETAIL_STUB_CONFIG_H
+
+#include <boost/noncopyable.hpp>
+
+
+namespace tetengo2 { namespace detail { namespace stub
+{
+    /*!
+        \brief The class for a detail implementation of a config.
+    */
+    class config : private boost::noncopyable
+    {
+    public:
+        // types
+
+
+        // static functions
+
+
+    };
+
+
+}}}
+
+
+#endif
---
+++ tetengo2_detail/include/tetengo2.detail.windows.config.h
@@ -0,0 +1,44 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::config.
+
+    Copyright (C) 2007-2013 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_WINDOWS_CONFIG_H)
+#define TETENGO2_DETAIL_WINDOWS_CONFIG_H
+
+#include <boost/noncopyable.hpp>
+
+//#pragma warning (push)
+//#pragma warning (disable: 4005)
+//#include <intsafe.h>
+//#include <stdint.h>
+//#pragma warning(pop)
+//#define NOMINMAX
+//#define OEMRESOURCE
+//#include <Windows.h>
+
+
+namespace tetengo2 { namespace detail { namespace windows
+{
+    /*!
+        \brief The class for a detail implementation of a config.
+    */
+    class config : private boost::noncopyable
+    {
+    public:
+        // types
+
+
+        // static functions
+
+
+    };
+
+
+}}}
+
+
+#endif
