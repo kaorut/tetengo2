56937303cfec20767c5289a2e07cef91c32cfdd6
kaoru <kaorut@mc.pikara.ne.jp>
2013-02-19 06:57:51 +0900

    Created bobura::config_traits.

---
+++ bobura/src/bobura.config_traits.h
@@ -0,0 +1,26 @@
+/*! \file
+    \brief The definition of bobura::config_traits.
+
+    Copyright (C) 2007-2013 kaoru
+
+    $Id$
+*/
+
+#if !defined(BOBURA_CONFIGTRAITS_H)
+#define BOBURA_CONFIGTRAITS_H
+
+
+namespace bobura
+{
+   /*!
+        \brief The class for a configuration traits.
+    */
+    class config_traits
+    {
+    public:
+        // types
+
+
+}
+
+#endif
