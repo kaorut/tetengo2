bf0d12b13aee91cf7874f1410c0ddbccbd400dee
kaoru <kaorut@mc.pikara.ne.jp>
2013-02-26 07:02:37 +0900

    Skipped a compilation error.

--- bobura/test/test_bobura.view.diagram.time_line.cpp
+++ bobura/test/test_bobura.view.diagram.time_line.cpp
@@ -69,6 +69,9 @@ namespace
 }
 
 
+#if defined(__GNUC__) && defined(SKIP_COMPILATION)
+#   warning Skipped the compilation to avoid errors.
+#else
 BOOST_AUTO_TEST_SUITE(test_bobura)
 BOOST_AUTO_TEST_SUITE(view)
 BOOST_AUTO_TEST_SUITE(diagram)
@@ -183,3 +186,4 @@ BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
 BOOST_AUTO_TEST_SUITE_END()
+#endif
