68594ea6fa2ba5b375b7dcffcc05c41af49db355
kaoru <kaorut@mc.pikara.ne.jp>
2013-03-08 21:02:37 +0900

    Created make_test_case_list.pl/sh.

---
+++ tools/make_test_case_list.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Makes a test case list.
+# Copyright (C) 2007-2013 kaoru
+# $Id$
+
+LANG=C
+SOLUTIONDIR=`dirname $0`/..
+. $SOLUTIONDIR/tools/paths.sh
+
+rm -f "$SOLUTIONDIR/tools/tmp"
+
+for f in `list_test_sources $SOLUTIONDIR`;
+do
+    echo -ne "Parsing: $f        \r" >&2;
+    "$SOLUTIONDIR/tools/make_test_case_list.pl" "$f" >> "$SOLUTIONDIR/tools/tmp";
+done
+echo -ne "Done.                                                        \n" >&2
+
+sort < "$SOLUTIONDIR/tools/tmp" | uniq;
+
+rm -f "$SOLUTIONDIR/tools/tmp"
