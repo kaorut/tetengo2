d5fc391b96da1031e0187051fc763ae4a9d5cae4
kaoru <kaorut@mc.pikara.ne.jp>
2013-03-09 07:24:29 +0900

    Changed the output file names of check_test_cases.sh.

--- tools/check_test_cases.sh
+++ tools/check_test_cases.sh
@@ -6,12 +6,11 @@
 LANG=C
 SOLUTIONDIR=`dirname $0`/..
 
-test -e "$SOLUTIONDIR/tools/zzz_functions.txt" || \
-    "$SOLUTIONDIR/tools/zzz_make_function_list.sh" > "$SOLUTIONDIR/tools/zzz_functions.txt"
-test -e "$SOLUTIONDIR/tools/zzz_test_cases.txt" || \
-    "$SOLUTIONDIR/tools/zzz_make_test_case_list.sh" > "$SOLUTIONDIR/tools/zzz_test_cases.txt"
+FUNCTIONS_TXT="$SOLUTIONDIR/tools/zzz_test_case_check_functions.txt"
+TESTCASES_TXT="$SOLUTIONDIR/tools/zzz_test_case_check_test_cases.txt"
+
+test -e "$FUNCTIONS_TXT" || "$SOLUTIONDIR/tools/zzz_make_function_list.sh" > "$FUNCTIONS_TXT"
+test -e "$TESTCASES_TXT" || "$SOLUTIONDIR/tools/zzz_make_test_case_list.sh" > "$TESTCASES_TXT"
 
 "$SOLUTIONDIR/tools/zzz_check_test_cases.pl" \
-    "$SOLUTIONDIR/tools/zzz_functions.txt" \
-    "$SOLUTIONDIR/tools/zzz_test_cases.txt" \
-    "$SOLUTIONDIR/tools/test_case_check_ignore.txt"
+    "$FUNCTIONS_TXT" "$TESTCASES_TXT" "$SOLUTIONDIR/tools/test_case_check_ignore.txt"
