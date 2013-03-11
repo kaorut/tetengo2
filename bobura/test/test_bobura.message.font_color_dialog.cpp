3856bb2814d7716eb8f7035a9ff6da3a51aaccf4
kaoru <kaorut@mc.pikara.ne.jp>
2013-03-12 06:42:31 +0900

    Implemented test cases.

--- bobura/test/test_bobura.message.font_color_dialog.cpp
+++ bobura/test/test_bobura.message.font_color_dialog.cpp
@@ -6,8 +6,15 @@
     $Id$
 */
 
+#include <memory>
+#include <utility>
+#include <vector>
+
 //#include <boost/mpl/at.hpp>
+#include <boost/optional.hpp>
 #include <boost/test/unit_test.hpp>
+
+#include <tetengo2.gui.measure.h>
 
 #include "bobura.type_list.h"
 
@@ -18,9 +25,13 @@ namespace
 {
     // types
 
-    typedef boost::mpl::at<bobura::ui_type_list, bobura::type::ui::dialog>::type dialog_type;
+    typedef boost::mpl::at<bobura::common_type_list, bobura::type::size>::type size_type;
+
+    typedef boost::mpl::at<bobura::locale_type_list, bobura::type::locale::message_catalog>::type message_catalog_type;
 
     typedef boost::mpl::at<bobura::ui_type_list, bobura::type::ui::window>::type window_type;
+
+    typedef boost::mpl::at<bobura::ui_type_list, bobura::type::ui::dialog>::type dialog_type;
 
     class concrete_dialog : public dialog_type
     {
@@ -36,6 +47,31 @@ namespace
 
     };
 
+    typedef boost::mpl::at<bobura::ui_type_list, bobura::type::ui::list_box>::type list_box_type;
+
+    typedef window_type::dimension_type dimension_type;
+
+    typedef tetengo2::gui::dimension<dimension_type>::width_type width_type;
+
+    typedef tetengo2::gui::dimension<dimension_type>::height_type height_type;
+
+    typedef boost::mpl::at<bobura::ui_type_list, bobura::type::ui::canvas>::type canvas_type;
+
+    typedef canvas_type::font_type font_type;
+
+    typedef canvas_type::color_type color_type;
+
+    typedef boost::mpl::at<bobura::common_dialog_type_list, bobura::type::common_dialog::font>::type font_dialog_type;
+
+    typedef
+        boost::mpl::at<bobura::common_dialog_type_list, bobura::type::common_dialog::color>::type color_dialog_type;
+
+
+    // functions
+
+    void update()
+    {}
+
 
 }
 
@@ -50,14 +86,24 @@ BOOST_AUTO_TEST_SUITE(category_list_box_selection_changed)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        boost::optional<size_type> current_category_index;
+        window_type parent;
+        list_box_type list_box(parent, list_box_type::scroll_bar_style_type::none);
+        const bobura::message::font_color_dialog::category_list_box_selection_changed<size_type, list_box_type>
+        observer(current_category_index, list_box, update);
     }
 
     BOOST_AUTO_TEST_CASE(operator_paren)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        boost::optional<size_type> current_category_index;
+        window_type parent;
+        list_box_type list_box(parent, list_box_type::scroll_bar_style_type::none);
+        const bobura::message::font_color_dialog::category_list_box_selection_changed<size_type, list_box_type>
+        observer(current_category_index, list_box, update);
+
+        observer();
     }
 
 
@@ -69,14 +115,48 @@ BOOST_AUTO_TEST_SUITE(sample_picture_box_paint)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        const std::vector<std::pair<boost::optional<font_type>, boost::optional<color_type>>> font_color_list(
+            2,
+            std::pair<boost::optional<font_type>, boost::optional<color_type>>(
+                boost::make_optional<font_type>(font_type::dialog_font()),
+                boost::make_optional<color_type>(color_type(0x12, 0x34, 0x56))
+            )
+        );
+        const message_catalog_type message_catalog;
+        const bobura::message::font_color_dialog::sample_picture_box_paint<
+            size_type, canvas_type, message_catalog_type
+        > observer(
+            font_color_list,
+            boost::make_optional<size_type>(1), 
+            dimension_type(width_type(42), height_type(24)),
+            message_catalog
+        );
     }
 
     BOOST_AUTO_TEST_CASE(operator_paren)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        const std::vector<std::pair<boost::optional<font_type>, boost::optional<color_type>>> font_color_list(
+            2,
+            std::pair<boost::optional<font_type>, boost::optional<color_type>>(
+                boost::make_optional<font_type>(font_type::dialog_font()),
+                boost::make_optional<color_type>(color_type(0x12, 0x34, 0x56))
+            )
+        );
+        const message_catalog_type message_catalog;
+        const bobura::message::font_color_dialog::sample_picture_box_paint<
+            size_type, canvas_type, message_catalog_type
+        > observer(
+            font_color_list,
+            boost::make_optional<size_type>(1), 
+            dimension_type(width_type(42), height_type(24)),
+            message_catalog
+        );
+
+        window_type window;
+        const std::unique_ptr<canvas_type> p_canvas = window.create_canvas();
+        observer(*p_canvas);
     }
 
 
@@ -88,14 +168,52 @@ BOOST_AUTO_TEST_SUITE(font_button_mouse_clicked)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type parent;
+        concrete_dialog dialog(parent);
+        std::vector<std::pair<boost::optional<font_type>, boost::optional<color_type>>> font_color_list(
+            2,
+            std::pair<boost::optional<font_type>, boost::optional<color_type>>(
+                boost::make_optional<font_type>(font_type::dialog_font()),
+                boost::make_optional<color_type>(color_type(0x12, 0x34, 0x56))
+            )
+        );
+        const message_catalog_type message_catalog;
+        const bobura::message::font_color_dialog::font_button_mouse_clicked<
+            size_type, dialog_type, font_dialog_type, canvas_type, message_catalog_type
+        > observer(
+            dialog,
+            font_color_list,
+            boost::make_optional<size_type>(1), 
+            update,
+            message_catalog
+        );
     }
 
     BOOST_AUTO_TEST_CASE(operator_paren)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type parent;
+        concrete_dialog dialog(parent);
+        std::vector<std::pair<boost::optional<font_type>, boost::optional<color_type>>> font_color_list(
+            2,
+            std::pair<boost::optional<font_type>, boost::optional<color_type>>(
+                boost::make_optional<font_type>(font_type::dialog_font()),
+                boost::make_optional<color_type>(color_type(0x12, 0x34, 0x56))
+            )
+        );
+        const message_catalog_type message_catalog;
+        const bobura::message::font_color_dialog::font_button_mouse_clicked<
+            size_type, dialog_type, font_dialog_type, canvas_type, message_catalog_type
+        > observer(
+            dialog,
+            font_color_list,
+            boost::make_optional<size_type>(1), 
+            update,
+            message_catalog
+        );
+
+        observer();
     }
 
 
@@ -107,14 +225,52 @@ BOOST_AUTO_TEST_SUITE(color_button_mouse_clicked)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type parent;
+        concrete_dialog dialog(parent);
+        std::vector<std::pair<boost::optional<font_type>, boost::optional<color_type>>> font_color_list(
+            2,
+            std::pair<boost::optional<font_type>, boost::optional<color_type>>(
+                boost::make_optional<font_type>(font_type::dialog_font()),
+                boost::make_optional<color_type>(color_type(0x12, 0x34, 0x56))
+            )
+        );
+        const message_catalog_type message_catalog;
+        const bobura::message::font_color_dialog::color_button_mouse_clicked<
+            size_type, dialog_type, color_dialog_type, canvas_type, message_catalog_type
+        > observer(
+            dialog,
+            font_color_list,
+            boost::make_optional<size_type>(1), 
+            update,
+            message_catalog
+        );
     }
 
     BOOST_AUTO_TEST_CASE(operator_paren)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type parent;
+        concrete_dialog dialog(parent);
+        std::vector<std::pair<boost::optional<font_type>, boost::optional<color_type>>> font_color_list(
+            2,
+            std::pair<boost::optional<font_type>, boost::optional<color_type>>(
+                boost::make_optional<font_type>(font_type::dialog_font()),
+                boost::make_optional<color_type>(color_type(0x12, 0x34, 0x56))
+            )
+        );
+        const message_catalog_type message_catalog;
+        const bobura::message::font_color_dialog::color_button_mouse_clicked<
+            size_type, dialog_type, color_dialog_type, canvas_type, message_catalog_type
+        > observer(
+            dialog,
+            font_color_list,
+            boost::make_optional<size_type>(1), 
+            update,
+            message_catalog
+        );
+
+        observer();
     }
 
 
@@ -151,14 +307,20 @@ BOOST_AUTO_TEST_SUITE(cancel_button_mouse_clicked)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type parent;
+        concrete_dialog dialog(parent);
+        const bobura::message::font_color_dialog::cancel_button_mouse_clicked<dialog_type> observer(dialog);
     }
 
     BOOST_AUTO_TEST_CASE(operator_paren)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        window_type parent;
+        concrete_dialog dialog(parent);
+        const bobura::message::font_color_dialog::cancel_button_mouse_clicked<dialog_type> observer(dialog);
+
+        observer();
     }
 
 
