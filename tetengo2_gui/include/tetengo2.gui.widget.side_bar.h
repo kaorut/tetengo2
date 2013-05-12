521d7f45ed535bb358669a9c37124e369f27b370
kaoru <kaorut@mc.pikara.ne.jp>
2013-05-12 11:23:48 +0900

    Implemented tetengo2::gui::widget::side_bar.

--- tetengo2_gui/include/tetengo2.gui.widget.side_bar.h
+++ tetengo2_gui/include/tetengo2.gui.widget.side_bar.h
@@ -10,6 +10,7 @@
 #define TETENGO2_GUI_WIDGET_SIDEBAR_H
 
 #include <algorithm>
+#include <memory>
 //#include <utility>
 //#include <vector>
 
@@ -80,7 +81,8 @@ namespace tetengo2 { namespace gui { namespace widget
         */
         explicit side_bar(widget_type& parent)
         :
-        base_type(parent, base_type::scroll_bar_style_type::none)
+        base_type(parent, base_type::scroll_bar_style_type::none),
+        m_p_items()
         {
             initialize_side_bar(this);
         }
@@ -108,6 +110,116 @@ namespace tetengo2 { namespace gui { namespace widget
 
         typedef typename gui::dimension<dimension_type>::height_type height_type;
 
+        class item
+        {
+        public:
+            // constructors and destructors
+
+            virtual ~item()
+            TETENGO2_CPP11_NOEXCEPT
+            {}
+
+
+            // functions
+
+            void draw(canvas_type& canvas)
+            {
+                draw_impl(canvas);
+            }
+
+
+        protected:
+            // constructors
+
+            item(position_type&& position, dimension_type&& dimension)
+            :
+            m_position(std::move(position)),
+            m_dimension(std::move(dimension))
+            {}
+
+
+            // functions
+
+            const position_type& position()
+            const
+            {
+                return m_position;
+            }
+
+            const dimension_type& dimension()
+            const
+            {
+                return m_dimension;
+            }
+
+
+        private:
+            // variables
+
+            const position_type m_position;
+
+            const dimension_type m_dimension;
+
+
+            // virtual functions
+
+            virtual void draw_impl(canvas_type& canvas)
+            = 0;
+
+
+        };
+
+        class caption : public item
+        {
+        public:
+            // constructors and destructor
+
+            explicit caption(const side_bar& side_bar_)
+            :
+            item(position_type(left_type(0), top_type(0)), calculate_dimension(side_bar_))
+            {}
+
+        private:
+            // static functions
+
+            static const dimension_type& state_button_dimension()
+            {
+                static const dimension_type singleton(width_type(1), height_type(1));
+                return singleton;
+            }
+
+            static dimension_type calculate_dimension(const side_bar& side_bar_)
+            {
+                static const height_type padding = height_type(1) / 4;
+                static const height_type text_height(1);
+
+                const dimension_type client_dimension = side_bar_.client_dimension();
+                const auto& width = gui::dimension<dimension_type>::width(client_dimension);
+                const auto& height =
+                    std::max(text_height, gui::dimension<dimension_type>::height(state_button_dimension())); 
+            
+                return dimension_type(width, height + padding * 2);
+            }
+
+
+            // virtual functions
+
+            virtual void draw_impl(canvas_type& canvas)
+            override
+            {
+                auto original_background = canvas.background().clone();
+                canvas.set_background(
+                    make_unique<solid_background_type>(system_color_set_type::title_bar_background())
+                );
+
+                canvas.fill_rectangle(position(), dimension());
+
+                canvas.set_background(std::move(original_background));
+            }
+
+
+        };
+
 
         // static functions
 
@@ -117,19 +229,25 @@ namespace tetengo2 { namespace gui { namespace widget
                 make_unique<solid_background_type>(system_color_set_type::dialog_background())
             );
 
+            p_side_bar->m_p_items.push_back(make_unique<caption>(*p_side_bar));
+
             p_side_bar->paint_observer_set().paint_background().disconnect_all_slots();
             p_side_bar->paint_observer_set().paint_background().connect(
                 [p_side_bar](canvas_type& canvas) { return paint_background(*p_side_bar, canvas); }
             );
             p_side_bar->paint_observer_set().paint().connect(
-                [p_side_bar](canvas_type& canvas) { draw_caption(*p_side_bar, canvas); }
+                [p_side_bar](canvas_type& canvas)
+                {
+                    std::for_each(
+                        p_side_bar->m_p_items.begin(),
+                        p_side_bar->m_p_items.end(),
+                        [&canvas](const std::unique_ptr<item>& p_item)
+                        {
+                            p_item->draw(canvas);
+                        }
+                    );
+                }
             );
-        }
-
-        static const dimension_type& state_button_dimension()
-        {
-            static const dimension_type singleton(width_type(1), height_type(1));
-            return singleton;
         }
 
         static bool paint_background(const side_bar& side_bar, canvas_type& canvas)
@@ -143,84 +261,72 @@ namespace tetengo2 { namespace gui { namespace widget
             return true;
         }
 
-        static void draw_caption(const side_bar& side_bar, canvas_type& canvas)
-        {
-            auto original_color = canvas.color();
-            auto original_background = canvas.background().clone();
-            canvas.set_color(system_color_set_type::title_bar_text());
-            canvas.set_background(make_unique<solid_background_type>(system_color_set_type::title_bar_background()));
+        //static void draw_caption(const side_bar& side_bar, canvas_type& canvas)
+        //{
+        //    auto original_color = canvas.color();
+        //    auto original_background = canvas.background().clone();
+        //    canvas.set_color(system_color_set_type::title_bar_text());
+        //    canvas.set_background(make_unique<solid_background_type>(system_color_set_type::title_bar_background()));
 
-            static const height_type padding = height_type(1) / 4;
+        //    static const height_type padding = height_type(1) / 4;
 
-            const auto text_dimension = canvas.calc_text_dimension(side_bar.text());
-            const auto& client_dimension = side_bar.client_dimension();
-            canvas.fill_rectangle(
-                position_type(left_type(0), top_type(0)), caption_dimension(client_dimension, text_dimension, padding)
-            );
+        //    const auto text_dimension = canvas.calc_text_dimension(side_bar.text());
+        //    const auto& client_dimension = side_bar.client_dimension();
+        //    canvas.fill_rectangle(
+        //        position_type(left_type(0), top_type(0)), caption_dimension(client_dimension, text_dimension, padding)
+        //    );
 
-            draw_state_button(canvas, padding);
+        //    draw_state_button(canvas, padding);
 
-            canvas.draw_text(side_bar.text(), text_position(text_dimension, padding));
+        //    canvas.draw_text(side_bar.text(), text_position(text_dimension, padding));
 
-            canvas.set_background(std::move(original_background));
-            canvas.set_color(std::move(original_color));
-        }
+        //    canvas.set_background(std::move(original_background));
+        //    canvas.set_color(std::move(original_color));
+        //}
 
-        static dimension_type caption_dimension(
-            const dimension_type& client_dimension,
-            const dimension_type& text_dimension,
-            const height_type&    padding
-        )
-        {
-            const auto& width = gui::dimension<dimension_type>::width(client_dimension);
-            
-            const auto& height =
-                std::max(
-                    gui::dimension<dimension_type>::height(text_dimension),
-                    gui::dimension<dimension_type>::height(state_button_dimension())
-                ); 
-            
-            return dimension_type(width, height + padding * 2);
-        }
+        //static void draw_state_button(canvas_type& canvas, const height_type& padding)
+        //{
+        //    auto original_color = canvas.color();
+        //    auto original_line_width = canvas.line_width();
+        //    auto original_background = canvas.background().clone();
+        //    canvas.set_color(color_type(0, 0, 0));
+        //    canvas.set_line_width(size_type(1) / 16);
+        //    canvas.set_background(make_unique<solid_background_type>(color_type(255, 255, 255)));
 
-        static void draw_state_button(canvas_type& canvas, const height_type& padding)
-        {
-            auto original_color = canvas.color();
-            auto original_line_width = canvas.line_width();
-            auto original_background = canvas.background().clone();
-            canvas.set_color(color_type(0, 0, 0));
-            canvas.set_line_width(size_type(1) / 16);
-            canvas.set_background(make_unique<solid_background_type>(color_type(255, 255, 255)));
+        //    static const auto& width = gui::dimension<dimension_type>::width(state_button_dimension());
+        //    static const auto& height = gui::dimension<dimension_type>::height(state_button_dimension());
+        //    std::vector<position_type> positions;
+        //    positions.emplace_back(left_type::from(padding), top_type::from(padding));
+        //    positions.emplace_back(left_type::from(padding + width), top_type::from(padding + height / 2));
+        //    positions.emplace_back(left_type::from(padding), top_type::from(padding + height));
 
-            static const auto& width = gui::dimension<dimension_type>::width(state_button_dimension());
-            static const auto& height = gui::dimension<dimension_type>::height(state_button_dimension());
-            std::vector<position_type> positions;
-            positions.emplace_back(left_type::from(padding), top_type::from(padding));
-            positions.emplace_back(left_type::from(padding + width), top_type::from(padding + height / 2));
-            positions.emplace_back(left_type::from(padding), top_type::from(padding + height));
+        //    canvas.fill_polygon(positions.begin(), positions.end());
+        //    canvas.draw_polygon(positions.begin(), positions.end());
 
-            canvas.fill_polygon(positions.begin(), positions.end());
-            canvas.draw_polygon(positions.begin(), positions.end());
+        //    canvas.set_background(std::move(original_background));
+        //    canvas.set_line_width(std::move(original_line_width));
+        //    canvas.set_color(std::move(original_color));
+        //}
 
-            canvas.set_background(std::move(original_background));
-            canvas.set_line_width(std::move(original_line_width));
-            canvas.set_color(std::move(original_color));
-        }
+        //static position_type text_position(const dimension_type& text_dimension, const height_type& padding)
+        //{
+        //    left_type left =
+        //        left_type::from(gui::dimension<dimension_type>::width(state_button_dimension()) + padding * 2);
 
-        static position_type text_position(const dimension_type& text_dimension, const height_type& padding)
-        {
-            left_type left =
-                left_type::from(gui::dimension<dimension_type>::width(state_button_dimension()) + padding * 2);
+        //    const auto& text_height = gui::dimension<dimension_type>::height(text_dimension);
+        //    const auto& status_button_height = gui::dimension<dimension_type>::height(state_button_dimension());
+        //    top_type top =
+        //        text_height < status_button_height ?
+        //        top_type::from((status_button_height - text_height) / 2) + top_type::from(padding) :
+        //        top_type::from(padding);
+        //    
+        //    return position_type(std::move(left), std::move(top));
+        //}
 
-            const auto& text_height = gui::dimension<dimension_type>::height(text_dimension);
-            const auto& status_button_height = gui::dimension<dimension_type>::height(state_button_dimension());
-            top_type top =
-                text_height < status_button_height ?
-                top_type::from((status_button_height - text_height) / 2) + top_type::from(padding) :
-                top_type::from(padding);
-            
-            return position_type(std::move(left), std::move(top));
-        }
+
+        // variables
+
+        std::vector<std::unique_ptr<item>> m_p_items;
 
 
     };
