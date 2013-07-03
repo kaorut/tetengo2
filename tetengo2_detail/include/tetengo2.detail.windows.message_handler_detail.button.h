0900c4b59b6b69c07183a83aedcb475e9e9b42ec
kaoru <kaorut@mc.pikara.ne.jp>
2013-07-04 06:46:11 +0900

    Stop using a timer to detect a mouse click

--- tetengo2_detail/include/tetengo2.detail.windows.message_handler_detail.button.h
+++ tetengo2_detail/include/tetengo2.detail.windows.message_handler_detail.button.h
@@ -30,37 +30,6 @@ namespace tetengo2 { namespace detail { namespace windows { namespace message_ha
     namespace button
     {
         template <typename Button>
-        boost::optional< ::LRESULT> on_timer(Button& button, const ::WPARAM w_param, const ::LPARAM l_param)
-        {
-            suppress_unused_variable_warning(l_param);
-
-            if (w_param == WM_LBUTTONDOWN)
-            {
-                if (
-                    button.mouse_observer_set().clicked().empty() &&
-                    button.mouse_observer_set().doubleclicked().empty()
-                )
-                {
-                    return boost::none;
-                }
-
-                const auto result = ::KillTimer(button.details()->handle.get(), WM_LBUTTONDOWN);
-                if (result == 0)
-                {
-                    BOOST_THROW_EXCEPTION(
-                        std::system_error(
-                            std::error_code(::GetLastError(), win32_category()), "Can't kill a timer for mouse clicks."
-                        )
-                    );
-                }
-
-                return boost::make_optional< ::LRESULT>(0);
-            }
-
-            return boost::none;
-        }
-
-        template <typename Button>
         boost::optional< ::LRESULT> on_tetengo2_command(Button& button, const ::WPARAM w_param, const ::LPARAM l_param)
         {
             suppress_unused_variable_warning(w_param, l_param);
