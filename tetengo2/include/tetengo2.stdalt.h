2c14d68e4f31fe5feda1c8feed81c5645070c8e3
kaoru <kaorut@mc.pikara.ne.jp>
2013-07-12 06:22:59 +0900

    Copy tetengo2::make_unique() to tetengo2::stdalt::make_unique()

--- tetengo2/include/tetengo2.stdalt.h
+++ tetengo2/include/tetengo2.stdalt.h
@@ -12,7 +12,7 @@
 #include "tetengo2.utility.h"
 
 
-/* noexcept *****************************************************************/
+/* noexcept *******************************************************************/
 
 #if !defined(DOCUMENTATION)
 #   if (defined(__GNUC__) && defined(__GNUC_MINOR__) && __GNUC__ >= 4 && __GNUC_MINOR__ >= 7)
@@ -30,7 +30,7 @@
 #endif
 
 
-/* insertion iterator *******************************************************/
+/* insertion iterator *********************************************************/
 
 #if !defined(DOCUMENTATION)
 #   if (defined(_MSC_VER) && _MSC_VER >= 1600)
@@ -84,7 +84,7 @@ namespace tetengo2 { namespace stdalt
 }}
 
 
-/* lambda this capture bug workaround ***************************************/
+/* lambda this capture bug workaround *****************************************/
 
 #if !defined(DOCUMENTATION)
 #   if (defined(__GNUC__) && defined(__GNUC_MINOR__) && __GNUC__ == 4 && __GNUC_MINOR__ <= 7)
@@ -102,7 +102,7 @@ namespace tetengo2 { namespace stdalt
 #endif
 
 
-/* thread *******************************************************************/
+/* thread *********************************************************************/
 
 #if !defined(DOCUMENTATION)
 #   if (defined(__GNUC__) && defined(__GNUC_MINOR__) && __GNUC__ >= 4 && __GNUC_MINOR__ >= 7)
@@ -183,6 +183,363 @@ namespace tetengo2 { namespace stdalt
     void this_thread_sleep_for(const Int ms)
     {
         boost::this_thread::sleep_for(boost::chrono::milliseconds(ms));
+    }
+#endif
+
+
+}}
+
+
+/* make_unique ****************************************************************/
+
+#if !defined(DOCUMENTATION)
+#   define TETENGO2_STDALT_STD_MAKE_UNIQUE_SUPPORTED 0
+#endif
+
+namespace tetengo2 { namespace stdalt
+{
+#if TETENGO2_STDALT_STD_MAKE_UNIQUE_SUPPORTED || defined(DOCUMENTATION)
+    /*!
+        \brief Makes a unique pointer.
+
+        \tparam T A type.
+
+        \return A unique pointer
+    */
+    template <typename T>
+    std::unique_ptr<T> make_unique()
+    {
+        return std::make_unique<T>();
+    }
+
+    /*!
+        \brief Makes a unique pointer.
+
+        \tparam T  A type.
+        \tparam A1 An argument type #1.
+
+        \param arg1 An argument #1.
+
+        \return A unique pointer
+    */
+    template <typename T, typename A1>
+    std::unique_ptr<T> make_unique(A1&& arg1)
+    {
+        return std::make_unique<T>(std::forward<A1>(arg1));
+    }
+
+    /*!
+        \brief Makes a unique pointer.
+
+        \tparam T  A type.
+        \tparam A1 An argument type #1.
+        \tparam A2 An argument type #2.
+
+        \param arg1 An argument #1.
+        \param arg2 An argument #2.
+
+        \return A unique pointer
+    */
+    template <typename T, typename A1, typename A2>
+    std::unique_ptr<T> make_unique(A1&& arg1, A2&& arg2)
+    {
+        return std::make_unique<T>(std::forward<A1>(arg1), std::forward<A2>(arg2));
+    }
+#else
+    template <typename T>
+    std::unique_ptr<T> make_unique()
+    {
+        return std::unique_ptr<T>(new T());
+    }
+
+    template <typename T, typename A1>
+    std::unique_ptr<T> make_unique(A1&& arg1)
+    {
+        return std::unique_ptr<T>(new T(std::forward<A1>(arg1)));
+    }
+
+    template <typename T, typename A1, typename A2>
+    std::unique_ptr<T> make_unique(A1&& arg1, A2&& arg2)
+    {
+        return std::unique_ptr<T>(new T(std::forward<A1>(arg1), std::forward<A2>(arg2)));
+    }
+
+    template <typename T, typename A1, typename A2, typename A3>
+    std::unique_ptr<T> make_unique(A1&& arg1, A2&& arg2, A3&& arg3)
+    {
+        return std::unique_ptr<T>(new T(std::forward<A1>(arg1), std::forward<A2>(arg2), std::forward<A3>(arg3)));
+    }
+
+    template <typename T, typename A1, typename A2, typename A3, typename A4>
+    std::unique_ptr<T> make_unique(A1&& arg1, A2&& arg2, A3&& arg3, A4&& arg4)
+    {
+        return
+            std::unique_ptr<T>(
+                new T(std::forward<A1>(arg1), std::forward<A2>(arg2), std::forward<A3>(arg3), std::forward<A4>(arg4))
+            );
+    }
+
+    template <typename T, typename A1, typename A2, typename A3, typename A4, typename A5>
+    std::unique_ptr<T> make_unique(A1&& arg1, A2&& arg2, A3&& arg3, A4&& arg4, A5&& arg5)
+    {
+        return
+            std::unique_ptr<T>(
+                new T(
+                    std::forward<A1>(arg1),
+                    std::forward<A2>(arg2),
+                    std::forward<A3>(arg3),
+                    std::forward<A4>(arg4),
+                    std::forward<A5>(arg5)
+                )
+            );
+    }
+
+    template <typename T, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6>
+    std::unique_ptr<T> make_unique(A1&& arg1, A2&& arg2, A3&& arg3, A4&& arg4, A5&& arg5, A6&& arg6)
+    {
+        return
+            std::unique_ptr<T>(
+                new T(
+                    std::forward<A1>(arg1),
+                    std::forward<A2>(arg2),
+                    std::forward<A3>(arg3),
+                    std::forward<A4>(arg4),
+                    std::forward<A5>(arg5),
+                    std::forward<A6>(arg6)
+                )
+            );
+    }
+
+    template <typename T, typename A1, typename A2, typename A3, typename A4, typename A5, typename A6, typename A7>
+    std::unique_ptr<T> make_unique(A1&& arg1, A2&& arg2, A3&& arg3, A4&& arg4, A5&& arg5, A6&& arg6, A7&& arg7)
+    {
+        return
+            std::unique_ptr<T>(
+                new T(
+                    std::forward<A1>(arg1),
+                    std::forward<A2>(arg2),
+                    std::forward<A3>(arg3),
+                    std::forward<A4>(arg4),
+                    std::forward<A5>(arg5),
+                    std::forward<A6>(arg6),
+                    std::forward<A7>(arg7)
+                )
+            );
+    }
+
+    template <
+        typename T,
+        typename A1,
+        typename A2,
+        typename A3,
+        typename A4,
+        typename A5,
+        typename A6,
+        typename A7,
+        typename A8
+    >
+    std::unique_ptr<T> make_unique(
+        A1&& arg1,
+        A2&& arg2,
+        A3&& arg3,
+        A4&& arg4,
+        A5&& arg5,
+        A6&& arg6,
+        A7&& arg7,
+        A8&& arg8
+    )
+    {
+        return
+            std::unique_ptr<T>(
+                new T(
+                    std::forward<A1>(arg1),
+                    std::forward<A2>(arg2),
+                    std::forward<A3>(arg3),
+                    std::forward<A4>(arg4),
+                    std::forward<A5>(arg5),
+                    std::forward<A6>(arg6),
+                    std::forward<A7>(arg7),
+                    std::forward<A8>(arg8)
+                )
+            );
+    }
+
+    template <
+        typename T,
+        typename A1,
+        typename A2,
+        typename A3,
+        typename A4,
+        typename A5,
+        typename A6,
+        typename A7,
+        typename A8,
+        typename A9
+    >
+    std::unique_ptr<T> make_unique(
+        A1&& arg1,
+        A2&& arg2,
+        A3&& arg3,
+        A4&& arg4,
+        A5&& arg5,
+        A6&& arg6,
+        A7&& arg7,
+        A8&& arg8,
+        A9&& arg9
+    )
+    {
+        return
+            std::unique_ptr<T>(
+                new T(
+                    std::forward<A1>(arg1),
+                    std::forward<A2>(arg2),
+                    std::forward<A3>(arg3),
+                    std::forward<A4>(arg4),
+                    std::forward<A5>(arg5),
+                    std::forward<A6>(arg6),
+                    std::forward<A7>(arg7),
+                    std::forward<A8>(arg8),
+                    std::forward<A9>(arg9)
+                )
+            );
+    }
+
+    template <
+        typename T,
+        typename A1,
+        typename A2,
+        typename A3,
+        typename A4,
+        typename A5,
+        typename A6,
+        typename A7,
+        typename A8,
+        typename A9,
+        typename A10
+    >
+    std::unique_ptr<T> make_unique(
+        A1&&  arg1,
+        A2&&  arg2,
+        A3&&  arg3,
+        A4&&  arg4,
+        A5&&  arg5,
+        A6&&  arg6,
+        A7&&  arg7,
+        A8&&  arg8,
+        A9&&  arg9,
+        A10&& arg10
+    )
+    {
+        return
+            std::unique_ptr<T>(
+                new T(
+                    std::forward<A1>(arg1),
+                    std::forward<A2>(arg2),
+                    std::forward<A3>(arg3),
+                    std::forward<A4>(arg4),
+                    std::forward<A5>(arg5),
+                    std::forward<A6>(arg6),
+                    std::forward<A7>(arg7),
+                    std::forward<A8>(arg8),
+                    std::forward<A9>(arg9),
+                    std::forward<A10>(arg10)
+                )
+        );
+    }
+
+    template <
+        typename T,
+        typename A1,
+        typename A2,
+        typename A3,
+        typename A4,
+        typename A5,
+        typename A6,
+        typename A7,
+        typename A8,
+        typename A9,
+        typename A10,
+        typename A11
+    >
+    std::unique_ptr<T> make_unique(
+        A1&&  arg1,
+        A2&&  arg2,
+        A3&&  arg3,
+        A4&&  arg4,
+        A5&&  arg5,
+        A6&&  arg6,
+        A7&&  arg7,
+        A8&&  arg8,
+        A9&&  arg9,
+        A10&& arg10,
+        A11&& arg11
+    )
+    {
+        return
+            std::unique_ptr<T>(
+                new T(
+                    std::forward<A1>(arg1),
+                    std::forward<A2>(arg2),
+                    std::forward<A3>(arg3),
+                    std::forward<A4>(arg4),
+                    std::forward<A5>(arg5),
+                    std::forward<A6>(arg6),
+                    std::forward<A7>(arg7),
+                    std::forward<A8>(arg8),
+                    std::forward<A9>(arg9),
+                    std::forward<A10>(arg10),
+                    std::forward<A11>(arg11)
+                )
+        );
+    }
+
+    template <
+        typename T,
+        typename A1,
+        typename A2,
+        typename A3,
+        typename A4,
+        typename A5,
+        typename A6,
+        typename A7,
+        typename A8,
+        typename A9,
+        typename A10,
+        typename A11,
+        typename A12
+    >
+    std::unique_ptr<T> make_unique(
+        A1&&  arg1,
+        A2&&  arg2,
+        A3&&  arg3,
+        A4&&  arg4,
+        A5&&  arg5,
+        A6&&  arg6,
+        A7&&  arg7,
+        A8&&  arg8,
+        A9&&  arg9,
+        A10&& arg10,
+        A11&& arg11,
+        A12&& arg12
+    )
+    {
+        return
+            std::unique_ptr<T>(
+                new T(
+                    std::forward<A1>(arg1),
+                    std::forward<A2>(arg2),
+                    std::forward<A3>(arg3),
+                    std::forward<A4>(arg4),
+                    std::forward<A5>(arg5),
+                    std::forward<A6>(arg6),
+                    std::forward<A7>(arg7),
+                    std::forward<A8>(arg8),
+                    std::forward<A9>(arg9),
+                    std::forward<A10>(arg10),
+                    std::forward<A11>(arg11),
+                    std::forward<A12>(arg12)
+                )
+        );
     }
 #endif
 
