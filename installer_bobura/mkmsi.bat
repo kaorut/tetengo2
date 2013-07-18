13ee89b193bf9c596a9ead9a2bbcee851ef09893
kaoru <kaorut@mc.pikara.ne.jp>
2013-07-19 05:50:25 +0900

    Modify the Makefile

--- installer_bobura/mkmsi.bat
+++ installer_bobura/mkmsi.bat
@@ -3,4 +3,4 @@ rem Launches the NMAKE Script.
 rem Copyright (C) 2007-2013 kaoru
 rem $Id$
 
-nmake /F Makefile.nmake "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
+nmake /f Makefile.nmake "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
