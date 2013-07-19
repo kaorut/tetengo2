52e8ab2190541a74d506a7230bce433d8e0afa0a
kaoru <kaorut@mc.pikara.ne.jp>
2013-07-19 21:09:13 +0900

    Enable 64-bit support

--- installer_bobura/mkmsi.bat
+++ installer_bobura/mkmsi.bat
@@ -3,4 +3,12 @@ rem Launches the NMAKE Script.
 rem Copyright (C) 2007-2013 kaoru
 rem $Id$
 
+if        "%PLATFORM%"=="Win32" (
+    set ARCH=x86
+) else if "%PLATFORM%"=="x64" (
+    set ARCH=x64
+)
+
 nmake /f Makefile.nmake "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
+
+set ARCH=
