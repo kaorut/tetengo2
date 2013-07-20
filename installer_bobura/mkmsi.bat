a80651d3e59d131a565011b898f53f3cf5a4437e
kaoru <kaorut@mc.pikara.ne.jp>
2013-07-20 18:35:33 +0900

    Changed the predefined macro name for mkmsi.bat

--- installer_bobura/mkmsi.bat
+++ installer_bobura/mkmsi.bat
@@ -3,12 +3,9 @@ rem Launches the NMAKE Script.
 rem Copyright (C) 2007-2013 kaoru
 rem $Id$
 
-if        "%PLATFORM%"=="Win32" (
-    set ARCH=x86
-) else if "%PLATFORM%"=="x64" (
-    set ARCH=x64
+if not "%MKMSI_PLATFORM%"=="x64" (
+    nmake PLATFORM=Win32 ARCH=x86 /f Makefile.nmake "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
 )
-
-nmake /f Makefile.nmake "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
-
-set ARCH=
+if not "%MKMSI_PLATFORM%"=="Win32" (
+    nmake PLATFORM=x64 ARCH=x64 /f Makefile.nmake "%1" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
+)
