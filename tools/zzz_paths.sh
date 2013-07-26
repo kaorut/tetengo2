3fb737e43c36f272da945c643ada6c1b69466908
kaoru <kaorut@mc.pikara.ne.jp>
2013-07-27 06:39:46 +0900

    Update the lint scripts

--- tools/zzz_paths.sh
+++ tools/zzz_paths.sh
@@ -8,6 +8,7 @@ list_sources()
     find \
         $1/bobura/src \
         $1/bobura_model/include \
+        $1/setup/src \
         $1/tetengo2/include \
         $1/tetengo2/src \
         $1/tetengo2_detail/include \
