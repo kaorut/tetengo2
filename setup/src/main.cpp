7f794a883d1cd3c41a7adc35efc0008e9e87e9ca
kaoru <kaorut@mc.pikara.ne.jp>
2013-07-27 06:33:39 +0900

    Create source files

---
+++ setup/src/main.cpp
@@ -0,0 +1,25 @@
+#define NOMINMAX
+#define OEMRESOURCE
+#include <Windows.h>
+
+#include <tetengo2.stdalt.h>
+
+/*!
+    \brief The entry point of the application.
+
+    \param hInstance     Not used.
+    \param hPrevInstance Not used.
+    \param lpCmdLine     Not used.
+    \param nCmdShow      Not used.
+
+    \return The exit status code.
+*/
+int WINAPI wWinMain(
+    const ::HINSTANCE hInstance,
+    const ::HINSTANCE hPrevInstance,
+    const ::LPWSTR    lpCmdLine,
+    const int         nCmdShow
+)
+{
+    return 0;
+}
