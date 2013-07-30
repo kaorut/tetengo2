1442c24378eee035db8f2d4a929747712e82cec5
kaoru <kaorut@mc.pikara.ne.jp>
2013-07-31 06:17:28 +0900

    Enable a code sign

---
+++ tools/codesign.bat
@@ -0,0 +1,8 @@
+@echo off
+rem Code-signs a file.
+rem Copyright (C) 2007-2013 kaoru
+rem $Id$
+
+if not exist "%1tools\zzz_codesign.bat" exit /b
+
+"%1tools\zzz_codesign.bat" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
