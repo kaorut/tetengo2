cfb7d19e6db5b540badbd72751214e7d2bead0c9
kaoru <kaorut@mc.pikara.ne.jp>
2013-08-27 06:27:11 +0900

    Implement Windows specific procedure

--- tetengo2_detail/include/tetengo2.detail.windows.message_handler_detail.abstra.h
+++ tetengo2_detail/include/tetengo2.detail.windows.message_handler_detail.abstra.h
@@ -11,9 +11,12 @@
 #define TETENGO2_DETAIL_WINDOWS_MESSAGEHANDLERDETAIL_ABSTRACTWINDOW_H
 
 #include <algorithm>
+#include <string>
+#include <system_error>
 #include <vector>
 
 #include <boost/optional.hpp>
+#include <boost/throw_exception.hpp>
 
 #pragma warning (push)
 #pragma warning (disable: 4005)
@@ -92,6 +95,36 @@ namespace tetengo2 { namespace detail { namespace windows { namespace message_ha
             return boost::make_optional< ::LRESULT>(0);
         }
 
+        template <typename Path>
+        std::vector<Path> make_paths(const ::HDROP drop_handle)
+        {
+            const ::UINT count = ::DragQueryFileW(drop_handle, 0xFFFFFFFF, NULL, 0);
+
+            std::vector<Path> paths;
+            paths.reserve(count);
+
+            for (::UINT i = 0; i < count; ++i)
+            {
+                const ::UINT length = ::DragQueryFileW(drop_handle, i, NULL, 0);
+                std::vector<wchar_t> path_string(length + 1, 0);
+
+                const ::UINT result =
+                    ::DragQueryFileW(drop_handle, i, path_string.data(), static_cast< ::UINT>(path_string.size()));
+                if (result == 0)
+                {
+                    BOOST_THROW_EXCEPTION(
+                        std::system_error(
+                            std::error_code(::GetLastError(), win32_category()), "Can't obtain the dropped file path."
+                        )
+                    );
+                }
+
+                paths.emplace_back(std::wstring(path_string.begin(), path_string.begin() + length));
+            }
+
+            return paths;
+        }
+
         template <typename AbstractWindow>
         boost::optional< ::LRESULT> on_drop_files(
             AbstractWindow& abstract_window,
@@ -105,7 +138,7 @@ namespace tetengo2 { namespace detail { namespace windows { namespace message_ha
                 return boost::none;
 
             typedef typename AbstractWindow::file_drop_observer_set_type::path_type path_type;
-            const std::vector<path_type> paths;
+            const std::vector<path_type> paths = make_paths<path_type>(reinterpret_cast< ::HDROP>(w_param));
             abstract_window.file_drop_observer_set().file_dropped()(paths);
             return boost::make_optional< ::LRESULT>(0);
         }
