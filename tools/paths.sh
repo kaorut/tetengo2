b22df93f321d68265e213656ea90805071bc413b
kaoru <kaorut@mc.pikara.ne.jp>
2014-03-07 07:29:40 +0900

    Modify paths.sh

--- tools/paths.sh
+++ tools/paths.sh
@@ -6,9 +6,6 @@
 list_sources()
 {
     find \
-        $1/bobura/src \
-        $1/bobura_model/include \
-        $1/setup/src \
         $1/tetengo2/include \
         $1/tetengo2/src \
         $1/tetengo2_detail/include \
@@ -22,8 +19,6 @@ list_sources()
 list_test_sources()
 {
     find \
-        $1/bobura/test \
-        $1/bobura_model/test \
         $1/tetengo2/test \
         $1/tetengo2_gui/test \
         -maxdepth 1 \
