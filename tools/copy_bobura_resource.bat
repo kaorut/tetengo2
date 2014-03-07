e111938b17d3a29dd7c0012f8978d2b992353420
kaoru <kaorut@mc.pikara.ne.jp>
2014-03-08 01:58:47 +0900

    Remove copy_bobura_resouce.bat

--- tools/copy_bobura_resource.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Copies Bobura resource files
-rem Copyright (C) 2007-2014 kaoru
-rem $Id$
-
-xcopy /y /i "%1res\messages" "%2messages"
-rem xcopy /y /i "%1res\images" "%2images"
-mkdir "%2images"
-xcopy /y /i "%1res\win32\bobura_app.ico" "%2images"
