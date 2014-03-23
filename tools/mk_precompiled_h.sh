24674be313f6d585e9b858aa55900b204c61a9e3
kaoru <kaorut@mc.pikara.ne.jp>
2014-03-23 14:28:31 +0900

    Create mk_precompiled_h.sh and zzz_mk_precompiled_h.pl

---
+++ tools/mk_precompiled_h.sh
@@ -0,0 +1,18 @@
+#/bin/sh
+# Makes precompiled.h.
+# Copyright (C) 2007-2014 kaoru
+# $Id$
+
+TOOLS_DIR=`dirname $0`
+SOLUTION_DIR="$1"
+test -n "$SOLUTION_DIR" || SOLUTION_DIR="$TOOLS_DIR/.."
+
+PRECOMPILED_H="$SOLUTION_DIR/precompiled/precompiled.h"
+PRECOMPILED_H_TMP=$PRECOMPILED_H".tmp"
+
+"$TOOLS_DIR/zzz_mk_precompiled_h.pl" \
+  "$SOLUTION_DIR/tools/stdlib_headers.txt" \
+  "$SOLUTION_DIR/tools/precompiled.template.h" \
+  > $PRECOMPILED_H_TMP
+
+mv $PRECOMPILED_H_TMP $PRECOMPILED_H
