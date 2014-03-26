25780b0e8b66ed70ae88b3aeb5d3b5778f1583bf
kaoru <kaorut@mc.pikara.ne.jp>
2014-03-27 07:28:30 +0900

    Modify codesign.bat

--- tools/codesign.bat
+++ tools/codesign.bat
@@ -3,6 +3,6 @@ rem Code-signs a file.
 rem Copyright (C) 2007-2014 kaoru
 rem $Id$
 
-if not exist "%1tools\zzz_codesign.bat" exit /b
+if not exist "%~dp0zzz_codesign.bat" exit /b
 
-"%1tools\zzz_codesign.bat" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
+"%~dp0zzz_codesign.bat" "%2" "%3" "%4" "%5" "%6" "%7" "%8" "%9"
