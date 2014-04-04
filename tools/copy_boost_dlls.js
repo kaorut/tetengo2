1647949cced7434034929104b9abc932f0108be9
kaoru <kaorut@mc.pikara.ne.jp>
2014-04-05 07:29:14 +0900

    Modify copy_boost_dlls.js to read boost library file list from an outer file

--- tools/copy_boost_dlls.js
+++ tools/copy_boost_dlls.js
@@ -1,40 +1,44 @@
 /*
-	Boost DLL Copier
-	Copyright (C) 2007-2014 kaoru
-	$Id$
+    Boost DLL Copier
+    Copyright (C) 2007-2014 kaoru
+    $Id$
 */
 
 function getDestinationDirectory(solutionDirectory, releaseOrDebug, platform)
 {
-	var filesystem = WScript.CreateObject("Scripting.FileSystemObject");
-	
-	return filesystem.BuildPath(solutionDirectory, "bin\\" + releaseOrDebug + "." + platform);
+    var filesystem = WScript.CreateObject("Scripting.FileSystemObject");
+    
+    return filesystem.BuildPath(solutionDirectory, "bin\\" + releaseOrDebug + "." + platform);
 }
 
-function getDllNames(version, debug)
+function getLibNames(stream)
 {
-	var dllNames = new Array;
-	
-	dllNames.push("boost_bzip2-*-mt-" + (debug ? "gd-" : "") + version + ".dll");
-	dllNames.push("boost_chrono-*-mt-" + (debug ? "gd-" : "") + version + ".dll");
-	dllNames.push("boost_date_time-*-mt-" + (debug ? "gd-" : "") + version + ".dll");
-	dllNames.push("boost_filesystem-*-mt-" + (debug ? "gd-" : "") + version + ".dll");
-	dllNames.push("boost_iostreams-*-mt-" + (debug ? "gd-" : "") + version + ".dll");
-	dllNames.push("boost_program_options-*-mt-" + (debug ? "gd-" : "") + version + ".dll");
-	dllNames.push("boost_system-*-mt-" + (debug ? "gd-" : "") + version + ".dll");
-	dllNames.push("boost_thread-*-mt-" + (debug ? "gd-" : "") + version + ".dll");
-	dllNames.push("boost_unit_test_framework-*-mt-" + (debug ? "gd-" : "") + version + ".dll");
-	
-	return dllNames;
+    var libNames = new Array;
+    while (!stream.AtEndOfStream)
+        libNames.push(stream.ReadLine());
+    return libNames;
+}
+
+function getDllNames(boostLibsStream, version, debug)
+{
+    var libNames = getLibNames(boostLibsStream);
+    
+    var dllNames = new Array;
+    for (i = 0; i < libNames.length; ++i)
+    {
+        dllNames.push(libNames[i] + "-*-mt-" + (debug ? "gd-" : "") + version + ".dll");
+    }
+    
+    return dllNames;
 }
 
 function copyFile(sourceDirectory, destinationDirectory, fileName)
 {
-	var filesystem = WScript.CreateObject("Scripting.FileSystemObject");
-	
-	var sourceFile = filesystem.BuildPath(sourceDirectory, fileName);
-	WScript.Echo("Copying \"" + sourceFile + "\" to \"" + destinationDirectory + "\" ...");
-	filesystem.CopyFile(sourceFile, destinationDirectory);
+    var filesystem = WScript.CreateObject("Scripting.FileSystemObject");
+    
+    var sourceFile = filesystem.BuildPath(sourceDirectory, fileName);
+    WScript.Echo("Copying \"" + sourceFile + "\" to \"" + destinationDirectory + "\" ...");
+    //filesystem.CopyFile(sourceFile, destinationDirectory);
 }
 
 
@@ -42,34 +46,43 @@ function copyFile(sourceDirectory, destinationDirectory, fileName)
 
 function include(script)
 {
-	var path = WScript.ScriptFullName.replace(new RegExp(WScript.ScriptName + "$"), "");
-	var stream = WScript.CreateObject("Scripting.FileSystemObject").OpenTextFile(path + script, 1, false, 0);
-	try { return stream.ReadAll(); } finally { stream.Close(); }
+    var path = WScript.ScriptFullName.replace(new RegExp(WScript.ScriptName + "$"), "");
+    var stream = WScript.CreateObject("Scripting.FileSystemObject").OpenTextFile(path + script, 1, false, 0);
+    try { return stream.ReadAll(); } finally { stream.Close(); }
 }
 eval(include("zzz_parse_solution_config.js"));
 
-if (WScript.Arguments.length < 2)
+if (WScript.Arguments.length <= 4)
 {
-	WScript.Echo("Usage: cscript copy_boost_dlls.js SOLUTION_DIRECTORY RELEASE_OR_DEBUG PLATFORM");
-	WScript.Quit(1);
+    WScript.Echo("Usage: cscript copy_boost_dlls.js boost_libs.txt config.h SOLUTION_DIRECTORY  RELEASE_OR_DEBUG PLATFORM");
+    WScript.Quit(1);
 }
-var solutionDirectory = WScript.Arguments(0);
+var boostLibsFileName = WScript.Arguments(0);
 var configFileName = WScript.Arguments(1);
-var releaseOrDebug = WScript.Arguments(2);
-var platform = WScript.Arguments(3);
+var solutionDirectory = WScript.Arguments(2);
+var releaseOrDebug = WScript.Arguments(3);
+var platform = WScript.Arguments(4);
 
-var stream = WScript.CreateObject("Scripting.FileSystemObject").OpenTextFile(configFileName, 1, false, 0);
+var boostLibsStream = WScript.CreateObject("Scripting.FileSystemObject").OpenTextFile(boostLibsFileName, 1, false, 0);
 try
 {
-	var version = getMacroValue(stream, "BOOST_VERSION");
-	var sourceDirectory = getMacroValue(stream, "BOOST_LIB_" + platform);
-	var destinationDirectory = getDestinationDirectory(solutionDirectory, releaseOrDebug, platform);
-	var dllNames = getDllNames(version, releaseOrDebug.toLowerCase() == "debug");
+    var configStream = WScript.CreateObject("Scripting.FileSystemObject").OpenTextFile(configFileName, 1, false, 0);
+    try
+    {
+        var version = getMacroValue(configStream, "BOOST_VERSION");
+        var sourceDirectory = getMacroValue(configStream, "BOOST_LIB_" + platform);
+        var destinationDirectory = getDestinationDirectory(solutionDirectory, releaseOrDebug, platform);
+        var dllNames = getDllNames(boostLibsStream, version, releaseOrDebug.toLowerCase() == "debug");
 
-	for (var i = 0; i < dllNames.length; ++i)
-		copyFile(sourceDirectory, destinationDirectory, dllNames[i]);
+        for (var i = 0; i < dllNames.length; ++i)
+            copyFile(sourceDirectory, destinationDirectory, dllNames[i]);
+    }
+    finally
+    {
+        configStream.Close();
+    }
 }
 finally
 {
-	stream.Close();
+    boostLibsStream.close();
 }
