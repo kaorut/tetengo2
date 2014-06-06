58ba52f6e97dba12c1a51be15a948b3af750dc06
kaoru <kaorut@mc.pikara.ne.jp>
2014-06-07 03:12:37 +0900

    Create test_tetengo2::detail_type_list #11

---
+++ tetengo2/test/test_tetengo2.detail_type_list.h
@@ -0,0 +1,20 @@
+/*! \file
+    \brief The definition of test_tetengo2::detail_type_list.
+
+    Copyright (C) 2007-2014 kaoru
+
+    $Id$
+*/
+
+#if !defined(TESTTETENGO2_DETAILTYPELIST_H)
+#define TESTTETENGO2_DETAILTYPELIST_H
+
+
+namespace test_tetengo2
+{
+
+
+}
+
+
+#endif
