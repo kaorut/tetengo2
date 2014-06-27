61d7821744b7dc49ab4452fc26407aa78aeab8e1
kaoru <kaorut@mc.pikara.ne.jp>
2014-06-28 00:56:35 +0900

    Remove unused type aliases in tetengo2::gui::widget::image #18

--- tetengo2_gui/include/tetengo2/gui/widget/image.h
+++ tetengo2_gui/include/tetengo2/gui/widget/image.h
@@ -46,29 +46,14 @@ namespace tetengo2 { namespace gui { namespace widget
         //! The detail implementation type of a widget.
         using widget_details_type = typename details_traits_type::widget_details_type;
 
-        //! The detail implementation type.
-        using details_type = typename widget_details_type::widget_details_type;
-
         //! The drawing details type.
         using drawing_details_type = typename details_traits_type::drawing_details_type;
 
         //! The icon details type.
         using icon_details_type = typename details_traits_type::icon_details_type;
 
-        //! The alert details type.
-        using alert_details_type = typename details_traits_type::alert_details_type;
-
-        //! The cursor details type.
-        using cursor_details_type = typename details_traits_type::cursor_details_type;
-
-        //! The scroll details type.
-        using scroll_details_type = typename details_traits_type::scroll_details_type;
-
         //! The message handler details type.
         using message_handler_details_type = typename details_traits_type::message_handler_details_type;
-
-        //! The virtual key details type.
-        using virtual_key_details_type = typename details_traits_type::virtual_key_details_type;
 
         //! The base type.
         using base_type = control<traits_type, details_traits_type>;
