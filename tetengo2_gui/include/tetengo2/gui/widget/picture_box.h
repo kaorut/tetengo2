2c8e859d7cc3ae2c8beb2bea97a4018483791ec4
kaoru <kaorut@mc.pikara.ne.jp>
2014-06-28 00:57:37 +0900

    Remove unused type aliases in tetengo2::gui::widget::picture_box #18

--- tetengo2_gui/include/tetengo2/gui/widget/picture_box.h
+++ tetengo2_gui/include/tetengo2/gui/widget/picture_box.h
@@ -44,29 +44,11 @@ namespace tetengo2 { namespace gui { namespace widget
         //! The detail implementation type of a widget.
         using widget_details_type = typename details_traits_type::widget_details_type;
 
-        //! The detail implementation type.
-        using details_type = typename widget_details_type::widget_details_type;
-
-        //! The drawing details type.
-        using drawing_details_type = typename details_traits_type::drawing_details_type;
-
         //! The icon details type.
         using icon_details_type = typename details_traits_type::icon_details_type;
 
-        //! The alert details type.
-        using alert_details_type = typename details_traits_type::alert_details_type;
-
-        //! The cursor details type.
-        using cursor_details_type = typename details_traits_type::cursor_details_type;
-
-        //! The scroll details type.
-        using scroll_details_type = typename details_traits_type::scroll_details_type;
-
         //! The message handler details type.
         using message_handler_details_type = typename details_traits_type::message_handler_details_type;
-
-        //! The virtual key details type.
-        using virtual_key_details_type = typename details_traits_type::virtual_key_details_type;
 
         //! The fast drawing details type.
         using fast_drawing_details_type = FastDrawingDetails;
@@ -77,37 +59,19 @@ namespace tetengo2 { namespace gui { namespace widget
         //! The widget type.
         using widget_type = typename base_type::base_type;
 
-        //! The position type.
-        using position_type = typename base_type::position_type;
-
-        //! The dimension type.
-        using dimension_type = typename base_type::dimension_type;
-
-        //! The system cursor type.
-        using system_cursor_type = typename base_type::system_cursor_type;
-
-        //! The cursor type.
-        using cursor_type = typename base_type::cursor_type;
-
-        //! The scroll bar type.
-        using scroll_bar_type = typename base_type::scroll_bar_type;
-
         //! The scroll bar style type.
         using scroll_bar_style_type = typename base_type::scroll_bar_style_type;
-
-        //! The mouse observer set type.
-        using mouse_observer_set_type = typename base_type::mouse_observer_set_type;
 
         //! The fast canvas traits type.
         using canvas_traits_type = typename base_type::canvas_traits_type;
 
-        //! The fast widget canvas type.
-        using fast_widget_canvas_type =
-            gui::drawing::widget_canvas<canvas_traits_type, fast_drawing_details_type, icon_details_type>;
-
         //! The fast canvas type.
         using fast_canvas_type =
             gui::drawing::canvas<canvas_traits_type, fast_drawing_details_type, icon_details_type>;
+
+        //! The fast widget canvas type.
+        using fast_widget_canvas_type =
+            gui::drawing::widget_canvas<canvas_traits_type, fast_drawing_details_type, icon_details_type>;
 
         //! The fast paint observer set type.
         using fast_paint_observer_set_type = gui::message::paint_observer_set<fast_canvas_type>;
