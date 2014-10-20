a0f0c3a774b6ce5559d7a08178ba690d37b0f946
kaoru <kaorut@mc.pikara.ne.jp>
2014-10-21 06:16:55 +0900

    Create check_type_list_type_duplication #25

---
+++ tools/check_type_list_type_duplication.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+# Checks type list type duplications.
+# Copyright (C) 2007-2014 kaoru
+# $Id$
+
+TOOLS_DIR=`dirname $0`
+SOLUTION_DIR="$1"
+test -n "$SOLUTION_DIR" || SOLUTION_DIR="$TOOLS_DIR/.."
+
+EXIT_STATUS=0
+
+. "$SOLUTION_DIR/tools/paths.sh"
+for f in `list_sources $SOLUTION_DIR; list_test_sources $SOLUTION_DIR`;
+do
+    "$TOOLS_DIR/zzz_check_type_list_type_duplication.pl" "$f";
+    test $? -eq 0 || EXIT_STATUS=1;
+done
+
+exit $EXIT_STATUS
---
+++ tools/zzz_check_type_list_type_duplication.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+# Checks the #include directives.
+# Copyright (C) 2007-2014 kaoru
+# $Id$
+
+
+use strict;
+
+
+my($source_filename) = $ARGV[0];
+
+my(%occurrence);
+
+open(my $fh, $source_filename) || die "Can't open $source_filename";
+while(<$fh>)
+{
+	if (/[ :](type::[0-9A-Za-z_:]+)/)
+	{
+		++$occurrence{$1};
+	}
+}
+close($fh);
+
+my($source_filename_shown) = 0;
+while (my($name, $count) = each(%occurrence))
+{
+	if ($count >= 2)
+	{
+		if (!$source_filename_shown)
+		{
+			print "WARNING: $source_filename: Same type list type is used twice or more.\n";
+			$source_filename_shown = 1;
+		}
+		
+		print "  ".$name."\n";
+	}
+}
