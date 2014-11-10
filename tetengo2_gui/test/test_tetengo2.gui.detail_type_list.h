ef521d85532916ba321b3b370926c26586a39af0
kaoru <kaorut@mc.pikara.ne.jp>
2014-11-11 01:24:05 +0900

    Change the type lists to plain aliases #28

--- tetengo2_gui/test/test_tetengo2.gui.detail_type_list.h
+++ tetengo2_gui/test/test_tetengo2.gui.detail_type_list.h
@@ -37,6 +37,121 @@
 #endif
 
 
+namespace test_tetengo2 { namespace gui { namespace type_list_temp
+{
+#if !defined(DOCUMENTATION)
+    namespace detail { namespace detail
+    {
+        namespace test
+        {
+            using alert_details_type = tetengo2::detail::stub::alert;
+
+            using common_dialog_details_type = tetengo2::detail::stub::common_dialog;
+
+            using cursor_details_type = tetengo2::detail::stub::cursor;
+
+            using drawing_details_type = tetengo2::detail::stub::drawing;
+
+#if BOOST_COMP_MSVC
+            using encoding_details_type = tetengo2::detail::windows::encoding;
+#else
+            using encoding_details_type = tetengo2::detail::unixos::encoding;
+#endif
+
+            using gui_fixture_type = tetengo2::detail::stub::gui_fixture;
+
+            using icon_details_type = tetengo2::detail::stub::icon;
+
+            using menu_details_type = tetengo2::detail::stub::menu;
+
+            using message_handler_details_type = tetengo2::detail::stub::message_handler;
+
+            using message_loop_details_type = tetengo2::detail::stub::message_loop;
+
+            using mouse_capture_details_type = tetengo2::detail::stub::mouse_capture;
+
+            using scroll_details_type = tetengo2::detail::stub::scroll;
+
+            using shell_details_type = tetengo2::detail::stub::shell;
+
+            using system_color_details_type = tetengo2::detail::stub::system_color;
+
+            using timer_details_type = tetengo2::detail::stub::timer;
+
+            using unit_details_type = tetengo2::detail::stub::unit;
+
+            using virtual_key_details_type = tetengo2::detail::stub::virtual_key;
+
+            using widget_details_type = tetengo2::detail::stub::widget;
+
+        }
+
+    }}
+#endif
+
+    /*!
+        \brief The detail type list for the testing.
+    */
+    struct detail_for_test
+    {
+        //!< The alert type.
+        using alert_type = detail::detail::test::alert_details_type;
+
+        //!< The common dialog type.
+        using common_dialog_type = detail::detail::test::common_dialog_details_type;
+
+        //!< The cursor type.
+        using cursor_type = detail::detail::test::cursor_details_type;
+
+        //!< The drawing type.
+        using drawing_type = detail::detail::test::drawing_details_type;
+
+        //!< The encoding type.
+        using encoding_type = detail::detail::test::encoding_details_type;
+
+        //!< The GUI fixture type.
+        using gui_fixture_type = detail::detail::test::gui_fixture_type;
+
+        //!< The icon type.
+        using icon_type = detail::detail::test::icon_details_type;
+
+        //!< The menu type.
+        using menu_type = detail::detail::test::menu_details_type;
+
+        //!< The message handler type.
+        using message_handler_type = detail::detail::test::message_handler_details_type;
+
+        //!< The message loop type.
+        using message_loop_type = detail::detail::test::message_loop_details_type;
+
+        //!< The mouse_capture_type.
+        using mouse_capture_type = detail::detail::test::mouse_capture_details_type;
+
+        //!< The scroll type.
+        using scroll_type = detail::detail::test::scroll_details_type;
+
+        //!< The shell type.
+        using shell_type = detail::detail::test::shell_details_type;
+
+        //!< The system color type.
+        using system_color_type = detail::detail::test::system_color_details_type;
+
+        //!< The timer type.
+        using timer_type = detail::detail::test::timer_details_type;
+
+        //!< The unit type.
+        using unit_type = detail::detail::test::unit_details_type;
+
+        //!< The virtual key type.
+        using virtual_key_type = detail::detail::test::virtual_key_details_type;
+
+        //!< The widget type.
+        using widget_type = detail::detail::test::widget_details_type;
+
+    };
+
+}}}
+
 namespace test_tetengo2 { namespace gui
 {
     namespace type { namespace detail
--- tetengo2_gui/test/test_tetengo2.gui.type_list.h
+++ tetengo2_gui/test/test_tetengo2.gui.type_list.h
@@ -22,6 +22,118 @@
 #include "test_tetengo2.gui.detail_type_list.h"
 
 
+namespace test_tetengo2 { namespace gui { namespace type_list_temp
+{
+#if !defined(DOCUMENTATION)
+    namespace detail { namespace common
+    {
+        using size_type = std::size_t;
+
+        using difference_type = std::ptrdiff_t;
+
+        using string_type = std::string;
+
+        using position_type =
+            std::pair<tetengo2::gui::unit::pixel<difference_type>, tetengo2::gui::unit::pixel<difference_type>>;
+
+        using dimension_type = std::pair<tetengo2::gui::unit::pixel<size_type>, tetengo2::gui::unit::pixel<size_type>>;
+
+        template <typename DetailTypeList>
+        using widget_details_type = typename DetailTypeList::widget_type;
+
+        using exception_string_type = std::string;
+
+        template <typename DetailTypeList>
+        using encoding_details_type = typename DetailTypeList::encoding_type;
+
+        template <typename DetailTypeList>
+        using internal_encoding_type =
+            tetengo2::text::encoding::locale<string_type, encoding_details_type<DetailTypeList>>;
+
+        template <typename DetailTypeList>
+        using ui_encoding_type =
+            tetengo2::text::encoding::locale<string_type, encoding_details_type<DetailTypeList>>;
+
+        template <typename DetailTypeList>
+        using ui_encoder_type =
+            tetengo2::text::encoder<internal_encoding_type<DetailTypeList>, ui_encoding_type<DetailTypeList>>;
+
+        template <typename DetailTypeList>
+        using exception_encoding_type =
+            tetengo2::text::encoding::locale<exception_string_type, encoding_details_type<DetailTypeList>>;
+
+        template <typename DetailTypeList>
+        using exception_encoder_type =
+            tetengo2::text::encoder<internal_encoding_type<DetailTypeList>, exception_encoding_type<DetailTypeList>>;
+
+        template <typename DetailTypeList>
+        using widget_traits_type =
+            tetengo2::gui::widget::widget_traits<
+                size_type,
+                size_type,
+                difference_type,
+                string_type,
+                position_type,
+                dimension_type,
+                ui_encoder_type<DetailTypeList>,
+                exception_encoder_type<DetailTypeList>
+            >;
+
+        template <typename DetailTypeList>
+        using widget_details_traits_type =
+            tetengo2::gui::widget::widget_details_traits<
+                widget_details_type<DetailTypeList>,
+                typename DetailTypeList::drawing_type,
+                typename DetailTypeList::icon_type,
+                typename DetailTypeList::alert_type,
+                typename DetailTypeList::cursor_type,
+                typename DetailTypeList::scroll_type,
+                typename DetailTypeList::message_handler_type,
+                typename DetailTypeList::virtual_key_type
+            >;
+
+    }}
+#endif
+
+    /*!
+        \brief The common type list.
+
+        \tparam DetailTypeList A detail type list.
+    */
+    template <typename DetailTypeList>
+    struct common
+    {
+        //! The size type.
+        using size_type = detail::common::size_type;
+
+        //! The difference type.
+        using difference_type = detail::common::difference_type;
+
+        //! The string_type.
+        using string_type = detail::common::string_type;
+
+        //! The position type.
+        using position_type = detail::common::position_type;
+
+        //! The dimension type.
+        using dimension_type = detail::common::dimension_type;
+
+        //! The UI encoder_type.
+        using ui_encoder_type = detail::common::ui_encoder_type<DetailTypeList>;
+
+        //! The exception encoder type.
+        using exception_encoder_type = detail::common::exception_encoder_type<DetailTypeList>;
+
+        //! The widget traits type.
+        using widget_traits_type = detail::common::widget_traits_type<DetailTypeList>;
+
+        //! The widget details traits type.
+        using widget_details_traits_type = detail::common::widget_details_traits_type<DetailTypeList>;
+
+    };
+
+}}}
+
 namespace test_tetengo2 { namespace gui
 {
     namespace type
@@ -129,6 +241,5 @@ namespace test_tetengo2 { namespace gui
 
 
 }}
-
 
 #endif
