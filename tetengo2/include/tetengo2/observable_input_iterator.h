70043b4a699d665bea02f374c3a32dea85506f0a
kaoru <kaorut@mc.pikara.ne.jp>
2015-03-01 00:06:07 +0900

    Create tetengo2::observable_input_iterator #46

---
+++ tetengo2/include/tetengo2/observable_input_iterator.h
@@ -0,0 +1,32 @@
+/*! \file
+    \brief The definition of tetengo2::observable_input_iterator.
+
+    Copyright (C) 2007-2015 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_OBSERVABLEINPUTITERATOR_H)
+#define TETENGO2_OBSERVABLEINPUTITERATOR_H
+
+
+namespace tetengo2
+{
+    /*!
+        \brief The class template for an observable input iterator.
+
+        \tparam InputIterator An input iterator type.
+    */
+    template <typename InputIterator>
+    class observable_input_iterator
+    {
+    public:
+
+
+    };
+
+
+}
+
+
+#endif
---
+++ tetengo2/test/test_tetengo2.observable_input_iterator.cpp
@@ -0,0 +1,27 @@
+/*! \file
+    \brief Test of class tetengo2::observable_input_iterator.
+
+    Copyright (C) 2007-2015 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include <tetengo2.h>
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo2)
+BOOST_AUTO_TEST_SUITE(observable_input_iterator)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
