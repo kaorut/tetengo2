5c51a6d21dd579135e795008e3861c2ce54dae42
kaoru <kaorut@mc.pikara.ne.jp>
2015-03-01 00:06:21 +0900

    Implement tetengo2::observable_input_iterator #46

--- tetengo2/include/tetengo2/observable_input_iterator.h
+++ tetengo2/include/tetengo2/observable_input_iterator.h
@@ -9,6 +9,10 @@
 #if !defined(TETENGO2_OBSERVABLEINPUTITERATOR_H)
 #define TETENGO2_OBSERVABLEINPUTITERATOR_H
 
+#include <iterator>
+
+#include <boost/iterator/iterator_facade.hpp>
+
 
 namespace tetengo2
 {
@@ -18,9 +22,66 @@ namespace tetengo2
         \tparam InputIterator An input iterator type.
     */
     template <typename InputIterator>
-    class observable_input_iterator
+    class observable_input_iterator :
+        public boost::iterator_facade<
+            observable_input_iterator<InputIterator>, typename InputIterator::value_type, std::input_iterator_tag
+        >
     {
     public:
+        // types
+
+        //! The iterator type.
+        using iterator = InputIterator;
+
+
+        // constructors and destructor
+
+        explicit observable_input_iterator(iterator input_iterator)
+        :
+        m_input_iterator(input_iterator)
+        {}
+
+
+        // functions
+
+        /*!
+            \brief Dereferences the iterator.
+
+            \return The dereferenced value.
+        */
+        typename observable_input_iterator::reference dereference()
+        const
+        {
+            return *m_input_iterator;
+        }
+
+        /*!
+            \brief Checks whether the iterator is equal to another iterator.
+
+            \param another Another iterator.
+
+            \retval true  When the iterator is equal to another iterator.
+            \retval false Otherwise.
+        */
+        bool equal(const observable_input_iterator& another)
+        const
+        {
+            return m_input_iterator == another.m_input_iterator;
+        }
+
+        /*!
+            \brief Increments the iterator.
+        */
+        void increment()
+        {
+            ++m_input_iterator;
+        }
+
+
+    private:
+        // variables
+
+        iterator m_input_iterator;
 
 
     };
--- tetengo2/test/test_tetengo2.observable_input_iterator.cpp
+++ tetengo2/test/test_tetengo2.observable_input_iterator.cpp
@@ -6,6 +6,8 @@
     $Id$
 */
 
+#include <vector>
+
 #include <boost/test/unit_test.hpp>
 
 #include <tetengo2.h>
@@ -19,7 +21,52 @@ BOOST_AUTO_TEST_SUITE(observable_input_iterator)
     {
         BOOST_TEST_PASSPOINT();
 
-        BOOST_WARN_MESSAGE(false, "Not implemented yet.");
+        std::vector<int> v{ 42, 43, 44, 45, };
+
+        const tetengo2::observable_input_iterator<std::vector<int>::iterator> iterator{ v.begin() };
+    }
+
+    BOOST_AUTO_TEST_CASE(dereference)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        std::vector<int> v{ 42, 43, 44, 45, };
+
+        const tetengo2::observable_input_iterator<std::vector<int>::iterator> iterator{ v.begin() };
+
+        BOOST_CHECK_EQUAL(*iterator, 42);
+    }
+
+    BOOST_AUTO_TEST_CASE(equal)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        std::vector<int> v{ 42, 43, 44, 45, };
+
+        const tetengo2::observable_input_iterator<std::vector<int>::iterator> iterator1{ v.begin() };
+        const tetengo2::observable_input_iterator<std::vector<int>::iterator> iterator2{ v.begin() };
+
+        BOOST_CHECK(iterator1 == iterator2);
+    }
+
+    BOOST_AUTO_TEST_CASE(increment)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        std::vector<int> v{ 42, 43, 44, 45, };
+
+        tetengo2::observable_input_iterator<std::vector<int>::iterator> iterator{ v.begin() };
+
+        ++iterator;
+
+        BOOST_CHECK_EQUAL(*iterator, 43);
+
+        ++iterator;
+        ++iterator;
+        ++iterator;
+
+        const tetengo2::observable_input_iterator<std::vector<int>::iterator> last{ v.end() };
+        BOOST_CHECK(iterator == last);
     }
 
 
