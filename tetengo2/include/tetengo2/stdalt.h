1ea13529ae5c30b52e704f6685194c8d62ec0129
kaoru <kaorut@mc.pikara.ne.jp>
2015-05-15 07:35:13 +0900

    Remove the standard C++ alternatives #50

--- tetengo2/include/tetengo2/stdalt.h
+++ tetengo2/include/tetengo2/stdalt.h
@@ -9,7 +9,6 @@
 #if !defined(TETENGO2_STDALT_H)
 #define TETENGO2_STDALT_H
 
-#include <boost/core/ignore_unused.hpp>
 #include <boost/predef.h>
 
 
@@ -28,96 +27,6 @@
 #   define TETENGO2_STDALT_NOEXCEPT noexcept
 #else
 #   define TETENGO2_STDALT_NOEXCEPT
-#endif
-
-
-/* destructor default implementation ******************************************/
-
-#if !defined(DOCUMENTATION)
-#   if BOOST_COMP_MSVC >= BOOST_VERSION_NUMBER(12, 0, 0) || BOOST_COMP_GNUC >= BOOST_VERSION_NUMBER(4, 8, 0)
-#       define TETENGO2_STDALT_DESTRUCTOR_DEFAULT_IMPLEMENTATION_SUPPORTED 1
-#   else
-#       define TETENGO2_STDALT_DESTRUCTOR_DEFAULT_IMPLEMENTATION_SUPPORTED 0
-#   endif
-#endif
-
-#if TETENGO2_STDALT_DESTRUCTOR_DEFAULT_IMPLEMENTATION_SUPPORTED || defined(DOCUMENTATION)
-//! The destructor default implementation.
-#   define TETENGO2_STDALT_DESTRUCTOR_DEFAULT_IMPLEMENTATION TETENGO2_STDALT_NOEXCEPT = default
-#else
-#   define TETENGO2_STDALT_DESTRUCTOR_DEFAULT_IMPLEMENTATION TETENGO2_STDALT_NOEXCEPT {}
-#endif
-
-
-/* insertion iterator *********************************************************/
-
-#if !defined(DOCUMENTATION)
-#   if BOOST_COMP_MSVC >= BOOST_VERSION_NUMBER(11, 0, 0)
-#       define TETENGO2_STDALT_INSERTION_ITERATOR_SUPPORTED 1
-#   else
-#       define TETENGO2_STDALT_INSERTION_ITERATOR_SUPPORTED 0
-#   endif
-#endif
-
-#if !TETENGO2_STDALT_INSERTION_ITERATOR_SUPPORTED
-#   include <iterator>
-#endif
-
-namespace tetengo2 { namespace stdalt
-{
-#if TETENGO2_STDALT_INSERTION_ITERATOR_SUPPORTED || defined(DOCUMENTATION)
-    /*!
-        \brief Returns an iterator for insert(), erase(), etc. of the containers.
-
-        \param container A container.
-        \param iterator  An iterator.
-
-        \return An iterator for insert(), erase(), etc. of the container.
-    */
-    template <typename Container>
-    typename Container::const_iterator as_insertion_iterator(
-        Container&                               container,
-        const typename Container::const_iterator iterator
-    )
-    {
-        boost::ignore_unused(container);
-
-        return iterator;
-    }
-#else
-    template <typename Container>
-    typename Container::iterator as_insertion_iterator(
-        Container&                               container,
-        const typename Container::const_iterator iterator
-    )
-    {
-        typename Container::iterator mutable_iterator = container.begin();
-        std::advance(
-            mutable_iterator, std::distance<typename Container::const_iterator>(container.begin(), iterator)
-        );
-        return mutable_iterator;
-    }
-#endif
-
-
-}}
-
-
-/* lambda this capture bug workaround *****************************************/
-
-#if !defined(DOCUMENTATION)
-#   if BOOST_COMP_GNUC && BOOST_COMP_GNUC < BOOST_VERSION_NUMBER(4, 8, 0)
-#       define TETENGO2_STDALT_HAS_LAMBDA_THIS_CAPTURE_BUG 1
-#   else
-#       define TETENGO2_STDALT_HAS_LAMBDA_THIS_CAPTURE_BUG 0
-#   endif
-#endif
-
-#if TETENGO2_STDALT_HAS_LAMBDA_THIS_CAPTURE_BUG || defined(DOCUMENTATION)
-    //! The lambda this capture bug workaround.
-#   define TETENGO2_STDALT_LAMBDA_THIS_BUG_WA this,
-#else
-#   define TETENGO2_STDALT_LAMBDA_THIS_BUG_WA
 #endif
 
 
