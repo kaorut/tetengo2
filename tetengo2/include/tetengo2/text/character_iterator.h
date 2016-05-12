632cfb3efb4cf20980bad6d7f24f8258f2f3422b
kaoru <kaorut@mc.pikara.ne.jp>
2016-05-12 11:19:41 +0900

    Create tetengo2::text::character_iterator #75

---
+++ tetengo2/include/tetengo2/text/character_iterator.h
@@ -0,0 +1,45 @@
+/*! \file
+    \brief The definition of tetengo2::text::character_iterator.
+
+    Copyright (C) 2007-2016 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_TEXT_CHARACTERITERATOR_H)
+#define TETENGO2_TEXT_CHARACTERITERATOR_H
+
+
+namespace tetengo2 { namespace text
+{
+    /*!
+        \brief The class template for a character iterator.
+
+        \tparam String A string type.
+    */
+    template <typename String>
+    class character_iterator
+    {
+    public:
+        // types
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Creates a character iterator.
+        */
+        character_iterator()
+        {}
+
+
+        // functions
+
+
+    };
+
+
+}}
+
+
+#endif
---
+++ tetengo2/test/test_tetengo2.text.character_iterator.cpp
@@ -0,0 +1,38 @@
+/*! \file
+    \brief Test of class tetengo2::text::character_iterator.
+
+    Copyright (C) 2007-2016 kaoru
+
+    $Id$
+*/
+
+#include <boost/test/unit_test.hpp>
+
+#include <tetengo2.h>
+
+#include "test_tetengo2.type_list.h"
+
+
+namespace
+{
+    // types
+
+}
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo2)
+BOOST_AUTO_TEST_SUITE(text)
+BOOST_AUTO_TEST_SUITE(character_iterator)
+    // test cases
+
+    BOOST_AUTO_TEST_CASE(construction)
+    {
+        BOOST_TEST_PASSPOINT();
+
+        BOOST_WARN_MESSAGE(false, "Implement It.");
+    }
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
