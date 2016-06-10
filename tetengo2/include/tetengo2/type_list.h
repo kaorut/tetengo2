7b026227330332be24eff95e4b45554a5ca16340
kaoru <kaorut@mc.pikara.ne.jp>
2016-06-11 05:22:51 +0900

    Create tetengo2::type_list #98

---
+++ tetengo2/include/tetengo2/type_list.h
@@ -0,0 +1,24 @@
+/*! \file
+    \brief The definition of the type list for tetengo2.
+
+    Copyright (C) 2007-2016 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_TYPELIST_H)
+#define TETENGO2_TYPELIST_H
+
+#include <cstddef>
+
+
+namespace tetengo2 { namespace type_list
+{
+    //! The type for size.
+    using size_type = std::size_t;
+
+
+}}
+
+
+#endif
