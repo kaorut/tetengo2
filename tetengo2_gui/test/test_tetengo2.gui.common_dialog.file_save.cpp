baf74c322938a311cdc8c4f27efe24e32a4e7c7e
kaoru <kaorut@mc.pikara.ne.jp>
2016-06-15 04:59:18 +0900

    Modify #includes in test_tetengo2::gui::common_dialog #98

--- tetengo2_gui/test/test_tetengo2.gui.common_dialog.file_save.cpp
+++ tetengo2_gui/test/test_tetengo2.gui.common_dialog.file_save.cpp
@@ -10,8 +10,8 @@
 #include <boost/optional.hpp>
 #include <boost/test/unit_test.hpp>
 
-#include <tetengo2.h>
-#include <tetengo2.gui.h>
+#include <tetengo2/gui/common_dialog/file_save.h>
+#include <tetengo2/gui/widget/window.h>
 
 #include "test_tetengo2.gui.type_list.h"
 
