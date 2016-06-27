b9cb5e9acad052922376408886946f81edc8e0cb
kaoru <kaorut@mc.pikara.ne.jp>
2016-06-28 03:41:39 +0900

    Implement tetengo2::detail::base::config #98

---
+++ tetengo2_detail/src/tetengo2.detail.base.config.cpp
@@ -0,0 +1,22 @@
+/*! \file
+    \brief The definition of tetengo2::detail::base::config.
+
+    Copyright (C) 2007-2016 kaoru
+
+    $Id$
+*/
+
+
+#include <tetengo2/detail/base/config.h>
+
+
+namespace tetengo2 { namespace detail { namespace base
+{
+    config::~config()
+    = default;
+
+    config::config()
+    = default;
+
+
+}}}
