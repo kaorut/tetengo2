c7925a9e08d754b91b75ddbac00e5cbc3d103ca9
kaoru <kaorut@mc.pikara.ne.jp>
2016-06-29 04:38:40 +0900

    Implement tetengo2::detail::unixos/windows::config #98

---
+++ tetengo2_detail/src/tetengo2.detail.unixos.config.cpp
@@ -0,0 +1,16 @@
+/*! \file
+    \brief The definition of tetengo2::detail::unixos::config.
+
+    Copyright (C) 2007-2016 kaoru
+
+    $Id$
+*/
+
+#include <tetengo2/detail/unixos/config.h>
+
+
+namespace tetengo2 { namespace detail { namespace unixos
+{
+
+
+}}}
---
+++ tetengo2_detail/src/tetengo2.detail.windows.config.cpp
@@ -0,0 +1,16 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::config.
+
+    Copyright (C) 2007-2016 kaoru
+
+    $Id$
+*/
+
+#include <tetengo2/detail/windows/config.h>
+
+
+namespace tetengo2 { namespace detail { namespace windows
+{
+
+
+}}}
