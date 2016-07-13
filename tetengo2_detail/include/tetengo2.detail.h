caf80cdbb102a28b6b73f81658d8ebe40cfb8035
kaoru <kaorut@mc.pikara.ne.jp>
2016-07-14 05:26:43 +0900

    Create tetengo2.detail.h #98

---
+++ tetengo2_detail/include/tetengo2.detail.h
@@ -0,0 +1,16 @@
+/*! \file
+    \brief The facade of tetengo2::detail.
+
+    Copyright (C) 2007-2016 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_H)
+#define TETENGO2_DETAIL_H
+
+#include <tetengo2/detail/base/config.h>
+#include <tetengo2/detail/config.h>
+
+
+#endif
