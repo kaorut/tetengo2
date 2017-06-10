adc3a106ce1735a306ad91110214e5d995223df5
kaoru <kaorut@mc.pikara.ne.jp>
2017-06-11 06:13:46 +0900

    Implement tetengo2::text::encode #98

--- tetengo2/include/tetengo2/text/encoder.h
+++ tetengo2/include/tetengo2/text/encoder.h
@@ -9,6 +9,7 @@
 #if !defined(TETENGO2_TEXT_ENCODER_H)
 #define TETENGO2_TEXT_ENCODER_H
 
+#include <type_traits>
 #include <utility>
 
 
@@ -97,7 +98,7 @@ namespace tetengo2 { namespace text
         external_string_type encode(internal_string_type string)
         const
         {
-            return m_external_encoding.from_pivot(m_internal_encoding.to_pivot(std::move(string)));
+            return encode_impl<external_string_type>(std::move(string));
         }
 
         /*!
@@ -120,6 +121,43 @@ namespace tetengo2 { namespace text
         internal_encoding_type m_internal_encoding;
 
         external_encoding_type m_external_encoding;
+
+
+        // functions
+
+        template <typename ES, typename IS>
+        ES encode_impl(IS string, typename std::enable_if<std::is_convertible<IS, ES>::value>::type* = nullptr)
+        const
+        {
+            if (m_internal_encoding == m_external_encoding)
+                return std::move(string);
+            else
+                return m_external_encoding.from_pivot(m_internal_encoding.to_pivot(std::move(string)));
+        }
+
+        template <typename ES, typename IS>
+        ES encode_impl(IS string, typename std::enable_if<!std::is_convertible<IS, ES>::value>::type* = nullptr)
+        const
+        {
+            return m_external_encoding.from_pivot(m_internal_encoding.to_pivot(std::move(string)));
+        }
+
+        template <typename IS, typename ES>
+        IS decode(ES string, typename std::enable_if<std::is_convertible<ES, IS>::value>::type* = nullptr)
+        const
+        {
+            if (m_internal_encoding == m_external_encoding)
+                return std::move(string);
+            else
+                return m_internal_encoding.from_pivot(m_external_encoding.to_pivot(std::move(string)));
+        }
+
+        template <typename IS, typename ES>
+        IS decode(ES string, typename std::enable_if<!std::is_convertible<ES, IS>::value>::type* = nullptr)
+        const
+        {
+            return m_internal_encoding.from_pivot(m_external_encoding.to_pivot(std::move(string)));
+        }
 
 
     };
