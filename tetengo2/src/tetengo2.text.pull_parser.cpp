87a920d39d74f49dc1502dfae5d775ab8f42d25e
kaoru <kaorut@mc.pikara.ne.jp>
2017-06-17 11:42:52 +0900

    Implement tetengo2::text::pull_parser #98

---
+++ tetengo2/src/tetengo2.text.pull_parser.cpp
@@ -0,0 +1,16 @@
+/*! \file
+    \brief The definition of tetengo2::text::pull_parser.
+
+    Copyright (C) 2007-2017 kaoru
+
+    $Id$
+*/
+
+#include <tetengo2/text/pull_parser.h>
+
+
+namespace tetengo2 { namespace text
+{
+
+
+}}
