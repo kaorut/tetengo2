2b60aba6e7751174b88c015ca9ecb6232d9d4e55
kaoru <kaorut@mc.pikara.ne.jp>
2017-06-17 22:36:30 +0900

    Implement tetengo2::message::message_catalog_parser #98

---
+++ tetengo2/src/tetengo2.message.message_catalog_parser.cpp
@@ -0,0 +1,16 @@
+/*! \file
+    \brief The definition of tetengo2::message::message_catalog_parser.
+
+    Copyright (C) 2007-2017 kaoru
+
+    $Id$
+*/
+
+#include <tetengo2/message/message_catalog_parser.h>
+
+
+namespace tetengo2 { namespace message
+{
+
+
+}}
