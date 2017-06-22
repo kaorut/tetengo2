70e7de5f85383e06a11f454831c80903a49ba783
kaoru <kaorut@mc.pikara.ne.jp>
2017-06-23 07:27:02 +0900

    Implement tetengo2::detail::stub::alert #98 (wip)

--- tetengo2_detail/src/tetengo2.detail.stub.alert.cpp
+++ tetengo2_detail/src/tetengo2.detail.stub.alert.cpp
@@ -6,11 +6,88 @@
     $Id$
 */
 
+#include <boost/core/ignore_unused.hpp>
+#include <boost/core/noncopyable.hpp>
+
 #include <tetengo2/detail/stub/alert.h>
+#include <tetengo2/stdalt.h>
 
 
 namespace tetengo2 { namespace detail { namespace stub
 {
+    class alert::impl : private boost::noncopyable
+    {
+    public:
+        // types
+
+        using string_type = alert::string_type;
+
+        using widget_handle_type = alert::widget_handle_type;
+
+
+        // static functions
+
+        static const alert& instance();
+
+
+        // functions
+
+        widget_handle_type root_ancestor_widget_handle_impl(const widget_handle_type widget_handle)
+        const
+        {
+            boost::ignore_unused(widget_handle);
+
+            return nullptr;
+        }
+
+        virtual void show_task_dialog_impl(
+            const widget_handle_type widget_handle,
+            const string_type&       caption,
+            const string_type&       text1,
+            const string_type&       text2,
+            const string_type&       source_file_name,
+            const int                source_file_line
+        )
+        const
+        {
+            boost::ignore_unused(widget_handle, caption, text1, text2, source_file_name, source_file_line);
+        }
+
+
+    };
+
+
+    const alert& alert::instance()
+    {
+        return impl::instance();
+    }
+
+    alert::~alert()
+    = default;
+
+    alert::alert()
+    :
+    m_p_impl(stdalt::make_unique<impl>())
+    {}
+
+    alert::widget_handle_type alert::root_ancestor_widget_handle_impl(const widget_handle_type widget_handle)
+    const
+    {
+        return m_p_impl->root_ancestor_widget_handle_impl(widget_handle);
+    }
+
+    void alert::show_task_dialog_impl(
+        const widget_handle_type widget_handle,
+        const string_type&       caption,
+        const string_type&       text1,
+        const string_type&       text2,
+        const string_type&       source_file_name,
+        const int                source_file_line
+    )
+    const
+    {
+        m_p_impl->show_task_dialog_impl(widget_handle, caption, text1, text2, source_file_name, source_file_line);
+    }
 
 
 }}}
