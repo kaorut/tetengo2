557c2a016d1ff3cf430d0b821c3726d5541f2df0
kaoru <kaorut@mc.pikara.ne.jp>
2017-06-29 00:37:10 +0900

    Create tetengo2/detail/base/cursor.h and tetengo2.detail.base/stub/windows.cursor.cpp #98

---
+++ tetengo2_detail/include/tetengo2/detail/base/cursor.h
@@ -0,0 +1,20 @@
+/*! \file
+    \brief The definition of tetengo2::detail::base::cursor.
+
+    Copyright (C) 2007-2017 kaoru
+
+    $Id$
+*/
+
+#if !defined(TETENGO2_DETAIL_BASE_CURSOR_H)
+#define TETENGO2_DETAIL_BASE_CURSOR_H
+
+
+namespace tetengo2 { namespace detail { namespace base
+{
+
+
+}}}
+
+
+#endif
---
+++ tetengo2_detail/src/tetengo2.detail.base.cursor.cpp
@@ -0,0 +1,16 @@
+/*! \file
+    \brief The definition of tetengo2::detail::base::cursor.
+
+    Copyright (C) 2007-2017 kaoru
+
+    $Id$
+*/
+
+#include <tetengo2/detail/base/cursor.h>
+
+
+namespace tetengo2 { namespace detail { namespace base
+{
+
+
+}}}
