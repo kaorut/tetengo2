87dfdc6da542b2257abf25b0a0c658d40cb4acc5
kaoru <kaorut@mc.pikara.ne.jp>
2017-06-29 02:35:50 +0900

    Implement tetengo2::detail::base::cursor #98

--- tetengo2_detail/include/tetengo2/detail/base/cursor.h
+++ tetengo2_detail/include/tetengo2/detail/base/cursor.h
@@ -9,9 +9,81 @@
 #if !defined(TETENGO2_DETAIL_BASE_CURSOR_H)
 #define TETENGO2_DETAIL_BASE_CURSOR_H
 
+#include <memory>
+#include <system_error>
+
+#include <boost/core/noncopyable.hpp>
+
 
 namespace tetengo2 { namespace detail { namespace base
 {
+    /*!
+        \brief The class for a detail implementation of a cursor.
+    */
+    class cursor : private boost::noncopyable
+    {
+    public:
+        // types
+
+        //! The cursor details type.
+        struct cursor_details_type;
+
+        //! The cursor details pointer type.
+        using cursor_details_ptr_type = std::unique_ptr<cursor_details_type>;
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Destroys the detail implementation.
+        */
+        virtual ~cursor();
+
+
+        // functions
+
+        /*!
+            \brief Creates a system cursor.
+
+            \param style A style.
+
+            \return A unique pointer to a system cursor.
+
+            \throw std::system_error When a system cursor cannot be created.
+        */
+        cursor_details_ptr_type create_system_cursor(int style)
+        const;
+
+
+    protected:
+        // constructors
+
+        /*!
+            \brief Creates a detail implementation.
+        */
+        cursor();
+
+
+    private:
+        // types
+
+        class impl;
+
+
+        // variables
+
+        const std::unique_ptr<impl> m_p_impl;
+
+
+        // virtual functions
+
+        virtual cursor_details_ptr_type create_system_cursor_impl(int style)
+        const = 0;
+
+
+    };
+
+
 
 
 }}}
