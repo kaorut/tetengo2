0282363d529d3bb9c747e46c7d923c70281009bc
kaoru <kaorut@mc.pikara.ne.jp>
2017-06-30 08:04:40 +0900

    Create tetengo2.gui.cursor.cursor_base and system #98

---
+++ tetengo2_gui/src/tetengo2.gui.cursor.cursor_base.cpp
@@ -0,0 +1,15 @@
+/*! \file
+    \brief The definition of tetengo2::gui::cursor::cursor_base.
+
+    Copyright (C) 2007-2017 kaoru
+
+    $Id$
+*/
+
+#include <tetengo2/gui/cursor/cursor_base.h>
+
+
+namespace tetengo2 { namespace gui { namespace cursor
+{
+
+}}}
