f724b29da16939483980f677800586c7de021d65
kaoru <kaorut@mc.pikara.ne.jp>
2017-07-06 01:17:33 +0900

    Implement tetengo2::detail::stub::impl_set #98

--- tetengo2_detail/src/tetengo2.detail.stub.impl_set.cpp
+++ tetengo2_detail/src/tetengo2.detail.stub.impl_set.cpp
@@ -6,10 +6,36 @@
     $Id$
 */
 
+#include <tetengo2/detail/stub/alert.h>
+#include <tetengo2/detail/stub/cursor.h>
 #include <tetengo2/detail/stub/impl_set.h>
 
 
 namespace tetengo2 { namespace detail { namespace stub
 {
+    const impl_set& impl_set::instance()
+    {
+        static const impl_set singleton;
+        return singleton;
+    }
+
+    impl_set::~impl_set()
+    = default;
+
+    impl_set::impl_set()
+    = default;
+
+    const base::alert& impl_set::alert_impl()
+    const
+    {
+        return alert::instance();
+    }
+
+    const base::cursor& impl_set::cursor_impl()
+    const
+    {
+        return cursor::instance();
+    }
+
 
 }}}
