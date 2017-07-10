7667a1801962cda6d3c078cfac8cbb275d946273
kaoru <kaorut@mc.pikara.ne.jp>
2017-07-11 06:26:02 +0900

    Create tetengo2.detail.windows(.direct2d/.gdiplus).error_category.cpp #98

---
+++ tetengo2_detail/src/tetengo2.detail.windows.gdiplus.error_category.cpp
@@ -0,0 +1,15 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::gdiplus::error_category.
+
+    Copyright (C) 2007-2017 kaoru
+
+    $Id$
+*/
+
+#include <tetengo2/detail/windows/gdiplus/error_category.h>
+
+
+namespace tetengo2 { namespace detail { namespace windows { namespace gdiplus
+{
+
+}}}}
