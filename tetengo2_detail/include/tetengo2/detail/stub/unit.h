96f673e74d319e7bc85c175a1b38dd8a05bba491
kaoru <kaorut@mc.pikara.ne.jp>
2017-09-19 01:39:55 +0900

    Implement tetengo2::detail::stub::unit #98

--- tetengo2_detail/include/tetengo2/detail/stub/unit.h
+++ tetengo2_detail/include/tetengo2/detail/stub/unit.h
@@ -9,10 +9,9 @@
 #if !defined(TETENGO2_DETAIL_STUB_UNIT_H)
 #define TETENGO2_DETAIL_STUB_UNIT_H
 
-#include <type_traits>
+#include <memory>
 
-#include <boost/core/noncopyable.hpp>
-#include <boost/rational.hpp>
+#include <tetengo2/detail/base/unit.h>
 
 
 namespace tetengo2 { namespace detail { namespace stub
@@ -20,128 +19,62 @@ namespace tetengo2 { namespace detail { namespace stub
     /*!
         \brief The class for a detail implementation of a unit.
     */
-    class unit : private boost::noncopyable
+    class unit : public base::unit
     {
     public:
-        // static functions
+        // types
+
+        //! The size type.
+        using size_type = base::unit::size_type;
+
+        //! The difference type.
+        using difference_type = base::unit::difference_type;
+
+
+        // constructors and destructor
 
         /*!
-            \brief Translates a value in pixels into a value in ems.
-
-            \tparam Value      A value type.
-            \tparam PixelValue A pixel value type.
-
-            \param pixel_value A value in pixels.
-
-            \return The value in ems.
+            \brief Creates a detail implementation.
         */
-        template <typename Value, typename PixelValue>
-        static Value pixels_to_em(const PixelValue pixel_value)
-        {
-            return to_value<Value, PixelValue>(pixel_value, 12);
-        }
-
-        /*!
-            \brief Translates a value in ems into a value in pixels.
-
-            \tparam PixelValue A pixel value type.
-            \tparam Value      A value type.
-
-            \param value A value in ems.
-
-            \return The value in pixels.
-        */
-        template <typename PixelValue, typename Value>
-        static PixelValue em_to_pixels(const Value& value)
-        {
-            return to_pixel_value<PixelValue, Value>(value * Value{ 12 }, 1);
-        }
-
-        /*!
-            \brief Translates a value in pixels into a value in points.
-
-            \tparam Value      A value type.
-            \tparam PixelValue A pixel value type.
-
-            \param pixel_value A value in pixels.
-
-            \return The value in points.
-        */
-        template <typename Value, typename PixelValue>
-        static Value pixels_to_points(const PixelValue pixel_value)
-        {
-            return to_value<Value, PixelValue>(pixel_value * 3, 4);
-        }
-
-        /*!
-            \brief Translates a value in points into a value in pixels.
-
-            \tparam PixelValue A pixel value type.
-            \tparam Value      A value type.
-
-            \param value A value in points.
-
-            \return The value in pixels.
-        */
-        template <typename PixelValue, typename Value>
-        static PixelValue points_to_pixels(const Value& value)
-        {
-            return to_pixel_value<PixelValue, Value>(value * 4, 3);
-        }
+        unit();
 
 
     private:
-        // static functions
+        // types
 
-        template <typename Value, typename PixelValue>
-        static Value to_value(
-            const PixelValue numerator,
-            const PixelValue denominator,
-            typename std::enable_if<
-                std::is_convertible<boost::rational<typename Value::int_type>, Value>::value
-            >::type* = nullptr
-        )
-        {
-            return boost::rational<typename Value::int_type>(numerator, denominator);
-        }
-
-        template <typename Value, typename PixelValue>
-        static Value to_value(
-            const PixelValue numerator,
-            const PixelValue denominator,
-            typename std::enable_if<std::is_arithmetic<Value>::value>::type* = nullptr
-        )
-        {
-            return numerator / denominator;
-        }
-
-        template <typename PixelValue, typename Value>
-        static PixelValue to_pixel_value(
-            const Value& numerator,
-            const Value& denominator,
-            typename std::enable_if<
-                std::is_convertible<boost::rational<typename Value::int_type>, Value>::value
-            >::type* = nullptr
-        )
-        {
-            return boost::rational_cast<PixelValue>(numerator / denominator);
-        }
-
-        template <typename PixelValue, typename Value>
-        static PixelValue to_pixel_value(
-            const Value numerator,
-            const Value denominator,
-            typename std::enable_if<std::is_arithmetic<Value>::value>::type* = nullptr
-        )
-        {
-            return static_cast<PixelValue>(numerator / denominator);
-        }
+        class impl;
 
 
-        // forbidden operations
+        // variables
 
-        unit()
-        = delete;
+        const std::unique_ptr<impl> m_p_impl;
+
+
+        // virtual functions
+
+        virtual boost::rational<size_type> to_em_impl(size_type pixel_size)
+        const override;
+
+        virtual boost::rational<difference_type> to_em_impl(difference_type pixel_difference)
+        const override;
+
+        virtual boost::rational<size_type> to_point_impl(size_type pixel_size)
+        const override;
+
+        virtual boost::rational<difference_type> to_point_impl(difference_type pixel_difference)
+        const override;
+
+        virtual size_type em_to_pixel_impl(const boost::rational<size_type>& em_size)
+        const override;
+
+        virtual difference_type em_to_pixel_impl(const boost::rational<difference_type>& em_difference)
+        const override;
+
+        virtual size_type point_to_pixel_impl(const boost::rational<size_type>& point_size)
+        const override;
+
+        virtual difference_type point_to_pixel_impl(const boost::rational<difference_type>& point_difference)
+        const override;
 
 
     };
--- tetengo2_detail/src/tetengo2.detail.stub.unit.cpp
+++ tetengo2_detail/src/tetengo2.detail.stub.unit.cpp
@@ -6,11 +6,136 @@
     $Id$
 */
 
+#include <boost/core/noncopyable.hpp>
+
 #include <tetengo2/detail/stub/unit.h>
+#include <tetengo2/stdalt.h>
 
 
 namespace tetengo2 { namespace detail { namespace stub
 {
+    class unit::impl : private boost::noncopyable
+    {
+    public:
+        // types
+
+        using size_type = unit::size_type;
+
+        using difference_type = unit::difference_type;
+
+
+        // functions
+
+        boost::rational<size_type> to_em_impl(const size_type pixel_size)
+        const
+        {
+            return boost::rational<size_type>{ pixel_size, 12 };
+        }
+
+        boost::rational<difference_type> to_em_impl(const difference_type pixel_difference)
+        const
+        {
+            return boost::rational<difference_type>{ pixel_difference, 12 };
+        }
+
+        boost::rational<size_type> to_point_impl(const size_type pixel_size)
+        const
+        {
+            return boost::rational<size_type>{ pixel_size * 3, 4 };
+        }
+
+        boost::rational<difference_type> to_point_impl(const difference_type pixel_difference)
+        const
+        {
+            return boost::rational<difference_type>{ pixel_difference * 3, 4 };
+        }
+
+        size_type em_to_pixel_impl(const boost::rational<size_type>& em_size)
+        const
+        {
+            return boost::rational_cast<size_type>(em_size * boost::rational<size_type>{ 12 });
+        }
+
+        difference_type em_to_pixel_impl(const boost::rational<difference_type>& em_difference)
+        const
+        {
+            return boost::rational_cast<difference_type>(em_difference * boost::rational<difference_type>{ 12 });
+        }
+
+        size_type point_to_pixel_impl(const boost::rational<size_type>& point_size)
+        const
+        {
+            return
+                boost::rational_cast<size_type>(
+                    point_size * boost::rational<size_type>{ 4 } / boost::rational<size_type>{ 3 }
+                );
+        }
+
+        difference_type point_to_pixel_impl(const boost::rational<difference_type>& point_difference)
+        const
+        {
+            return
+                boost::rational_cast<difference_type>(
+                    point_difference * boost::rational<difference_type>{ 4 } / boost::rational<difference_type>{ 3 }
+                );
+        }
+
+
+    };
+
+
+    unit::unit()
+    :
+    m_p_impl(stdalt::make_unique<impl>())
+    {}
+
+    boost::rational<unit::size_type> unit::to_em_impl(const size_type pixel_size)
+    const
+    {
+        return m_p_impl->to_em_impl(pixel_size);
+    }
+
+    boost::rational<unit::difference_type> unit::to_em_impl(const difference_type pixel_difference)
+    const
+    {
+        return m_p_impl->to_em_impl(pixel_difference);
+    }
+
+    boost::rational<unit::size_type> unit::to_point_impl(const size_type pixel_size)
+    const
+    {
+        return m_p_impl->to_point_impl(pixel_size);
+    }
+
+    boost::rational<unit::difference_type> unit::to_point_impl(const difference_type pixel_difference)
+    const
+    {
+        return m_p_impl->to_point_impl(pixel_difference);
+    }
+
+    unit::size_type unit::em_to_pixel_impl(const boost::rational<size_type>& em_size)
+    const
+    {
+        return m_p_impl->em_to_pixel_impl(em_size);
+    }
+
+    unit::difference_type unit::em_to_pixel_impl(const boost::rational<difference_type>& em_difference)
+    const
+    {
+        return m_p_impl->em_to_pixel_impl(em_difference);
+    }
+
+    unit::size_type unit::point_to_pixel_impl(const boost::rational<size_type>& point_size)
+    const
+    {
+        return m_p_impl->point_to_pixel_impl(point_size);
+    }
+
+    unit::difference_type unit::point_to_pixel_impl(const boost::rational<difference_type>& point_difference)
+    const
+    {
+        return m_p_impl->point_to_pixel_impl(point_difference);
+    }
 
 
 }}}
