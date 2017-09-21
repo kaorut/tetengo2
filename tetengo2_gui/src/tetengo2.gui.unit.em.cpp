2fc3a1955400017f5856aa9db217dc398f629b71
kaoru <kaorut@mc.pikara.ne.jp>
2017-09-22 05:26:25 +0900

    Create tetengo2.gui.unit.em, pixel and point.cpp #98

---
+++ tetengo2_gui/src/tetengo2.gui.unit.em.cpp
@@ -0,0 +1,14 @@
+/*! \file
+    \brief The definition of tetengo2::gui::unit::em.
+
+    Copyright (C) 2007-2017 kaoru
+
+    $Id$
+*/
+
+
+namespace tetengo2 { namespace gui { namespace unit
+{
+
+
+}}}
---
+++ tetengo2_gui/src/tetengo2.gui.unit.point.cpp
@@ -0,0 +1,14 @@
+/*! \file
+    \brief The definition of tetengo2::gui::unit::point.
+
+    Copyright (C) 2007-2017 kaoru
+
+    $Id$
+*/
+
+
+namespace tetengo2 { namespace gui { namespace unit
+{
+
+
+}}}
