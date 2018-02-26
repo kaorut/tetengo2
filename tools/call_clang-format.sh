36037e507e6ef2dd85149dc865537aa51cb26024
kaoru <kaorut@mc.pikara.ne.jp>
2018-02-27 06:24:28 +0900

    Change how to call clang-format #124

---
+++ tools/call_clang-format.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Calls clang-format.
+# Copyright (C) 2007-2018 kaoru
+# $Id$
+
+CLANGFORMAT=$1
+FILE=$2
+
+"$CLANGFORMAT" -style=file "$FILE" > "$FILE.tmp"
+mv -f "$FILE.tmp" "$FILE"
+#!/bin/sh
+# Calls clang-format.
+# Copyright (C) 2007-2018 kaoru
+# $Id$
+
+CLANGFORMAT=$1
+FILE=$2
+
+"$CLANGFORMAT" -style=file "$FILE" > "$FILE.tmp"
+mv -f "$FILE.tmp" "$FILE"
