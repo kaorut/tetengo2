739e9eb0892c91a0eacc31b46ee8eadd5eb76786
kaoru <kaorut@mc.pikara.ne.jp>
2018-02-28 02:29:03 +0900

    Speed up call_clang-format.sh #124

--- tools/call_clang-format.sh
+++ tools/call_clang-format.sh
@@ -1,11 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 # Calls clang-format.
 # Copyright (C) 2007-2018 kaoru
 # $Id$
 
 CLANGFORMAT=$1
-FILE=$2
+FILES=${@:2}
 
-echo formatting $FILE ...
-"$CLANGFORMAT" -style=file "$FILE" > "$FILE.tmp"
-mv -f "$FILE.tmp" "$FILE"
+CORE_COUNT=`grep -c '^processor' /proc/cpuinfo`
+
+function call_clangformat_iter()
+{
+    FILE=$1
+    
+    echo "formatting $FILE ... "
+    "$CLANGFORMAT" -style=file > "$FILE.tmp"
+    mv -f "$FILE.tmp" "$FILE"
+}
+export -f call_clangformat_iter
+
+echo $FILES | tr ' ' '\n' | xargs -P $CORE_COUNT -n 1 -I {} bash -c "call_clangformat_iter {}"
+
