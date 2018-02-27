b9ab6a458f825e82253e2927f0795c0facb6d650
kaoru <kaorut@mc.pikara.ne.jp>
2018-02-28 02:28:37 +0900

    Correct call_clang-format.sh #124

--- tools/call_clang-format.sh
+++ tools/call_clang-format.sh
@@ -6,16 +6,6 @@
 CLANGFORMAT=$1
 FILE=$2
 
-"$CLANGFORMAT" -style=file "$FILE" > "$FILE.tmp"
-mv -f "$FILE.tmp" "$FILE"
-#!/bin/sh
-# Calls clang-format.
-# Copyright (C) 2007-2018 kaoru
-# $Id$
-
-CLANGFORMAT=$1
-FILE=$2
-
 echo formatting $FILE ...
 "$CLANGFORMAT" -style=file "$FILE" > "$FILE.tmp"
 mv -f "$FILE.tmp" "$FILE"
