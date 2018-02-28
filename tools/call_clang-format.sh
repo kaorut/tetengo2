07657a47324c8856055b502661cbe966bbbd35bb
kaoru <kaorut@mc.pikara.ne.jp>
2018-03-01 06:28:42 +0900

    Remove traing spaces, fix the line ends and modify .clang-format #124

--- tools/call_clang-format.sh
+++ tools/call_clang-format.sh
@@ -3,20 +3,28 @@
 # Copyright (C) 2007-2018 kaoru
 # $Id$
 
-CLANGFORMAT=$1
-FILES=${@:2}
+UNIX2DOS=$1
+CLANGFORMAT=$2
+FILES=${@:3}
 
 CORE_COUNT=`grep -c '^processor' /proc/cpuinfo`
 
 function call_clangformat_iter()
 {
-    CLANGFORMAT=$1
-    FILE=$2
-    
+    UNIX2DOS=$1
+    CLANGFORMAT=$2
+    FILE=$3
+
     "$CLANGFORMAT" -style=file "$FILE" > "$FILE.tmp"
+    test $? -eq 0 && mv -f "$FILE.tmp" "$FILE"
+
+    "$UNIX2DOS" < "$FILE" > "$FILE.tmp"
+    test $? -eq 0 && mv -f "$FILE.tmp" "$FILE"
+
+    sed -e "s/ +\(\r\n\)/\\1/g" < "$FILE" > "$FILE.tmp"
     test $? -eq 0 && mv -f "$FILE.tmp" "$FILE"
 }
 export -f call_clangformat_iter
 
-echo $FILES | tr ' ' '\n' | xargs -P $CORE_COUNT -n 1 -I {} bash -c "call_clangformat_iter $CLANGFORMAT {}"
+echo $FILES | tr ' ' '\n' | xargs -t -P $CORE_COUNT -n 1 -I {} bash -c "call_clangformat_iter $UNIX2DOS $CLANGFORMAT {}"
 
