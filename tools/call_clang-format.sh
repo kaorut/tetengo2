0ef77809ba5f3c583bc9cd95deca986a30ad19ff
kaoru <kaorut@mc.pikara.ne.jp>
2018-03-01 06:29:29 +0900

    Correct call_clang-format.sh #124

--- tools/call_clang-format.sh
+++ tools/call_clang-format.sh
@@ -21,10 +21,9 @@ function call_clangformat_iter()
     sed -e "s/ \+\(\r\?\)$/\1/g" < "$FILE" > "$FILE.tmp"
     test $? -eq 0 && mv -f "$FILE.tmp" "$FILE"
 
-    "$CLANGFORMAT" -style=file "$FILE" > "$FILE.tmp"
+    "$CLANGFORMAT" -style=file -fallback-style=none "$FILE" > "$FILE.tmp"
     test $? -eq 0 && mv -f "$FILE.tmp" "$FILE"
 }
 export -f call_clangformat_iter
 
 echo $FILES | tr ' ' '\n' | xargs -P $CORE_COUNT -n 1 -I {} bash -c "call_clangformat_iter $UNIX2DOS $CLANGFORMAT {}"
-
