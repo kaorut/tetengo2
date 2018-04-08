13ac80a8e41c3a95ed0c172221a3de6022ecb4d2
kaoru <kaorut@mc.pikara.ne.jp>
2018-04-09 05:33:49 +0900

    Create the alternative to std::filesystem #127

--- tetengo2/include/tetengo2/stdalt.h
+++ tetengo2/include/tetengo2/stdalt.h
@@ -9,6 +9,14 @@
 #if !defined(TETENGO2_STDALT_H)
 #define TETENGO2_STDALT_H
 
+#if __has_include(<filesystem>)
+#   include <filesystem>
+#elif __has_include(<experimental/filesystem>)
+#   include <experimental/filesystem>
+#else
+#   error Standard filesystem library not found.
+#endif
+
 #include <boost/predef.h>
 
 
@@ -21,5 +29,21 @@
 #define TETENGO2_STDALT_MAYBE_UNUSED [[maybe_unused]]
 #endif
 
+
+namespace tetengo2::stdalt {
+#if (BOOST_COMP_MSVC && BOOST_COMP_MSVC < BOOST_VERSION_NUMBER(19, 20, 0)) || \
+    (BOOST_COMP_CLANG && BOOST_COMP_CLANG < BOOST_VERSION_NUMBER(6, 0, 0)) || \
+    (BOOST_COMP_GNUC && BOOST_COMP_GNUC < BOOST_VERSION_NUMBER(7, 0, 0))
+    namespace filesystem = std::experimental::filesystem;
+#elif defined(DOCUMENTATION)
+    /*!
+        \brief The alternate to std::filesystem.
+    */
+    namespace filesystem = std::filesystem;
+#else
+#   error Unsupported compiler.
+#endif
+
+}
 
 #endif
