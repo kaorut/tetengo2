8984deaf7c31b4f7ffb723f8527df0017eac9ad3
kaoru <kaorut@mc.pikara.ne.jp>
2018-06-07 00:13:49 +0900

    Rewrite tetengo2::detail::windows::picture #98

--- tetengo2_detail/include/tetengo2/detail/windows/picture.h
+++ tetengo2_detail/include/tetengo2/detail/windows/picture.h
@@ -9,188 +9,97 @@
 #if !defined(TETENGO2_DETAIL_WINDOWS_PICTURE_H)
 #define TETENGO2_DETAIL_WINDOWS_PICTURE_H
 
-#include <system_error> // IWYU pragma: keep
-#include <utility> // IWYU pragma: keep
+#include <memory>
 
-#include <boost/throw_exception.hpp>
-
-#pragma warning(push)
-#pragma warning(disable : 4005)
-#include <intsafe.h>
-#include <stdint.h> // IWYU pragma: keep
-#pragma warning(pop)
-#define NOMINMAX
-#define OEMRESOURCE
-#include <ObjBase.h>
-#include <Windows.h>
-#include <wincodec.h>
-
-#include <tetengo2/detail/windows/com_ptr.h> // IWYU pragma: keep
-#include <tetengo2/detail/windows/error_category.h> // IWYU pragma: keep
+#include <boost/core/noncopyable.hpp>
+#include <tetengo2/detail/windows/com_ptr.h>
+#include <tetengo2/gui/dimension.h>
 #include <tetengo2/stdalt.h>
 
-#if !defined(DOCUMENTATION)
-// Use the older version of WIC with the Windows SDK 8.0 for the time being.
-#if defined(_WIN32_WINNT_WIN8) && _WIN32_WINNT >= _WIN32_WINNT_WIN8
-#define Tetengo2_CLSID_WICImagingFactory ::CLSID_WICImagingFactory1
-#else
-#define Tetengo2_CLSID_WICImagingFactory ::CLSID_WICImagingFactory
-#endif
-#endif
 
-
-namespace tetengo2::detail::windows::picture {
-#if !defined(DOCUMENTATION)
-    namespace detail {
+namespace tetengo2::detail::windows {
+    /*!
+        \brief The class for a detail implementation of a picture.
+    */
+    class picture : private boost::noncopyable
+    {
+    public:
         // types
 
-        using wic_imaging_factory_ptr_type = unique_com_ptr<::IWICImagingFactory>;
+        //! The dimension type.
+        using dimension_type = gui::em_dimension;
+
+        //! The details type.
+        using details_type = ::IWICBitmapSource;
+
+        //! The details pointer type.
+        using details_ptr_type = unique_com_ptr<details_type>;
+
+
+        // static functions
+
+        /*!
+            \brief Returns the instance.
+
+            \return The instance.
+        */
+        static const picture& instance();
 
 
         // functions
 
-        inline wic_imaging_factory_ptr_type create_wic_imaging_factory()
-        {
-            ::IWICImagingFactory* rp_factory = nullptr;
-            const auto            hr = ::CoCreateInstance(
-                Tetengo2_CLSID_WICImagingFactory, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&rp_factory));
-            if (FAILED(hr))
-            {
-                BOOST_THROW_EXCEPTION(
-                    (std::system_error{ std::error_code{ hr, wic_category() }, "Can't create WIC imaging factory." }));
-            }
+        /*!
+            \brief Returns the WIC image factory.
 
-            return wic_imaging_factory_ptr_type{ rp_factory };
-        }
-    }
-#endif
+            \return The WIC image factory.
+        */
+        ::IWICImagingFactory& wic_imaging_factory() const;
+
+        /*!
+            \brief Creates a picture.
+
+            \param dimension A dimension.
+
+            \return A unique pointer to a picture.
+        */
+        details_ptr_type create(const dimension_type& dimension) const;
+
+        /*!
+            \brief Reads a picture.
+
+            \param path A path.
+
+            \return A unique pointer to a picture.
+
+            \throw std::system_error When the picture cannot be read.
+        */
+        details_ptr_type read(const tetengo2::stdalt::filesystem::path& path) const;
+
+        /*!
+            \brief Returns the dimension of a picture.
+
+            \param picture A picture.
+
+            \return The dimension of the picture.
+        */
+        dimension_type dimension(const details_type& picture) const;
 
 
-    // types
+    private:
+        // types
 
-    //! The details type.
-    using details_type = ::IWICBitmapSource;
-
-    //! The details pointer type.
-    using details_ptr_type = unique_com_ptr<details_type>;
+        class impl;
 
 
-    // functions
+        // variables
 
-    /*!
-        \brief Returns the WIC image factory.
+        const std::unique_ptr<impl> m_p_impl;
 
-        \return The WIC image factory.
-    */
-    inline ::IWICImagingFactory& wic_imaging_factory()
-    {
-        static const detail::wic_imaging_factory_ptr_type p_factory{ detail::create_wic_imaging_factory() };
-        return *p_factory;
-    }
 
-    /*!
-        \brief Creates a picture.
+        // constructors
 
-        \tparam Dimension A dimension type.
-
-        \param dimension A dimension.
-
-        \return A unique pointer to a picture.
-    */
-    template <typename Dimension>
-    details_ptr_type create(const Dimension& dimension)
-    {
-        ::IWICBitmap* rp_bitmap = nullptr;
-        const auto    hr = wic_imaging_factory().CreateBitmap(
-            dimension.width().to_pixels(),
-            dimension.height().to_pixels(),
-            ::GUID_WICPixelFormat32bppPBGRA,
-            ::WICBitmapCacheOnDemand,
-            &rp_bitmap);
-        if (FAILED(hr))
-            BOOST_THROW_EXCEPTION((std::system_error{ std::error_code(hr, wic_category()), "Can't create bitmap." }));
-
-        return { rp_bitmap };
-    }
-
-    /*!
-        \brief Reads a picture.
-
-        \param path A path.
-
-        \return A unique pointer to a picture.
-
-        \throw std::system_error When the picture cannot be read.
-    */
-    inline details_ptr_type read(const tetengo2::stdalt::filesystem::path& path)
-    {
-        ::IWICBitmapDecoder* rp_decoder = nullptr;
-        const auto           create_decoder_hr = wic_imaging_factory().CreateDecoderFromFilename(
-            path.c_str(), nullptr, GENERIC_READ, WICDecodeMetadataCacheOnDemand, &rp_decoder);
-        if (FAILED(create_decoder_hr))
-        {
-            BOOST_THROW_EXCEPTION((std::system_error{ std::error_code{ create_decoder_hr, wic_category() },
-                                                      "Can't create WIC decoder." }));
-        }
-        const unique_com_ptr<::IWICBitmapDecoder> p_decoder{ rp_decoder };
-
-        ::IWICBitmapFrameDecode* rp_frame = nullptr;
-        const auto               get_frame_hr = p_decoder->GetFrame(0, &rp_frame);
-        if (FAILED(get_frame_hr))
-        {
-            BOOST_THROW_EXCEPTION(
-                (std::system_error{ std::error_code{ get_frame_hr, wic_category() }, "Can't create bitmap frame." }));
-        }
-        const unique_com_ptr<::IWICBitmapFrameDecode> p_frame{ rp_frame };
-
-        ::IWICFormatConverter* rp_format_converter = nullptr;
-        const auto create_format_converter_hr = wic_imaging_factory().CreateFormatConverter(&rp_format_converter);
-        if (FAILED(create_format_converter_hr))
-        {
-            BOOST_THROW_EXCEPTION((std::system_error{ std::error_code{ create_format_converter_hr, wic_category() },
-                                                      "Can't create format converter." }));
-        }
-        unique_com_ptr<::IWICFormatConverter> p_format_converter{ rp_format_converter };
-
-        const auto initialize_hr = p_format_converter->Initialize(
-            p_frame.get(),
-            ::GUID_WICPixelFormat32bppPBGRA,
-            WICBitmapDitherTypeNone,
-            nullptr,
-            0.0,
-            WICBitmapPaletteTypeCustom);
-        if (FAILED(initialize_hr))
-        {
-            BOOST_THROW_EXCEPTION((std::system_error{ std::error_code{ initialize_hr, wic_category() },
-                                                      "Can't initialize format converter." }));
-        }
-
-        return { std::move(p_format_converter) };
-    }
-
-    /*!
-        \brief Returns the dimension of a picture.
-
-        \tparam Dimension A dimension type.
-
-        \param picture A picture.
-
-        \return The dimension of the picture.
-    */
-    template <typename Dimension>
-    Dimension dimension(const details_type& picture)
-    {
-        ::UINT     width = 0;
-        ::UINT     height = 0;
-        const auto hr = const_cast<details_type&>(picture).GetSize(&width, &height);
-        if (FAILED(hr))
-        {
-            BOOST_THROW_EXCEPTION(
-                (std::system_error{ std::error_code{ hr, wic_category() }, "Can't get size of picture." }));
-        }
-
-        return { Dimension::unit_type::from_pixels(width), Dimension::unit_type::from_pixels(height) };
-    }
+        picture();
+    };
 }
 
 
