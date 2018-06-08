50a2e8e9bc0a7bf4306eb7e7806bd811cb63c4b4
kaoru <kaorut@mc.pikara.ne.jp>
2018-06-09 07:01:12 +0900

    Create tetengo2::detail::detail_impl_set() and set_detail_impl_set() #98

--- tetengo2_detail/include/tetengo2/detail/base/impl_set.h
+++ tetengo2_detail/include/tetengo2/detail/base/impl_set.h
@@ -14,98 +14,117 @@
 #include <boost/core/noncopyable.hpp>
 
 
-namespace tetengo2::detail::base {
-    class alert;
-    class cursor;
-    class gui_fixture;
-    class icon;
-    class shell;
-    class unit;
+namespace tetengo2::detail {
+    namespace base {
+        class alert;
+        class cursor;
+        class gui_fixture;
+        class icon;
+        class shell;
+        class unit;
+
+
+        /*!
+            The class for a detail implementation set.
+        */
+        class impl_set : private boost::noncopyable
+        {
+        public:
+            // constructors and destructor
+
+            /*!
+                \brief Destroys the detail implementation set.
+            */
+            virtual ~impl_set();
+
+
+            // functions
+
+            /*!
+                \brief Returns the detail implementation of alert.
+
+                \return The detail implementation.
+            */
+            const alert& alert_() const;
+
+            /*!
+                \brief Returns the detail implementation of alert.
+
+                \return The detail implementation.
+            */
+            const cursor& cursor_() const;
+
+            /*!
+                \brief Creates a detail implementation of GUI fixture.
+
+                \return A unique pointer to a detail implementation.
+            */
+            std::unique_ptr<gui_fixture> create_gui_fixture() const;
+
+            /*!
+                \brief Returns the detail implementation of icon.
+
+                \return The detail implementation.
+            */
+            const icon& icon_() const;
+
+            /*!
+                \brief Returns the detail implementation of shell.
+
+                \return The detail implementation.
+            */
+            const shell& shell_() const;
+
+            /*!
+                \brief Returns the detail implementation of unit.
+
+                \return The detail implementation.
+            */
+            const unit& unit_() const;
+
+
+        protected:
+            // constructors
+
+            /*!
+                \brief Creates a detail implementation set.
+            */
+            impl_set();
+
+
+        private:
+            // virtual functions
+
+            virtual const alert& alert_impl() const = 0;
+
+            virtual const cursor& cursor_impl() const = 0;
+
+            virtual std::unique_ptr<gui_fixture> create_gui_fixture_impl() const = 0;
+
+            virtual const icon& icon_impl() const = 0;
+
+            virtual const shell& shell_impl() const = 0;
+
+            virtual const unit& unit_impl() const = 0;
+        };
+    }
 
 
     /*!
-        The class for a detail implementation set.
+        \brief Returns the detail implementation set.
+
+        \return The detail implementation set.
+
+        \throw std::logic_error When no implementation set is set yet.
     */
-    class impl_set : private boost::noncopyable
-    {
-    public:
-        // constructors and destructor
+    const base::impl_set& detail_impl_set();
 
-        /*!
-            \brief Destroys the detail implementation set.
-        */
-        virtual ~impl_set();
+    /*!
+        \brief Sets a detail implementation set.
 
-
-        // functions
-
-        /*!
-            \brief Returns the detail implementation of alert.
-
-            \return The detail implementation.
-        */
-        const alert& alert_() const;
-
-        /*!
-            \brief Returns the detail implementation of alert.
-
-            \return The detail implementation.
-        */
-        const cursor& cursor_() const;
-
-        /*!
-            \brief Creates a detail implementation of GUI fixture.
-
-            \return A unique pointer to a detail implementation.
-        */
-        std::unique_ptr<gui_fixture> create_gui_fixture() const;
-
-        /*!
-            \brief Returns the detail implementation of icon.
-
-            \return The detail implementation.
-        */
-        const icon& icon_() const;
-
-        /*!
-            \brief Returns the detail implementation of shell.
-
-            \return The detail implementation.
-        */
-        const shell& shell_() const;
-
-        /*!
-            \brief Returns the detail implementation of unit.
-
-            \return The detail implementation.
-        */
-        const unit& unit_() const;
-
-
-    protected:
-        // constructors
-
-        /*!
-            \brief Creates a detail implementation set.
-        */
-        impl_set();
-
-
-    private:
-        // virtual functions
-
-        virtual const alert& alert_impl() const = 0;
-
-        virtual const cursor& cursor_impl() const = 0;
-
-        virtual std::unique_ptr<gui_fixture> create_gui_fixture_impl() const = 0;
-
-        virtual const icon& icon_impl() const = 0;
-
-        virtual const shell& shell_impl() const = 0;
-
-        virtual const unit& unit_impl() const = 0;
-    };
+        \param impl_set A detail implementation set.
+    */
+    void set_detail_impl_set(const base::impl_set& impl_set);
 }
 
 
--- tetengo2_detail/src/tetengo2.detail.base.impl_set.cpp
+++ tetengo2_detail/src/tetengo2.detail.base.impl_set.cpp
@@ -7,6 +7,9 @@
 */
 
 #include <memory>
+#include <stdexcept>
+
+#include <boost/throw_exception.hpp>
 
 #include <tetengo2/detail/base/gui_fixture.h>
 #include <tetengo2/detail/base/impl_set.h>
@@ -21,38 +24,58 @@ namespace tetengo2::detail::base {
 }
 
 
-namespace tetengo2::detail::base {
-    impl_set::~impl_set() = default;
+namespace tetengo2::detail {
+    namespace base {
+        impl_set::~impl_set() = default;
 
-    const alert& impl_set::alert_() const
-    {
-        return alert_impl();
+        const alert& impl_set::alert_() const
+        {
+            return alert_impl();
+        }
+
+        const cursor& impl_set::cursor_() const
+        {
+            return cursor_impl();
+        }
+
+        std::unique_ptr<gui_fixture> impl_set::create_gui_fixture() const
+        {
+            return create_gui_fixture_impl();
+        }
+
+        const icon& impl_set::icon_() const
+        {
+            return icon_impl();
+        }
+
+        const shell& impl_set::shell_() const
+        {
+            return shell_impl();
+        }
+
+        const unit& impl_set::unit_() const
+        {
+            return unit_impl();
+        }
+
+        impl_set::impl_set() = default;
     }
 
-    const cursor& impl_set::cursor_() const
-    {
-        return cursor_impl();
+
+    namespace {
+        const base::impl_set* g_p_impl_set = nullptr;
     }
 
-    std::unique_ptr<gui_fixture> impl_set::create_gui_fixture() const
+    const base::impl_set& detail_impl_set()
     {
-        return create_gui_fixture_impl();
+        if (!g_p_impl_set)
+            BOOST_THROW_EXCEPTION(std::logic_error{ "No detail implementation set is set yet." });
+
+        return *g_p_impl_set;
     }
 
-    const icon& impl_set::icon_() const
+    void set_detail_impl_set(const base::impl_set& impl_set)
     {
-        return icon_impl();
+        g_p_impl_set = &impl_set;
     }
-
-    const shell& impl_set::shell_() const
-    {
-        return shell_impl();
-    }
-
-    const unit& impl_set::unit_() const
-    {
-        return unit_impl();
-    }
-
-    impl_set::impl_set() = default;
 }
