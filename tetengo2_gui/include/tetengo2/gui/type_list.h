38cf680e374e3f7f650bf999a8aa9b7052652201
kaoru <kaorut@mc.pikara.ne.jp>
2018-06-28 00:12:39 +0900

    Add type list types #98

--- tetengo2_gui/include/tetengo2/gui/type_list.h
+++ tetengo2_gui/include/tetengo2/gui/type_list.h
@@ -11,11 +11,18 @@
 
 #include <tetengo2/gui/dimension.h>
 #include <tetengo2/gui/position.h>
+#include <tetengo2/gui/unit/em.h>
 
 
 namespace tetengo2::gui::type_list {
+    //! The type for position unit;
+    using position_unit_type = gui::unit::em;
+
     //! The type for position.
     using position_type = gui::em_position;
+
+    //! The type for dimension unit;
+    using dimension_unit_type = gui::unit::uem;
 
     //! The type for dimension.
     using dimension_type = gui::em_dimension;
