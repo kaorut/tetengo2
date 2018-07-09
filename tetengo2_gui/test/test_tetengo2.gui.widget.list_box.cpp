e915952b1d403f0bac4bfe6bba04c36202a6f32b
kaoru <kaorut@mc.pikara.ne.jp>
2018-07-10 02:33:16 +0900

    Remove template parameters from tetengo2::gui::widget::list_box #98

--- tetengo2_gui/test/test_tetengo2.gui.widget.list_box.cpp
+++ tetengo2_gui/test/test_tetengo2.gui.widget.list_box.cpp
@@ -11,7 +11,6 @@
 #include <string>
 
 #include <boost/preprocessor.hpp>
-#include <boost/signals2.hpp>
 #include <boost/test/unit_test.hpp>
 
 #include <tetengo2/gui/widget/list_box.h>
@@ -36,11 +35,7 @@ namespace {
 
     using window_type = tetengo2::gui::widget::window;
 
-    using list_box_type = tetengo2::gui::widget::list_box<
-        common_type_list_type::widget_details_type,
-        common_type_list_type::drawing_details_type,
-        common_type_list_type::scroll_details_type,
-        common_type_list_type::message_handler_details_type>;
+    using list_box_type = tetengo2::gui::widget::list_box;
 }
 
 
