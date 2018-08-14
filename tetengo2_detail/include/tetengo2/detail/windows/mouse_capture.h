279c3b0e79eecd315f19026e9375fc2e95d4ab21
kaoru <kaorut@mc.pikara.ne.jp>
2018-08-14 23:17:33 +0900

    Implement tetengo2::detail::windows::mouse_capture #98

--- tetengo2_detail/include/tetengo2/detail/windows/mouse_capture.h
+++ tetengo2_detail/include/tetengo2/detail/windows/mouse_capture.h
@@ -9,23 +9,20 @@
 #if !defined(TETENGO2_DETAIL_WINDOWS_MOUSECAPTURE_H)
 #define TETENGO2_DETAIL_WINDOWS_MOUSECAPTURE_H
 
-#include <boost/core/noncopyable.hpp>
+#include <memory>
 
-#pragma warning(push)
-#pragma warning(disable : 4005)
-#include <intsafe.h>
-#include <stdint.h> // IWYU pragma: keep
-#pragma warning(pop)
-#define NOMINMAX
-#define OEMRESOURCE
-#include <Windows.h>
+#include <tetengo2/detail/base/mouse_capture.h>
+
+namespace tetengo2 { namespace gui { namespace widget {
+    class widget;
+}}}
 
 
 namespace tetengo2::detail::windows {
     /*!
         \brief The class for a detail implementation of a mouse capture.
     */
-    class mouse_capture : private boost::noncopyable
+    class mouse_capture : public base::mouse_capture
     {
     public:
         // constructors and destructor
@@ -33,27 +30,25 @@ namespace tetengo2::detail::windows {
         /*!
             \brief Creates a detail implementation of a mouse capture.
 
-            \tparam Widget A widget type.
-
             \param widget A widget.
         */
-        template <typename Widget>
-        explicit mouse_capture(const Widget& widget)
-        {
-            ::SetCapture(widget.details().handle.get());
-        }
+        explicit mouse_capture(const gui::widget::widget& widget);
 
         /*!
             \brief Destroys the detail implementation of a mouse capture.
         */
-        ~mouse_capture() noexcept
-        {
-            ::ReleaseCapture();
-        }
+        ~mouse_capture();
 
 
     private:
+        // types
+
+        class impl;
+
+
         // variables
+
+        const std::unique_ptr<impl> m_p_impl;
     };
 }
 
---
+++ tetengo2_detail/src/tetengo2.detail.windows.mouse_capture.cpp
@@ -0,0 +1,49 @@
+/*! \file
+    \brief The definition of tetengo2::detail::windows::mouse_capture.
+
+    Copyright (C) 2007-2018 kaoru
+
+    $Id$
+*/
+
+#include <memory>
+
+#include <boost/core/noncopyable.hpp>
+
+#pragma warning(push)
+#pragma warning(disable : 4005)
+#include <intsafe.h>
+#include <stdint.h> // IWYU pragma: keep
+#pragma warning(pop)
+#define NOMINMAX
+#define OEMRESOURCE
+#include <Windows.h>
+
+#include <tetengo2/detail/windows/mouse_capture.h>
+#include <tetengo2/detail/windows/widget.h>
+#include <tetengo2/gui/widget/widget.h>
+
+
+namespace tetengo2::detail::windows {
+    class mouse_capture::impl : private boost::noncopyable
+    {
+    public:
+        // constructors and destructor
+
+        explicit impl(const gui::widget::widget& widget)
+        {
+            ::SetCapture(reinterpret_cast<::HWND>(
+                static_cast<const detail::windows::widget::windows_widget_details_type&>(widget.details()).handle));
+        }
+
+        ~impl() noexcept
+        {
+            ::ReleaseCapture();
+        }
+    };
+
+
+    mouse_capture::mouse_capture(const gui::widget::widget& widget) : m_p_impl{ std::make_unique<impl>(widget) } {}
+
+    mouse_capture::~mouse_capture() = default;
+}
