b7b78a768b2325e2a3aefa56dfe7675869789a3c
kaoru <kaorut@mc.pikara.ne.jp>
2018-08-15 22:58:14 +0900

    Implement tetengo2::detail::windows::message_loop #98

--- tetengo2_detail/include/tetengo2/detail/windows/message_loop.h
+++ tetengo2_detail/include/tetengo2/detail/windows/message_loop.h
@@ -9,24 +9,67 @@
 #if !defined(TETENGO2_DETAIL_WINDOWS_MESSAGELOOP_H)
 #define TETENGO2_DETAIL_WINDOWS_MESSAGELOOP_H
 
-#include <system_error>
+#include <memory>
 
-#include <boost/core/noncopyable.hpp>
-#include <boost/throw_exception.hpp>
+#include <tetengo2/detail/base/message_loop.h>
 
-#pragma warning(push)
-#pragma warning(disable : 4005)
-#include <intsafe.h>
-#include <stdint.h> // IWYU pragma: keep
-#pragma warning(pop)
-#define NOMINMAX
-#define OEMRESOURCE
-#include <Windows.h>
-
-#include <tetengo2/detail/windows/error_category.h>
+namespace tetengo2 { namespace gui { namespace widget {
+    class abstract_window;
+}}}
 
 
 namespace tetengo2::detail::windows {
+    /*!
+        \brief The class for a detail implementation of a message loop.
+    */
+    class message_loop : public base::message_loop
+    {
+    public:
+        // static functions
+
+        /*!
+            \brief Returns the instance.
+
+            \return The instance.
+        */
+        static const message_loop& instance();
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Destroys the detail implementation.
+        */
+        virtual ~message_loop();
+
+
+    private:
+        // types
+
+        class impl;
+
+
+        // variables
+
+        const std::unique_ptr<impl> m_p_impl;
+
+
+        // constructors
+
+        message_loop();
+
+
+        // virtual functions
+
+        virtual int loop_impl(gui::widget::abstract_window& window) const override;
+
+        virtual int dialog_loop_impl(gui::widget::abstract_window& dialog) const override;
+
+        virtual void break_loop_impl(int exit_code) const override;
+    };
+
+
+#if 0
     /*!
         \brief The class for a detail implementation of a message loop.
     */
@@ -154,6 +197,7 @@ namespace tetengo2::detail::windows {
 
         message_loop() = delete;
     };
+#endif
 }
 
 
