c81a0f9152d2e850f15f67d8afd2490b044c8e5f
kaoru <kaorut@mc.pikara.ne.jp>
2018-09-07 07:18:30 +0900

    Remove template parameters from tetengo2::gui::common_dialog::file_save and move the implementation to .cpp #98

--- tetengo2_gui/test/test_tetengo2.gui.common_dialog.file_save.cpp
+++ tetengo2_gui/test/test_tetengo2.gui.common_dialog.file_save.cpp
@@ -20,30 +20,17 @@
 #include <tetengo2/gui/widget/window.h>
 #include <tetengo2/stdalt.h>
 #include <tetengo2/text.h>
-
-#include "test_tetengo2.gui.detail_type_list.h"
-#include "test_tetengo2.gui.type_list.h"
+#include <tetengo2/type_list.h>
 
 
 namespace {
     // types
 
-    using detail_type_list_type = test_tetengo2::gui::type_list::detail_for_test;
-
-    using common_type_list_type = test_tetengo2::gui::type_list::common<detail_type_list_type>;
-
-    using string_type = common_type_list_type::string_type;
+    using string_type = tetengo2::type_list::string_type;
 
     using window_type = tetengo2::gui::widget::window;
 
-    using common_dialog_details_type = detail_type_list_type::common_dialog_type;
-
-    using file_save_dialog_type = tetengo2::gui::common_dialog::file_save<
-        common_dialog_details_type,
-        common_type_list_type::widget_details_type,
-        common_type_list_type::drawing_details_type,
-        common_type_list_type::scroll_details_type,
-        common_type_list_type::message_handler_details_type>;
+    using file_save_dialog_type = tetengo2::gui::common_dialog::file_save;
 
 
     // functions
