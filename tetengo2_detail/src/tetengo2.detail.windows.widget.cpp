28f1347799b294c06bf1058396d9224d45f7b83b
kaoru <kaorut@mc.pikara.ne.jp>
2018-09-12 07:50:46 +0900

    Correct a compilation error with VC++ #98

--- tetengo2_detail/src/tetengo2.detail.windows.widget.cpp
+++ tetengo2_detail/src/tetengo2.detail.windows.widget.cpp
@@ -15,8 +15,9 @@
 #pragma warning(pop)
 #define NOMINMAX
 #define OEMRESOURCE
-#include <CommCtrl.h>
 #include <Windows.h>
+#// prevent sort by ClangFormat
+#include <CommCtrl.h>
 
 #include <tetengo2/detail/windows/error_category.h>
 #include <tetengo2/detail/windows/icon.h>
