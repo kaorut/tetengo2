4e581f97d4f853ec3ad3e683339bb5dcbd15e0a8
kaoru <kaorut@mc.pikara.ne.jp>
2018-10-30 03:31:49 +0900

    Move the implementation to .cpp #98

--- tetengo2_gui/test/test_tetengo2.gui.message.paint_observer_set.cpp
+++ tetengo2_gui/test/test_tetengo2.gui.message.paint_observer_set.cpp
@@ -27,6 +27,13 @@ BOOST_AUTO_TEST_SUITE(test_tetengo2)
             BOOST_AUTO_TEST_SUITE(paint_observer_set)
                 // test cases
 
+                BOOST_AUTO_TEST_CASE(construction)
+                {
+                    BOOST_TEST_PASSPOINT();
+
+                    const paint_observer_set_type observer_set{};
+                }
+
                 BOOST_AUTO_TEST_CASE(paint_background)
                 {
                     BOOST_TEST_PASSPOINT();
